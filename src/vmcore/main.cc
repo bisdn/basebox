87682f27fa7f19049c68b252cf5b4a30ff5257a8
Andreas Köpsel <andreas.koepsel@bisdn.de>
2013-05-28 17:25:50 +0200

    some progress towards integration of libnl3

--- src/vmcore/main.cc
+++ src/vmcore/main.cc
@@ -1,9 +1,15 @@
+#include "cnetlink.h"
 
 int
 main(int argc, char** argv)
 {
+	cnetlink_owner *owner = 0;
 
-	return 0;
+	cnetlink netlink(owner);
+
+	netlink.get_route_notifications();
+
+	rofl::ciosrv::run();
 }
 
 
