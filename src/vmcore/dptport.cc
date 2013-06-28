05f2a6335836a376a23d2a6e597af9f5fdbe6aac
Andreas Koepsel <andreas.koepsel@bisdn.de>
2013-06-28 12:48:40 +0200

    new class: dptport

---
+++ src/vmcore/dptport.cc
@@ -0,0 +1,10 @@
+/*
+ * cdptport.cc
+ *
+ *  Created on: 28.06.2013
+ *      Author: andreas
+ */
+
+#include <dptport.h>
+
+
---
+++ src/vmcore/dptport.h
@@ -0,0 +1,23 @@
+/*
+ * dptport.h
+ *
+ *  Created on: 28.06.2013
+ *      Author: andreas
+ */
+
+#ifndef DPTPORT_H_
+#define DPTPORT_H_ 1
+
+#include <ctapdev.h>
+
+namespace dptmap
+{
+
+class cdptport
+{
+
+};
+
+}; // end of namespace
+
+#endif /* DPTPORT_H_ */
