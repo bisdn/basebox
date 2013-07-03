c999b42f513ad7e10449d387d4443bd5d2fe4b07
Andreas Koepsel <andreas.koepsel@bisdn.de>
2013-07-03 09:40:36 +0200

    new class: crtneigh

---
+++ src/vmcore/crtneigh.cc
@@ -0,0 +1,20 @@
+/*
+ * crtneigh.cc
+ *
+ *  Created on: 03.07.2013
+ *      Author: andreas
+ */
+
+
+#include <crtneigh.h>
+
+using namespace dptmap;
+
+
+crtneigh::crtneigh()
+{
+
+}
+
+
+
---
+++ src/vmcore/crtneigh.h
@@ -0,0 +1,27 @@
+/*
+ * crtneigh.h
+ *
+ *  Created on: 03.07.2013
+ *      Author: andreas
+ */
+
+#ifndef CRTNEIGH_H_
+#define CRTNEIGH_H_ 1
+
+
+namespace dptmap
+{
+
+class crtneigh
+{
+public:
+
+	/**
+	 *
+	 */
+	crtneigh();
+};
+
+}; // end of namespace
+
+#endif /* CRTNEIGH_H_ */
