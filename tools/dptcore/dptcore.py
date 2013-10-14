4c6239b56bd4148087d5c97aa9297cc4bd7b37bd
Andreas Koepsel <andreas.koepsel@bisdn.de>
2013-10-14 12:27:03 +0200

    m4 SILENT_RULES added to configure.ac / tested QMF agent support in dptcore.py

--- tools/dptcore/dptcore.py
+++ tools/dptcore/dptcore.py
@@ -13,39 +13,34 @@ EVENT_QUIT = 0
     
 class DptCoreQmfAgentHandler(qmf2.AgentHandler):
     def __init__(self, agentSession):
+        qmf2.AgentHandler.__init__(self, agentSession)
         self.agentSession = agentSession
         self.__set_schema()
-        qmf2.AgentHandler.__init__(self, agentSession)
-
         self.dptcore = {}
-        self.dptcore['data'] = qmf2.Data(self.sch_node)
+        self.dptcore['data'] = qmf2.Data(self.sch_dptcore)
         self.dptcore['addr'] = self.agentSession.addData(self.dptcore['data'], 'dptcore')
 
         
     def __set_schema(self):
-        self.sch_node = qmf2.Schema(qmf2.SCHEMA_TYPE_DATA, "de.bisdn.homegw", "dptcore")
-        #self.sch_node.addProperty(qmf2.SchemaProperty("dptcoreid", qmf2.SCHEMA_DATA_INT))
-        self.agentSession.registerSchema(self.sch_node)
-        print self.sch_node
+        self.sch_dptcore = qmf2.Schema(qmf2.SCHEMA_TYPE_DATA, "de.bisdn.homegw", "dptcore")
+        #self.sch_dptcore.addProperty(qmf2.SchemaProperty("dptcoreid", qmf2.SCHEMA_DATA_INT))
+        self.sch_dptcore_testMethod = qmf2.SchemaMethod("test", desc='testing method for QMF agent support in python')
+        self.sch_dptcore_testMethod.addArgument(qmf2.SchemaProperty("subcmd", qmf2.SCHEMA_DATA_STRING, direction=qmf2.DIR_IN))
+        self.sch_dptcore_testMethod.addArgument(qmf2.SchemaProperty("outstring", qmf2.SCHEMA_DATA_STRING, direction=qmf2.DIR_OUT))
+        self.sch_dptcore.addMethod(self.sch_dptcore_testMethod)
+        self.agentSession.registerSchema(self.sch_dptcore)
+        print self.sch_dptcore
         
         
     def method(self, handle, methodName, args, subtypes, addr, userId):
         print methodName
-        pass
-        #...method handling code goes here...
-        #For success, add output arguments:
-        #    handle.addReturnArgument("argname", argvalue)
-        #    ...
-        #    self.agent.methodSuccess(handle)
-        #For failure, raise an exception:
-        #    self.agent.raiseException(handle, "error text")
-        #Or, if you have created a schema for a structured exception:
-        #    ex = qmf2.Data(exceptionSchema)
-        #    ex.whatHappened = "it failed"
-        #    ex.howBad = 84
-        #    ex.detailMap = {}
-        #    ...
-        #    self.agent.raiseException(handle, ex)
+        if methodName == "test":
+            handle.addReturnArgument("outstring", "an output string ...")
+            self.agentSession.methodSuccess(handle)
+        elif methodName == "blub":
+            self.agentSession.methodSuccess(handle)
+        else:
+            self.agentSession.raiseException(handle, "blubber")
 
 
 
@@ -76,32 +71,38 @@ class DptCore(object):
             raise
         
 
-
-
     def add_event(self, event):
         self.events.append(event)
         os.write(self.pipeout, str(event.type))
 
     
     def run(self):
+        # start QMF agent handler thread 
+        self.qmf['handler'].start()
+        
         self.ep = select.epoll()
         self.ep.register(self.pipein, select.EPOLLET | select.EPOLLIN)
                 
-        while True:
+        cont = True
+        while cont:
             try :
                 self.ep.poll(timeout=-1, maxevents=1)
                 evType = os.read(self.pipein, 256)
-                #print "epoll done: " + evType + "\n"
-                
+                                
                 for event in self.events:
                     if event.type == EVENT_QUIT:
-                        return
+                        cont = False
                 else:
                     self.events = []
             except KeyboardInterrupt:
                 self.add_event(DptCoreEvent(self, EVENT_QUIT))
 
-
+        # stop QMF agent handler thread 
+        self.qmf['handler'].cancel()
+        # close QMF AgentSession
+        self.qmf['session'].close()
+        # close QMF connection
+        self.qmf['connection'].close()
 
 
 if __name__ == "__main__":
--- tools/dptcore/dptcore_test.py
+++ tools/dptcore/dptcore_test.py
@@ -15,3 +15,20 @@ dptcores = sess.getObjects(_class="dptcore", _package="de.bisdn.homegw")
 
 for dptcore in dptcores:
 	print dptcore
+        props = dptcore.getProperties()
+        for prop in props:
+                print prop
+        stats = dptcore.getStatistics()
+        for stat in stats:
+                print stat
+        methods = dptcore.getMethods()
+        for method in methods:
+                print method
+
+
+if len(dptcores) == 0:
+	exit(0)
+
+
+dptcore = dptcores[0]
+print dptcore.test("blub")
