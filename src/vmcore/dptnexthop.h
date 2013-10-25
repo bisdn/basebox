ea20e67c8b16cee13df5b9cc1f5fbe5959832bdf
Andreas Koepsel <andreas.koepsel@bisdn.de>
2013-10-25 16:18:00 +0200

    clean-up debug output

--- src/vmcore/dptnexthop.h
+++ src/vmcore/dptnexthop.h
@@ -165,13 +165,13 @@ public:
 #endif
 		crtneigh& rtn = cnetlink::get_instance().get_link(neigh.ifindex).get_neigh(neigh.nbindex);
 
-		os << "<dptnexthop ";
+		os << "<dptnexthop: ";
 			os << rtn.get_dst() << " dev " << cnetlink::get_instance().get_link(neigh.ifindex).get_devname();
 			os << " lladdr " << rtn.get_lladdr() << " state " << rtn.get_state() << " ";
 			//os << "ifindex=" << neigh.ifindex << " ";
 			//os << "nbindex=" << (unsigned int)neigh.nbindex << " ";
 			//os << "ofportno=" << (unsigned int)neigh.of_port_no << " ";
-			os << "oftableid=" << (unsigned int)neigh.of_table_id << " ";
+			os << "oftableid: " << (unsigned int)neigh.of_table_id << " ";
 			//os << "dstaddr=" << neigh.dstaddr << " ";
 			//os << "dstmask=" << neigh.dstmask << " ";
 			//os << "flowentry=" << s_fe << " ";
