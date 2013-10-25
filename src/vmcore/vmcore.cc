2925bf19ca8ecbe352c1bc3270abfdffeb0b9480
Andreas Koepsel <andreas.koepsel@bisdn.de>
2013-10-25 16:28:52 +0200

    clean-up debug output

--- src/vmcore/vmcore.cc
+++ src/vmcore/vmcore.cc
@@ -289,8 +289,10 @@ vmcore::handle_packet_in(rofl::cofdpt *dpt, rofl::cofmsg_packet_in *msg)
 void
 vmcore::route_created(uint8_t table_id, unsigned int rtindex)
 {
+	std::cerr << "ROUTE ADD? " << cnetlink::get_instance().get_route(table_id, rtindex) << std::endl;
+
 	// ignore local route table and unspecified table_id
-	if ((255 == table_id) || (0 == table_id)) {
+	if ((RT_TABLE_LOCAL/*255*/ == table_id) || (RT_TABLE_UNSPEC/*0*/ == table_id)) {
 		std::cerr << "vmcore::route_created() => suppressing table_id=" << (unsigned int)table_id << std::endl;
 		return;
 	}
@@ -306,8 +308,10 @@ vmcore::route_created(uint8_t table_id, unsigned int rtindex)
 void
 vmcore::route_updated(uint8_t table_id, unsigned int rtindex)
 {
+	std::cerr << "ROUTE UPDATE? " << cnetlink::get_instance().get_route(table_id, rtindex) << std::endl;
+
 	// ignore local route table and unspecified table_id
-	if ((255 == table_id) || (0 == table_id)) {
+	if ((RT_TABLE_LOCAL/*255*/ == table_id) || (RT_TABLE_UNSPEC/*0*/ == table_id)) {
 		std::cerr << "vmcore::route_updated() => suppressing table_id=" << (unsigned int)table_id << std::endl;
 		return;
 	}
@@ -325,8 +329,10 @@ vmcore::route_updated(uint8_t table_id, unsigned int rtindex)
 void
 vmcore::route_deleted(uint8_t table_id, unsigned int rtindex)
 {
+	std::cerr << "ROUTE DELETE? " << cnetlink::get_instance().get_route(table_id, rtindex) << std::endl;
+
 	// ignore local route table and unspecified table_id
-	if ((255 == table_id) || (0 == table_id)) {
+	if ((RT_TABLE_LOCAL/*255*/ == table_id) || (RT_TABLE_UNSPEC/*0*/ == table_id)) {
 		std::cerr << "vmcore::route_deleted() => suppressing table_id=" << (unsigned int)table_id << std::endl;
 		return;
 	}
