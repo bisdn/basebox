738624cc095653b9f3004b602208cc1bcc67c016
Andreas Koepsel <andreas.koepsel@bisdn.de>
2013-11-07 12:09:40 +0100

    dhcpclient.py => revised handling of DHCP state machine via dhclient-enter-hooks script

--- tools/proact/common/dhcpclient.py
+++ tools/proact/common/dhcpclient.py
@@ -44,38 +44,47 @@ class DhcpClient(object):
             dhclient_cmd = self.dhclient_binary + ' -6 -q -P -1 -pf ' + self.pidfile + ' ' + self.devname
             print 'DHCP request: executing command => ' + str(dhclient_cmd.split())
             self.process = subprocess.Popen(dhclient_cmd.split(), shell=False, stdout=subprocess.PIPE)
-            rc = self.process.communicate()
+            resultstring = self.process.communicate()
             
             self.old_prefixes = []
             self.new_prefixes = []
-        
-            do_bind = False
-            
-            for line in rc[0].split('\n'):
-                try:      
-                    print 'REASON: ' + str(line.split()[0])              
-                    (self.type, self.reason, old_prefix, new_prefix) = line.split()
-                    s_old_prefix = IPv6Prefix(old_prefix.split('=')[1].split('/')[0], old_prefix.split('=')[1].split('/')[1])
-                    s_new_prefix = IPv6Prefix(new_prefix.split('=')[1].split('/')[0], new_prefix.split('=')[1].split('/')[1])
-                    if not s_old_prefix in self.old_prefixes:
-                        self.old_prefixes.append(s_old_prefix)
-                    if not s_new_prefix in self.new_prefixes:
-                        self.new_prefixes.append(s_new_prefix)
-                    print 'DHCP reply received: ' + str(self)
-                    if self.reason == 'BOUND6' or self.reason == 'REBIND6':
-                        do_bind = True
-                except ValueError:
+          
+            for line in resultstring[0].split('\n'):
+                elems = {}
+                for token in line.split():
+                        (key, value) = token.split('=')
+                        elems[key] = value
+                        print str(key) + '=' + str(elems[key])
+
+                self.reason = elems.get('reason', None)
+
+                if self.reason == 'PREINIT6':
                     pass
-                except IndexError:
+                elif self.reason == 'BOUND6':
+                    self.bind(elems)
+                elif self.reason == 'RENEW6':
+                    self.bind(elems)
+                elif self.reason == 'REBIND6':
+                    self.bind(elems)
+                elif self.reason == 'REBOOT':
+                    self.bind(elems)
+                elif self.reason == 'EXPIRE':
+                    self.expire(elems)
+                elif self.reason == 'FAIL':
+                    self.fail(elems)
+                elif self.reason == 'STOP':
+                    self.stop(elems)
+                elif self.reason == 'RELEASE':
+                    self.unbind(elems)
+                elif self.reason == 'NBI':
                     pass
-                        
-            if do_bind:
-                self.state = self.STATE_PREFIX_ATTACHED
-                self.baseCore.addEvent(basecore.BaseCoreEvent(self, basecore.BaseCore.EVENT_PREFIX_ATTACHED))
-            
-        except ValueError, e:
-            print 'unable to acquire prefixes ' + str(self)
-        
+                elif self.reason == 'TIMEOUT':
+                    self.timeout(elems)
+                else:
+                    print 'unknown reason ' + str(self.reason) + ' detected from dhclient'
+                    return
+        except:
+            print 'error executing dhclient'
         
     
     def sendRelease(self):
@@ -113,6 +122,45 @@ class DhcpClient(object):
         else:
             return False
 
+    def bind(self, elems):
+        try:
+            print 'binding DHCP reply: ' + str(self)
+            
+            reason = elems.get('reason', None)
+            new_prefix = elems.get('new_ip6_prefix', None)
+            old_prefix = elems.get('old_ip6_prefix', None)
+            s_old_prefix = IPv6Prefix(old_prefix.split('=')[1].split('/')[0], old_prefix.split('=')[1].split('/')[1])
+            s_new_prefix = IPv6Prefix(new_prefix.split('=')[1].split('/')[0], new_prefix.split('=')[1].split('/')[1])
+            if not s_old_prefix in self.old_prefixes:
+                self.old_prefixes.append(s_old_prefix)
+            if not s_new_prefix in self.new_prefixes:
+                self.new_prefixes.append(s_new_prefix)
+            
+            self.state = self.STATE_PREFIX_ATTACHED
+            self.baseCore.addEvent(basecore.BaseCoreEvent(self, basecore.BaseCore.EVENT_PREFIX_ATTACHED))
+        except IndexError:
+            pass
+        except ValueError, e:
+            print 'unable to acquire prefixes ' + str(self)
+
+    def expire(self, elems):
+        print 'DHCP session expired (renewal failed): ' + str(self)
+        self.unbind(elems)
+    
+    def fail(self, elems):
+        print 'DHCP request failed (no servers found): ' + str(self)
+        self.unbind(elems)
+
+    def stop(self, elems):
+        print 'DHCP session stop (dhclient graceful shutdown): ' + str(self)
+    
+    def unbind(self, elems):
+        self.state = self.STATE_PREFIX_DETACHED
+        self.baseCore.addEvent(basecore.BaseCoreEvent(self, basecore.BaseCore.EVENT_PREFIX_DETACHED))
+    
+    def timeout(self, elems):
+        print 'DHCP request failed (no servers found, but old lease available): ' + str(self)
+        self.bind(elems)
 
 
 if __name__ == "__main__":
