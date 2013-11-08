6e5d81902d75fa4cc8d1b344ead9864d86ce302c
Andreas Koepsel <andreas.koepsel@bisdn.de>
2013-11-08 12:20:23 +0100

    xdpdproxy.py => try/catch in __init__()

--- tools/proact/common/xdpdproxy.py
+++ tools/proact/common/xdpdproxy.py
@@ -35,12 +35,16 @@ class XdpdProxy(proact.common.qmfhelper.QmfConsole):
             print self.__getXdpdHandle()
         except:
             raise XdpdProxyException('unable to find xdpd instance with ID ' + self.xdpdID)
-        lsiHandles = self.getObjects(_class='lsi', _package='de.bisdn.xdpd')
-        for lsiHandle in lsiHandles:
-            if lsiHandle.xdpdID == self.xdpdID and lsiHandle.dpid == dpid:
-                self.xdpdLsiProxies[dpid] = XdpdLsiProxy(dpname=lsiHandle.dpname, dpid=lsiHandle.dpid, ofversion=lsiHandle.ofversion,
+        try:
+            lsiHandles = self.getObjects(_class='lsi', _package='de.bisdn.xdpd')
+            for lsiHandle in lsiHandles:
+                if lsiHandle.xdpdID == self.xdpdID and lsiHandle.dpid == dpid:
+                    self.xdpdLsiProxies[dpid] = XdpdLsiProxy(dpname=lsiHandle.dpname, dpid=lsiHandle.dpid, ofversion=lsiHandle.ofversion,
                                       ntables=lsiHandle.ntables, ctlaf=lsiHandle.ctlaf, ctladdr=lsiHandle.ctladdr,
                                       ctlport=lsiHandle.ctlport, reconnect=lsiHandle.reconnect)
+                    print 'XdpdProxy: added LSI ' + str(self.xdpdLsiProxies[dpid])
+        except ValueError:
+            pass
 
         
         
