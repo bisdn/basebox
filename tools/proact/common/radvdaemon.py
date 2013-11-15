36b70b0173d3287ff8cd3bb333f890d622cca2b7
Andreas Koepsel <andreas.koepsel@bisdn.de>
2013-11-15 19:21:21 +0100

    radvdaemon.py => logging

--- tools/proact/common/radvdaemon.py
+++ tools/proact/common/radvdaemon.py
@@ -30,14 +30,14 @@ class RAdvd(object):
     
     def addPrefix(self, prefix):
         if not isinstance(prefix, ipv6prefix.IPv6Prefix):
-            print "not of type IPv6Prefix, ignoring"
+            self.baseCore.logger.warn('not of type IPv6Prefix, ignoring')
             return
         if not prefix in self.prefixes:
             self.prefixes.append(prefix)
         
     def delPrefix(self, prefix):
         if not isinstance(prefix, ipv6prefix.IPv6Prefix):
-            print "not of type IPv6Prefix, ignoring"
+            self.baseCore.logger.warn('not of type IPv6Prefix, ignoring')
             return
         if prefix in self.prefixes:
             self.prefixes.remove(prefix)
@@ -47,11 +47,11 @@ class RAdvd(object):
             self.stop()
         self.__rebuild_config()
         if len(self.prefixes) == 0:
-            print 'no prefixes available for radvd, suppressing start ' + str(self)
+            self.baseCore.logger.warn('no prefixes available for radvd, suppressing start ' + str(self))
             return
         self.state = self.STATE_ANNOUNCING
         radvd_cmd = self.radvd_binary + ' -C ' + self.conffile + ' -p ' + self.pidfile
-        print 'radvd start: executing command => ' + str(radvd_cmd)
+        self.baseCore.logger.debug('radvd start: executing command => ' + str(radvd_cmd))
         self.process = subprocess.Popen(radvd_cmd.split())
         self.baseCore.addEvent(basecore.BaseCoreEvent(self, basecore.BaseCore.EVENT_RADVD_START))
     
@@ -60,7 +60,7 @@ class RAdvd(object):
         if self.process == None:
             return
         kill_cmd = 'kill -INT ' + str(self.process.pid)
-        print 'radvd stop: executing command => ' + str(kill_cmd)
+        self.baseCore.logger.debug('radvd stop: executing command => ' + str(kill_cmd))
         subprocess.call(kill_cmd.split())
         self.process = None
         self.baseCore.addEvent(basecore.BaseCoreEvent(self, basecore.BaseCore.EVENT_RADVD_STOP))
