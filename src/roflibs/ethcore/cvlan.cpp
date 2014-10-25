19caeab7d04770326d50da559f9f340db20bb94d
Andreas Koepsel <andreas.koepsel@bisdn.de>
2014-10-25 18:13:20 +0200

    clean-up

--- src/roflibs/ethcore/cvlan.cpp
+++ src/roflibs/ethcore/cvlan.cpp
@@ -36,7 +36,7 @@ cvlan::handle_dpt_open(
 		// set broadcast entry in src stage
 		fm.set_table_id(table_id_eth_src);
 		fm.set_priority(0x8100);
-		fm.set_match().set_eth_dst(rofl::caddress_ll("ff:ff:ff:ff:ff:ff"), rofl::caddress_ll("01:00:00:00:00:00"));
+		fm.set_match().set_eth_dst(rofl::caddress_ll("01:00:00:00:00:00"), rofl::caddress_ll("01:00:00:00:00:00"));
 		fm.set_instructions().set_inst_goto_table().set_table_id(table_id_eth_local);
 		dpt.send_flow_mod_message(rofl::cauxid(0), fm);
 
@@ -105,7 +105,7 @@ cvlan::handle_dpt_close(
 		// remove broadcast entry in src stage
 		fm.set_table_id(table_id_eth_src);
 		fm.set_priority(0x8100);
-		fm.set_match().set_eth_dst(rofl::caddress_ll("ff:ff:ff:ff:ff:ff"), rofl::caddress_ll("01:00:00:00:00:00"));
+		fm.set_match().set_eth_dst(rofl::caddress_ll("01:00:00:00:00:00"), rofl::caddress_ll("01:00:00:00:00:00"));
 		dpt.send_flow_mod_message(rofl::cauxid(0), fm);
 
 		// remove flooding group entry itself
