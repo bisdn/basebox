77b686d3d074f3415efdfaa243c884fa9cca60fd
Andreas Koepsel <andreas.koepsel@bisdn.de>
2014-11-20 14:37:59 +0100

    ccookiebox => added output operator

--- src/roflibs/netlink/ccookiebox.hpp
+++ src/roflibs/netlink/ccookiebox.hpp
@@ -10,6 +10,8 @@
 
 #include <inttypes.h>
 
+#include <ostream>
+
 #include <rofl/common/crofdpt.h>
 #include <rofl/common/cauxid.h>
 #include <rofl/common/crandom.h>
@@ -111,7 +113,7 @@ public:
 		if (cookiestore.find(msg.get_cookie()) == cookiestore.end()) {
 			rofcore::logging::debug << "[ccookiebox][handle_packet_in] cookie: 0x"
 					<< std::hex << (unsigned long long)msg.get_cookie() << std::dec
-					<< " not found, dropping packet" << std::endl;
+					<< " not found, dropping packet" << std::endl << *this;
 			return;
 		}
 		cookiestore[msg.get_cookie()]->handle_packet_in(dpt, auxid, msg);
@@ -126,7 +128,7 @@ public:
 		if (cookiestore.find(msg.get_cookie()) == cookiestore.end()) {
 			rofcore::logging::debug << "[ccookiebox][handle_flow_removed] cookie: 0x"
 					<< std::hex << (unsigned long long)msg.get_cookie() << std::dec
-					<< " not found, dropping packet" << std::endl;
+					<< " not found, dropping packet" << std::endl << *this;
 			return;
 		}
 		cookiestore[msg.get_cookie()]->handle_flow_removed(dpt, auxid, msg);
@@ -172,6 +174,20 @@ private:
 			return;
 		}
 		cookiestore.erase(cookie);
+	};
+
+public:
+
+	friend std::ostream&
+	operator<< (std::ostream& os, const ccookiebox& box) {
+		os << rofcore::indent(0) << "<ccookiebox >" << std::endl;
+		for (std::map<uint64_t, ccookie_owner*>::const_iterator
+				it = box.cookiestore.begin(); it != box.cookiestore.end(); ++it) {
+			os << rofcore::indent(2) << "cookie: 0x"
+					<< std::hex << (unsigned long long)it->first << std::dec << " => 0x"
+					<< std::hex << (unsigned long)it->second << std::dec << std::endl;
+		}
+		return os;
 	};
 };
 
