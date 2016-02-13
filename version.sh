c4089d91cd58098f3f12e7ff29a02e6ad69cbd42
Tobias Jungel <tobias.jungel@bisdn.de>
2016-02-13 15:06:53 +0100

    improved version handling
    version taken from git; dist version from version file

---
+++ version.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ -x $(which git) -a -d .git ]
+then
+	VERSION=$(git describe  --dirty --always --tags)
+	echo ${VERSION} > VERSION
+elif [ -f VERSION ]
+then
+	VERSION=$(cat VERSION)
+else
+	exit 1
+fi
+
+echo ${VERSION} | tr -d '\n' 
