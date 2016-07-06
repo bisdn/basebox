ca3d68bd11bed7848b3c708b7b57fca551a7490d
Tobias Jungel <tobias.jungel@bisdn.de>
2016-07-06 14:12:25 +0200

    tap_manager: move pragma in header

--- src/roflibs/netlink/tap_manager.hpp
+++ src/roflibs/netlink/tap_manager.hpp
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#pragma once
+
 #include <deque>
 #include <string>
 #include <vector>
@@ -10,8 +12,6 @@
 #include <rofl/common/cpacket.h>
 
 #include "roflibs/netlink/ctapdev.hpp"
-
-#pragma once
 
 namespace rofcore {
 
