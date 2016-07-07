6b7791d1126040be9acbcaff4ba216ed1cdadafe
Tobias Jungel <tobias.jungel@bisdn.de>
2016-07-07 13:06:38 +0200

    cbasebox: move to roflibs of-dpa

--- src/baseboxd/baseboxd.cpp
+++ src/baseboxd/baseboxd.cpp
@@ -2,9 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "cbasebox.hpp"
 #include "cunixenv.hpp"
 #include "roflibs/netlink/clogging.hpp"
+#include "roflibs/of-dpa/cbasebox.hpp"
 
 int main(int argc, char **argv) {
   using rofcore::logging;
