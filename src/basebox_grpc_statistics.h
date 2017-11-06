d816f97484dd218375cd5ff3f369a442b6e17f13
Rubens Figueiredo <rubens.figueiredo@bisdn.de>
2017-11-06 10:21:16 +0100

    fix for the comments

--- src/basebox_grpc_statistics.h
+++ src/basebox_grpc_statistics.h
@@ -5,15 +5,13 @@
 #include "rofl/common/openflow/cofportstats.h"
 
 using namespace openconfig_interfaces;
-using namespace grpc;
-using ::google::protobuf::RepeatedPtrField;
 
 class NetworkStats final : public ::api::NetworkStatistics::Service {
 public:
   typedef ::api::Empty Empty;
   NetworkStats() {}
-  Status GetStatistics(ServerContext *context, const Empty *request,
-                       ServerWriter<Interfaces_Interface> *writer);
+  ::grpc::Status GetStatistics(::grpc::ServerContext *context, const Empty *request,
+                       ::grpc::ServerWriter<Interfaces_Interface> *writer);
   void addStatistics(
       const std::string &nodeInfo,
       const std::list<
