d77dc6400e51ee8e4789f86e25e6d1769f6f06ac
Tobias Jungel <tobias.jungel@bisdn.de>
2017-11-09 00:04:22 +0100

    remove topology

--- src/basebox_api.cc
+++ src/basebox_api.cc
@@ -1,48 +1,32 @@
 #include "basebox_api.h"
+#include "basebox_grpc_statistics.h"
+
 #include <glog/logging.h>
+#include <google/protobuf/repeated_field.h>
+#include <grpc++/channel.h>
+#include <grpc++/security/server_credentials.h>
+#include <grpc++/server_builder.h>
+#include <grpc++/server_context.h>
+#include <grpc/grpc.h>
 
 namespace basebox {
+
+ApiServer::ApiServer() : stats(new NetworkStats()) {}
+ApiServer::~ApiServer() { delete stats; }
 
 void ApiServer::runGRPCServer() {
   std::string server_address("0.0.0.0:5000");
   ::grpc::ServerBuilder builder;
 
   builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-  builder.RegisterService(topology);
   builder.RegisterService(stats);
   server = builder.BuildAndStart();
   LOG(INFO) << "gRPC server listening on " << server_address;
   server->Wait();
 }
 
-int ApiServer::addNode(
-    const std::string &nodeInfo,
-    const std::list<std::pair<std::string, rofl::openflow::cofport_stats_reply>>
-        &ports) {
-
-  topology->addNode(nodeInfo, ports);
-  stats->addStatistics(nodeInfo, ports);
-
-  return 0;
-}
-
-int ApiServer::addLink(const std::string &nodeSrc, const std::string &portSrc,
-                       const std::string &nodeDst, const std::string &portDst) {
-  topology->addLink(nodeSrc, portSrc, nodeDst, portDst);
-  return 0;
-}
-
 void ApiServer::initStructures() {
-    topology = new NetworkImpl();
     stats = new NetworkStats();
-
-    addNode("", {});
-    addLink("", "", "", "");
-}
-
-void ApiServer::flush() {
-  topology->flush();
-  stats->flush();
 }
 
 } // namespace basebox
