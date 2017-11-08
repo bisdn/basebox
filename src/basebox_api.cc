dc563de8183b6b8e1d44d496d56628fa093fd626
Rubens Figueiredo <rubens.figueiredo@bisdn.de>
2017-11-08 12:31:23 +0100

    Added gRPC functionality

---
+++ src/basebox_api.cc
@@ -0,0 +1,44 @@
+#include "basebox_api.h"
+#include <glog/logging.h>
+
+void ApiServer::runGRPCServer() {
+  std::string server_address("0.0.0.0:5000");
+  ::grpc::ServerBuilder builder;
+
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(topology);
+  builder.RegisterService(stats);
+  server = builder.BuildAndStart();
+  LOG(INFO) << "gRPC server listening on " << server_address;
+  server->Wait();
+}
+
+int ApiServer::addNode(
+    const std::string &nodeInfo,
+    const std::list<std::pair<std::string, rofl::openflow::cofport_stats_reply>>
+        &ports) {
+
+  topology->addNode(nodeInfo, ports);
+  stats->addStatistics(nodeInfo, ports);
+
+  return 0;
+}
+
+int ApiServer::addLink(const std::string &nodeSrc, const std::string &portSrc,
+                       const std::string &nodeDst, const std::string &portDst) {
+  topology->addLink(nodeSrc, portSrc, nodeDst, portDst);
+  return 0;
+}
+
+void ApiServer::initStructures() {
+    topology = new NetworkImpl();
+    stats = new NetworkStats();
+
+    addNode("", {});
+    addLink("", "", "", "");
+}
+
+void ApiServer::flush() {
+  topology->flush();
+  stats->flush();
+}
