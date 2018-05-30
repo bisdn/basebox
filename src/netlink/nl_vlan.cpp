4d6891e701574205c2a16fbe6da53d1ec2133e99
Rubens Figueiredo <r.figueiredo.52@gmail.com>
2018-05-30 13:03:03 +0200

    fix comments; ready for merge

--- src/netlink/nl_vlan.cpp
+++ src/netlink/nl_vlan.cpp
@@ -113,7 +113,7 @@ uint16_t nl_vlan::get_vid(rtnl_link *link) {
   auto lt = kind_to_link_type(rtnl_link_get_type(link));
   switch (lt) {
   case LT_BRIDGE:
-    LOG(INFO) << __FUNCTION__ << ": bridge default vid " << default_vid;
+    VLOG(2) << __FUNCTION__ << ": bridge default vid " << default_vid;
     vid = default_vid;
     break;
   case LT_TUN:
