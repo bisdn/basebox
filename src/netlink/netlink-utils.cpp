bisdn/basebox

commit b31c17cb67f8f97c190397a47993530a5a269a67

bond: add single port bond wrapper
* update link_type handling
* handle single bond interface

src/netlink/netlink-utils.cpp
@@ -5,14 +5,18 @@
 #include <cassert>
 #include <map>
 #include <memory>
+#include <string_view>
 #include <vector>
+#include <cstring>
 
 #include <glog/logging.h>
 #include <netlink/route/link.h>
 
 #include "netlink-utils.hpp"
+#include "nl_output.hpp"
 
-#define lt_names "unknown", "unsupported", "bridge", "tun", "vlan"
+#define lt_names                                                               \
+  "unsupported", "bond", "bond_slave", "bridge", "bridge_slave", "tun", "vlan"
 
 namespace basebox {
 
@@ -20,7 +24,7 @@ std::map<std::string, enum link_type> kind2lt;
 std::vector<std::string> lt2names = {lt_names};
 
 static void init_kind2lt_map() {
-  enum link_type lt = LT_UNKNOWN;
+  enum link_type lt = LT_UNSUPPORTED;
   for (const auto &n : lt2names) {
     assert(lt != LT_MAX);
     kind2lt.emplace(n, lt);
@@ -28,22 +32,52 @@ static void init_kind2lt_map() {
   }
 }
 
-enum link_type kind_to_link_type(const char *type) noexcept {
-  if (type == nullptr)
-    return LT_UNKNOWN;
+enum link_type get_link_type(rtnl_link *link) noexcept {
+  const char *type;
+  bool slave = false;
 
+  assert(link);
   assert(lt2names.size() == LT_MAX);
 
   if (kind2lt.size() == 0) {
     init_kind2lt_map();
   }
 
-  auto it = kind2lt.find(std::string(type)); // XXX string_view
+  if (rtnl_link_get_master(link)) {
+    // slave device
+    type = rtnl_link_get_slave_type(link);
+    slave = true;
 
-  if (it != kind2lt.end())
-    return it->second;
+    if (type == nullptr && rtnl_link_get_family(link) == AF_BRIDGE) {
+      // libnl bug or kernel adds an additional link?
+      return LT_BRIDGE_SLAVE;
+    }
 
-  VLOG(1) << __FUNCTION__ << ": type=" << type << " not supported";
+  } else {
+    type = rtnl_link_get_type(link);
+  }
+
+  VLOG(2) << __FUNCTION__ << ": type=" << std::string_view(type)
+          << ", af=" << rtnl_link_get_family(link) << ", slave=" << slave
+          << " of link " << OBJ_CAST(link);
+
+  // lo has no type
+  if (!type) {
+    return LT_UNSUPPORTED;
+  }
+
+  auto it = kind2lt.find(std::string(type));
+
+  if (it != kind2lt.end()) {
+    enum link_type lt = it->second;
+
+    if (slave)
+      lt = static_cast<enum link_type>(lt + 1);
+    return lt;
+  }
+
+  VLOG(1) << __FUNCTION__ << ": type=" << std::string_view(type)
+          << " not supported";
   return LT_UNSUPPORTED;
 }
 

meson.build
@@ -1,7 +1,7 @@
 project('baseboxd', 'cpp',
   version: run_command('cat', 'VERSION').stdout().strip(),
   default_options : [
-    'cpp_std=c++11',
+    'cpp_std=c++17',
     'b_asneeded=false',
     'prefix=/usr',
     'sysconfdir=/etc',
@@ -21,6 +21,8 @@ sources = files('''
   src/netlink/nbi_impl.hpp
   src/netlink/netlink-utils.cpp
   src/netlink/netlink-utils.hpp
+  src/netlink/nl_bond.cpp
+  src/netlink/nl_bond.hpp
   src/netlink/nl_bridge.cpp
   src/netlink/nl_bridge.hpp
   src/netlink/nl_hashing.hpp

src/netlink/cnetlink.cpp
@@ -8,6 +8,7 @@
 #include <fstream>
 #include <glog/logging.h>
 #include <iterator>
+#include <string_view>
 
 #include <sys/socket.h>
 #include <linux/if.h>
@@ -15,6 +16,7 @@
 #include <netlink/route/addr.h>
 #include <netlink/route/link.h>
 #include <netlink/route/link/vlan.h>
+#include <netlink/route/link/bonding.h>
 #include <netlink/route/neighbour.h>
 #include <netlink/route/route.h>
 
@@ -23,6 +25,7 @@
 #include "nl_output.hpp"
 #include "tap_manager.hpp"
 
+#include "nl_bond.hpp"
 #include "nl_l3.hpp"
 #include "nl_vlan.hpp"
 
@@ -31,7 +34,8 @@ namespace basebox {
 cnetlink::cnetlink()
     : swi(nullptr), thread(1), caches(NL_MAX_CACHE, nullptr), nl_proc_max(10),
       running(false), rfd_scheduled(false), bridge(nullptr),
-      vlan(new nl_vlan(this)), l3(new nl_l3(vlan, this)) {
+      bond(new nl_bond(this)), vlan(new nl_vlan(this)),
+      l3(new nl_l3(vlan, this)) {
 
   sock_tx = nl_socket_alloc();
   if (sock_tx == nullptr) {
@@ -277,7 +281,7 @@ bool cnetlink::is_bridge_interface(rtnl_link *l) const {
     if (_l == nullptr)
       return false;
 
-    auto lt = kind_to_link_type(rtnl_link_get_type(_l));
+    auto lt = get_link_type(_l);
 
     LOG(INFO) << __FUNCTION__ << ": lt=" << lt << " " << OBJ_CAST(_l);
     if (lt == LT_BRIDGE) {
@@ -287,7 +291,7 @@ bool cnetlink::is_bridge_interface(rtnl_link *l) const {
       get_bridge_ports(rtnl_link_get_ifindex(_l), &bridge_interfaces);
 
       for (auto br_intf : bridge_interfaces) {
-        if (tap_man->get_port_id(rtnl_link_get_ifindex(br_intf)) != 0)
+        if (get_port_id(rtnl_link_get_ifindex(br_intf)) != 0)
           return true;
       }
     }
@@ -309,6 +313,10 @@ int cnetlink::get_port_id(rtnl_link *l) const {
 
   if (rtnl_link_is_vlan(l)) {
     ifindex = rtnl_link_get_link(l);
+  } else if (rtnl_link_get_type(l) &&
+             (0 == strcmp(rtnl_link_get_type(l), "bond") ||
+              0 == strcmp(rtnl_link_get_type(l), "team"))) {
+    return bond->get_lag_id(l);
   } else {
     ifindex = rtnl_link_get_ifindex(l);
   }
@@ -801,48 +809,39 @@ void cnetlink::link_created(rtnl_link *link) noexcept {
   assert(link);
   assert(tap_man);
 
-  enum link_type lt = kind_to_link_type(rtnl_link_get_type(link));
-  int af = rtnl_link_get_family(link);
+  enum link_type lt = get_link_type(link);
 
   switch (lt) {
-  case LT_UNKNOWN:
-    switch (af) {
-    case AF_BRIDGE: { // a new bridge slave was created
+  case LT_BRIDGE_SLAVE: { // a new bridge slave was created
 
-      try {
-        // check for new bridge slaves
-        if (rtnl_link_get_master(link) == 0) {
-          LOG(ERROR) << __FUNCTION__ << ": unknown link " << OBJ_CAST(link);
-          return;
-        }
-
-        // slave interface
-        // use only the first bridge an interface is attached to
-        // XXX TODO more bridges!
-        if (bridge == nullptr) {
-          LOG(INFO) << __FUNCTION__ << ": using bridge "
-                    << rtnl_link_get_master(link);
-          bridge = new nl_bridge(this->swi, tap_man, this);
-          std::unique_ptr<rtnl_link, decltype(&rtnl_link_put)> br_link(
-              rtnl_link_get(caches[NL_LINK_CACHE], rtnl_link_get_master(link)),
-              rtnl_link_put);
-          bridge->set_bridge_interface(br_link.get());
-        }
-
-        LOG(INFO) << __FUNCTION__ << ": enslaving interface "
-                  << rtnl_link_get_name(link);
-
-        bridge->add_interface(link);
-      } catch (std::exception &e) {
-        LOG(ERROR) << __FUNCTION__ << ": failed: " << e.what();
+    try {
+      // check for new bridge slaves
+      if (rtnl_link_get_master(link) == 0) {
+        LOG(ERROR) << __FUNCTION__ << ": unknown link " << OBJ_CAST(link);
+        return;
       }
-    } break;
-    default:
-      LOG(WARNING) << __FUNCTION__ << ": ignoring link with af=" << af
-                   << " link:" << link;
-      break;
-    } // switch familiy
-    break;
+
+      // slave interface
+      // use only the first bridge an interface is attached to
+      // XXX TODO more bridges!
+      if (bridge == nullptr) {
+        LOG(INFO) << __FUNCTION__ << ": using bridge "
+                  << rtnl_link_get_master(link);
+        bridge = new nl_bridge(this->swi, tap_man, this);
+        std::unique_ptr<rtnl_link, decltype(&rtnl_link_put)> br_link(
+            rtnl_link_get(caches[NL_LINK_CACHE], rtnl_link_get_master(link)),
+            rtnl_link_put);
+        bridge->set_bridge_interface(br_link.get());
+      }
+
+      LOG(INFO) << __FUNCTION__ << ": enslaving interface "
+                << rtnl_link_get_name(link);
+
+      bridge->add_interface(link);
+    } catch (std::exception &e) {
+      LOG(ERROR) << __FUNCTION__ << ": failed: " << e.what();
+    }
+  } break;
   case LT_TUN: {
     int ifindex = rtnl_link_get_ifindex(link);
     std::string name(rtnl_link_get_name(link));
@@ -853,6 +852,10 @@ void cnetlink::link_created(rtnl_link *link) noexcept {
     uint16_t vid = rtnl_link_vlan_get_id(link);
     vlan->add_vlan(link, vid, true);
   } break;
+  case LT_BOND: {
+    VLOG(1) << __FUNCTION__ << ": new bond interface " << OBJ_CAST(link);
+    bond->add_lag(link);
+  } break;
   default:
     LOG(WARNING) << __FUNCTION__ << ": ignoring link with lt=" << lt
                  << " link:" << link;
@@ -861,58 +864,56 @@ void cnetlink::link_created(rtnl_link *link) noexcept {
 }
 
 void cnetlink::link_updated(rtnl_link *old_link, rtnl_link *new_link) noexcept {
-
-  link_type lt_old = kind_to_link_type(rtnl_link_get_type(old_link));
-  link_type lt_new = kind_to_link_type(rtnl_link_get_type(new_link));
-
+  link_type lt_old = get_link_type(old_link);
+  link_type lt_new = get_link_type(new_link);
   int af_old = rtnl_link_get_family(old_link);
   int af_new = rtnl_link_get_family(new_link);
 
-  if (lt_old != lt_new) {
-    VLOG(1) << __FUNCTION__ << ": link type changed from " << lt_old << " to "
-            << lt_new;
-    // this is currently handled elsewhere
-    return;
-  }
+  VLOG(3) << __FUNCTION__ << ": old_link_type="
+          << std::string_view(rtnl_link_get_type(old_link))
+          << ", new_link_type="
+          << std::string_view(rtnl_link_get_type(new_link))
+          << ", old_link_slave_type="
+          << std::string_view(rtnl_link_get_slave_type(old_link))
+          << ", new_link_slave_type="
+          << std::string_view(rtnl_link_get_slave_type(new_link))
+          << ", af_old=" << af_old << ", af_new=" << af_new;
 
   if (af_old != af_new) {
     VLOG(1) << __FUNCTION__ << ": af changed from " << af_old << " to "
             << af_new;
     // this is currently handled elsewhere
-    return;
-  }
-
-  // AF_UNSPEC changes are not yet supported for links
-  if (rtnl_link_get_family(new_link) == AF_UNSPEC) {
-    // XXX TODO handle ifup/ifdown...
+    // the for us interesting case is the bridge port creation
     return;
   }
 
   switch (lt_old) {
-  case LT_UNKNOWN:
-    switch (af_old) {
-
-    case AF_BRIDGE: { // a bridge slave was changed
-      if (bridge) {
-        try {
+  case LT_BRIDGE_SLAVE: // a bridge slave was changed
+    if (bridge) {
+      try {
+        if (rtnl_link_get_family(old_link) == AF_BRIDGE &&
+            rtnl_link_get_family(new_link) == AF_BRIDGE)
           bridge->update_interface(old_link, new_link);
-        } catch (std::exception &e) {
-          LOG(ERROR) << __FUNCTION__ << ": failed: " << e.what();
-        }
+      } catch (std::exception &e) {
+        LOG(ERROR) << __FUNCTION__ << ": failed: " << e.what();
       }
-
-    } break;
-    default:
-      LOG(ERROR) << __FUNCTION__ << ": AF not handled " << af_old;
-      break;
     }
     break;
-  case LT_VLAN: {
-    VLOG(1) << __FUNCTION__ << ": ignoring vlan interface update";
-  } break;
-  case LT_BRIDGE: {
-    VLOG(1) << __FUNCTION__ << ": ignoring bridge update";
-  } break;
+  case LT_TUN:
+    if (lt_new == LT_BOND_SLAVE) {
+      // XXX link enslaved
+      LOG(INFO) << __FUNCTION__ << ": link enslaved";
+      rtnl_link *_bond = get_link(rtnl_link_get_master(new_link), AF_UNSPEC);
+      bond->add_lag_member(_bond, new_link);
+      break;
+    }
+    /* fallthrough */
+  case LT_VLAN:
+  case LT_BOND:
+  case LT_BRIDGE:
+  case LT_UNSUPPORTED:
+    VLOG(1) << __FUNCTION__ << ": ignoring update of lt=" << lt_old;
+    break;
   default:
     LOG(ERROR) << __FUNCTION__ << ": link type not handled " << lt_old;
     break;
@@ -921,27 +922,21 @@ void cnetlink::link_updated(rtnl_link *old_link, rtnl_link *new_link) noexcept {
 
 void cnetlink::link_deleted(rtnl_link *link) noexcept {
   assert(link);
-  enum link_type lt = kind_to_link_type(rtnl_link_get_type(link));
-  int af = rtnl_link_get_family(link);
+
+  enum link_type lt = get_link_type(link);
 
   int ifindex(rtnl_link_get_ifindex(link));
   std::string portname(rtnl_link_get_name(link));
 
   switch (lt) {
-  case LT_UNKNOWN:
-    switch (af) {
-    case AF_BRIDGE:
-      try {
-        if (bridge) {
+  case LT_BRIDGE_SLAVE:
+    try {
+      if (bridge) {
+        if (rtnl_link_get_family(link) == AF_BRIDGE)
           bridge->delete_interface(link);
-        }
-      } catch (std::exception &e) {
-        LOG(ERROR) << __FUNCTION__ << ": failed: " << e.what();
       }
-      break;
-    default:
-      LOG(ERROR) << __FUNCTION__ << ": AF not handled " << af;
-      break;
+    } catch (std::exception &e) {
+      LOG(ERROR) << __FUNCTION__ << ": failed: " << e.what();
     }
     break;
   case LT_TUN:
@@ -958,6 +953,10 @@ void cnetlink::link_deleted(rtnl_link *link) noexcept {
     VLOG(1) << __FUNCTION__ << ": removed vlan interface " << OBJ_CAST(link);
     vlan->remove_vlan(link, rtnl_link_vlan_get_id(link), true);
     break;
+  case LT_BOND: {
+    VLOG(1) << __FUNCTION__ << ": removed bond interface " << OBJ_CAST(link);
+    bond->remove_lag(link);
+  } break;
   default:
     LOG(ERROR) << __FUNCTION__ << ": link type not handled " << lt;
     break;
@@ -1045,6 +1044,7 @@ void cnetlink::register_switch(switch_interface *swi) noexcept {
   this->swi = swi;
   l3->register_switch_interface(swi);
   vlan->register_switch_interface(swi);
+  bond->register_switch_interface(swi);
 
   swi->subscribe_to(switch_interface::SWIF_ARP);
 }
@@ -1073,6 +1073,8 @@ int cnetlink::handle_port_status_events() {
   std::deque<std::tuple<uint32_t, enum nbi::port_status, int>> _pc_changes;
   std::deque<std::tuple<uint32_t, enum nbi::port_status, int>> _pc_retry;
 
+  VLOG(1) << __FUNCTION__;
+
   {
     std::lock_guard<std::mutex> scoped_lock(pc_mutex);
     _pc_changes.swap(port_status_changes);
@@ -1094,6 +1096,7 @@ int cnetlink::handle_port_status_events() {
         LOG(ERROR) << __FUNCTION__
                    << ": no ifindex of port_id=" << std::get<0>(change)
                    << " found";
+        // XXX TODO resync caches?
       }
       continue;
     }

src/netlink/cnetlink.hpp
@@ -19,6 +19,7 @@
 namespace basebox {
 
 // forward declaration
+class nl_bond;
 class nl_l3;
 class nl_vlan;
 class tap_manager;
@@ -110,6 +111,7 @@ private:
 
   std::shared_ptr<tap_manager> tap_man;
   nl_bridge *bridge;
+  std::shared_ptr<nl_bond> bond;
   std::shared_ptr<nl_vlan> vlan;
   std::shared_ptr<nl_l3> l3;
 

src/netlink/netlink-utils.hpp
@@ -23,14 +23,16 @@ struct rtnl_route;
 namespace basebox {
 
 enum link_type {
-  LT_UNKNOWN = 0,
   LT_UNSUPPORTED,
+  LT_BOND,
+  LT_BOND_SLAVE,
   LT_BRIDGE,
+  LT_BRIDGE_SLAVE,
   LT_TUN,
   LT_VLAN,
   LT_MAX /* must be last */
 };
 
-enum link_type kind_to_link_type(const char *type) noexcept;
+enum link_type get_link_type(rtnl_link *type) noexcept;
 
 } // namespace basebox

src/netlink/nl_bond.cpp
@@ -1,0 +1,135 @@
+#include "nl_bond.hpp"
+
+#include <cassert>
+#include <glog/logging.h>
+#include <netlink/route/link.h>
+
+#include "cnetlink.hpp"
+#include "nl_output.hpp"
+#include "sai.hpp"
+
+namespace basebox {
+
+nl_bond::nl_bond(cnetlink *nl) : swi(nullptr), nl(nl) {}
+
+uint32_t nl_bond::get_lag_id(rtnl_link *bond) {
+  assert(bond);
+
+  auto it = ifi2lag.find(rtnl_link_get_ifindex(bond));
+  if (it == ifi2lag.end()) {
+    LOG(ERROR) << __FUNCTION__ << ": lag_id not found of lag "
+               << OBJ_CAST(bond);
+    return 0;
+  }
+
+  // XXX currently we do not deal with a real lag since we just deal with
+  // a single port in the lag. Thus we just mimic the lag behaviour and map
+  // everything on the linux bond/team to the single physical port
+  auto rv_lm = lag_members.find(it->second);
+  if (rv_lm == lag_members.end()) {
+    VLOG(1) << ": no members in lag " << OBJ_CAST(bond);
+    return 0;
+  }
+
+  return rv_lm->second;
+}
+
+int nl_bond::add_lag(rtnl_link *bond) {
+  uint32_t lag_id = 0;
+  int rv = 0;
+
+  assert(bond);
+  rv = swi->lag_create(&lag_id);
+  if (rv < 0) {
+    LOG(ERROR) << __FUNCTION__ << ": failed to create lag for "
+               << OBJ_CAST(bond);
+    return rv;
+  }
+
+  auto rv_emp =
+      ifi2lag.emplace(std::make_pair(rtnl_link_get_ifindex(bond), lag_id));
+
+  if (!rv_emp.second) {
+    LOG(WARNING) << __FUNCTION__
+                 << ": lag exists with lag_id=" << rv_emp.first->second
+                 << " for bond " << OBJ_CAST(bond);
+    return -EEXIST;
+  }
+
+  return rv;
+}
+
+int nl_bond::remove_lag(rtnl_link *bond) {
+  int rv = 0;
+  auto it = ifi2lag.find(rtnl_link_get_ifindex(bond));
+
+  if (it == ifi2lag.end()) {
+    LOG(WARNING) << __FUNCTION__ << ": lag does not exist for "
+                 << OBJ_CAST(bond);
+    return -ENODEV;
+  }
+
+  rv = swi->lag_remove(it->second);
+  if (rv < 0) {
+    LOG(ERROR) << __FUNCTION__
+               << ": failed to remove lag with lag_id=" << it->second
+               << " for bond " << OBJ_CAST(bond);
+    return rv;
+  }
+
+  ifi2lag.erase(it);
+
+  return 0;
+}
+
+int nl_bond::add_lag_member(rtnl_link *bond, rtnl_link *link) {
+  int rv = 0;
+  auto it = ifi2lag.find(rtnl_link_get_ifindex(bond));
+  if (it == ifi2lag.end()) {
+    LOG(ERROR) << __FUNCTION__ << ": no lag_id found for " << OBJ_CAST(bond);
+    return -EINVAL;
+  }
+
+  uint32_t port_id = nl->get_port_id(link);
+  if (port_id == 0) {
+    VLOG(1) << __FUNCTION__ << ": ignoring port " << OBJ_CAST(link);
+    return -EINVAL;
+  }
+
+  auto lm_rv = lag_members.emplace(it->second, port_id);
+  if (!lm_rv.second) {
+    LOG(ERROR) << __FUNCTION__ << ": cannot add multiple ports to a lag";
+  }
+
+  swi->lag_add_member(it->second, port_id);
+
+  return rv;
+}
+
+int nl_bond::remove_lag_member(rtnl_link *bond, rtnl_link *link) {
+  int rv = 0;
+  auto it = ifi2lag.find(rtnl_link_get_ifindex(bond));
+  if (it == ifi2lag.end()) {
+    LOG(FATAL) << __FUNCTION__ << ": no lag_id found for " << OBJ_CAST(bond);
+  }
+
+  uint32_t port_id = nl->get_port_id(link);
+  if (port_id == 0) {
+    VLOG(1) << __FUNCTION__ << ": ignore invalid lag port " << OBJ_CAST(link);
+    return -EINVAL;
+  }
+
+  auto lm_rv = lag_members.find(it->second);
+  if (lm_rv == lag_members.end()) {
+    VLOG(1) << __FUNCTION__ << ": ignore invalid attached port "
+            << OBJ_CAST(link);
+    return -EINVAL;
+  }
+
+  swi->lag_remove_member(it->second, port_id);
+  lag_members.erase(lm_rv);
+
+  return rv;
+}
+
+} // namespace basebox

src/netlink/nl_bond.hpp
@@ -1,0 +1,37 @@
+#pragma once
+
+#include <cstdint>
+#include <map>
+#include <set>
+
+extern "C" {
+struct rtnl_link;
+}
+
+namespace basebox {
+
+class cnetlink;
+class switch_interface;
+
+class nl_bond {
+public:
+  nl_bond(cnetlink *nl);
+  ~nl_bond() {}
+
+  void register_switch_interface(switch_interface *swi) { this->swi = swi; }
+
+  uint32_t get_lag_id(rtnl_link *bond);
+  int add_lag(rtnl_link *bond);
+  int remove_lag(rtnl_link *bond);
+  int add_lag_member(rtnl_link *bond, rtnl_link *link);
+  int remove_lag_member(rtnl_link *bond, rtnl_link *link);
+
+private:
+  switch_interface *swi;
+  cnetlink *nl;
+
+  std::map<int, uint32_t> ifi2lag;
+  std::map<int, uint32_t> lag_members;
+};
+
+} // namespace basebox

src/netlink/nl_bridge.cpp
@@ -191,7 +191,7 @@ void nl_bridge::update_vlans(rtnl_link *old_link, rtnl_link *new_link) {
     return;
   }
 
-  uint32_t pport_no = tap_man->get_port_id(rtnl_link_get_ifindex(_link));
+  uint32_t pport_no = nl->get_port_id(rtnl_link_get_ifindex(_link));
   if (pport_no == 0) {
     LOG(ERROR) << __FUNCTION__
                << ": invalid pport_no=0 of link: " << OBJ_CAST(_link);
@@ -311,7 +311,7 @@ void nl_bridge::add_neigh_to_fdb(rtnl_neigh *neigh) {
   assert(sw);
   assert(neigh);
 
-  uint32_t port = tap_man->get_port_id(rtnl_neigh_get_ifindex(neigh));
+  uint32_t port = nl->get_port_id(rtnl_neigh_get_ifindex(neigh));
   if (port == 0) {
     VLOG(1) << "ignoring neigh" << neigh << ": not a tap port";
     return;
@@ -365,7 +365,7 @@ void nl_bridge::remove_neigh_from_fdb(rtnl_neigh *neigh) {
     nl_cache_remove(OBJ_CAST(n_lookup.get()));
   }
 
-  const uint32_t port = tap_man->get_port_id(rtnl_neigh_get_ifindex(neigh));
+  const uint32_t port = nl->get_port_id(rtnl_neigh_get_ifindex(neigh));
   rofl::caddress_ll mac((uint8_t *)nl_addr_get_binary_addr(addr),
                         nl_addr_get_len(addr));
 

commit 2adfe5095957c3638e1e9030575f3c1acef734c6

netlink: fix bridge port handling
Even so we get a bridge slave the bridge related changes are shown only
when the family is AF_BRIDGE. In this case we want to have the actual
interface type/kind.

fixes: b31c17c

src/netlink/netlink-utils.cpp
@@ -53,12 +53,19 @@ enum link_type get_link_type(rtnl_link *link) noexcept {
       return LT_BRIDGE_SLAVE;
     }
 
+    // the bridge device (not the bridge_slave)
+    if (type && 0 == lt2names[LT_BRIDGE].compare(type) &&
+        rtnl_link_get_type(link)) {
+      type = rtnl_link_get_type(link);
+      slave = false;
+    }
+
   } else {
     type = rtnl_link_get_type(link);
   }
 
   VLOG(2) << __FUNCTION__ << ": type=" << std::string_view(type)
-          << ", af=" << rtnl_link_get_family(link) << ", slave=" << slave
+          << ", slave=" << slave << ", af=" << rtnl_link_get_family(link)
           << " of link " << OBJ_CAST(link);
 
   // lo has no type