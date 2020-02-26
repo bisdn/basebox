96e9da150d78df448a240457a0f505495cf995ce
Rubens Figueiredo <rubens.figueiredo@bisdn.de>
2020-02-26 11:46:54 +0100

    tap_manager: fix ci linting

--- src/netlink/tap_manager.cc
+++ src/netlink/tap_manager.cc
@@ -79,8 +79,8 @@ int tap_manager::create_tapdev(uint32_t port_id, const std::string &port_name,
       r = -EINVAL;
     }
   } else {
-    VLOG(1) << __FUNCTION__ << ": " << port_name
-              << " with port_id=" << port_id << " already existing";
+    VLOG(1) << __FUNCTION__ << ": " << port_name << " with port_id=" << port_id
+            << " already existing";
     r = -EEXIST;
   }
 
