ff71ca5ae6afd0b58b43d230fe4eca0646824888
Rubens Figueiredo <rubens.figueiredo@bisdn.de>
2021-01-22 14:10:24 +0100

    nl_bond: change API to reflect STP setting; only set state in the case that bridge is stp_state > 0
    Signed-off-by: Rubens Figueiredo <rubens.figueiredo@bisdn.de>

--- src/netlink/nl_bond.cc
+++ src/netlink/nl_bond.cc
@@ -237,6 +237,9 @@ int nl_bond::add_lag_member(rtnl_link *bond, rtnl_link *link) {
               << ": bond was already bridge slave: " << OBJ_CAST(br_link);
       nl->link_created(br_link);
 
+      if (nl->get_bridge_stp_state() == 0)
+        return rv;
+
       auto new_state = rtnl_link_bridge_get_port_state(br_link);
       std::string state;
 
@@ -261,7 +264,7 @@ int nl_bond::add_lag_member(rtnl_link *bond, rtnl_link *link) {
         return rv;
       }
 
-      swi->ofdpa_stg_state_port_set(port_id, state);
+      swi->ofdpa_global_stp_state_port_set(port_id, state);
     }
 
     rv = nl->set_bridge_port_vlan_tpid(br_link);
@@ -325,7 +328,7 @@ int nl_bond::remove_lag_member(rtnl_link *bond, rtnl_link *link) {
   lag_members.erase(lm_rv);
 
   if (nl->is_bridge_interface(bond)) {
-    swi->ofdpa_stg_state_port_set(port_id, "forward");
+    swi->ofdpa_global_stp_state_port_set(port_id, "forward");
 
     auto br_link = nl->get_link(rtnl_link_get_ifindex(bond), AF_BRIDGE);
     rv = nl->unset_bridge_port_vlan_tpid(br_link);
@@ -343,7 +346,7 @@ int nl_bond::remove_lag_member(rtnl_link *bond, rtnl_link *link) {
       remove_l3_address(bond);
 
     if (nl->is_bridge_interface(bond))
-      swi->ofdpa_stg_state_port_set(port_id, "forward");
+      swi->ofdpa_global_stp_state_port_set(port_id, "forward");
 
     for (auto vid : vlans) {
       swi->ingress_port_vlan_remove(port_id, vid, false);
