bisdn/basebox

commit 186326a14a0750d0f6abda5296befd8777ef0d98

cnetlink: add support for Linux RTM_NEW/DELVLAN messages; 	* add libnl bridge-vlan cache support;
Signed-off-by: Rubens Figueiredo <rubens.figueiredo@bisdn.de>

src/netlink/cnetlink.cc
@@ -14,6 +14,7 @@
 #include <linux/if.h>
 #include <netlink/object.h>
 #include <netlink/route/addr.h>
+#include <netlink/route/bridge_vlan.h>
 #include <netlink/route/link.h>
 #include <netlink/route/link/bonding.h>
 #include <netlink/route/link/vlan.h>
@@ -177,6 +178,19 @@ void cnetlink::init_caches() {
                << ": nl_cache_mngr_add_cache_v2: add route/mdb to cache mngr";
   }
 #endif
+
+  /* init bridge-vlan cache */
+  rc = rtnl_bridge_vlan_alloc_cache_flags(sock_mon, &caches[NL_BVLAN_CACHE],
+                                          NL_CACHE_AF_ITER);
+  if (0 != rc) {
+    LOG(FATAL) << __FUNCTION__
+               << ": rtnl_bridge_vlan_alloc_cache_flags failed rc=" << rc;
+  }
+  rc = nl_cache_mngr_add_cache_v2(mngr, caches[NL_BVLAN_CACHE],
+                                  (change_func_v2_t)&nl_cb_v2, this);
+  if (0 != rc) {
+    LOG(FATAL) << __FUNCTION__ << ": add route/bridge-vlan to cache mngr";
+  }
 
   try {
     thread.add_read_fd(this, nl_cache_mngr_get_fd(mngr), true, false);
@@ -534,6 +548,10 @@ void cnetlink::handle_wakeup(rofl::cthread &thread) {
       route_mdb_apply(obj);
       break;
 #endif
+    case RTM_NEWVLAN:
+    case RTM_DELVLAN:
+      route_bridge_vlan_apply(obj);
+      break;
     default:
       LOG(ERROR) << __FUNCTION__ << ": unexpected netlink type "
                  << obj.get_msg_type();
@@ -1423,6 +1441,26 @@ void cnetlink::route_mdb_apply(const nl_obj &obj) {
     if (bridge)
       bridge->mdb_entry_remove(MDB_CAST(obj.get_new_obj()));
     break;
+  default:
+    LOG(ERROR) << __FUNCTION__ << ": invalid action " << obj.get_action();
+    break;
+  }
+}
+
+void cnetlink::route_bridge_vlan_apply(const nl_obj &obj) {
+  assert(obj.get_new_obj());
+
+  switch (obj.get_msg_type()) {
+  case RTM_NEWVLAN:
+    if (bridge)
+      bridge->set_pvlan_stp(BRIDGE_VLAN_CAST(obj.get_new_obj()));
+    break;
+#if 0
+  case RTM_DELVLAN:
+	if (bridge)
+	        bridge->set_pvlan_stp(BRIDGE_VLAN_CAST(obj.get_new_obj()));
+	break;
+#endif
   default:
     LOG(ERROR) << __FUNCTION__ << ": invalid action " << obj.get_action();
     break;

src/netlink/cnetlink.h
@@ -36,6 +36,7 @@ public:
     NL_NEIGH_CACHE,
     NL_ROUTE_CACHE,
     NL_MDB_CACHE,
+    NL_BVLAN_CACHE,
     NL_MAX_CACHE,
   };
 
@@ -167,6 +168,7 @@ private:
   void route_neigh_apply(const nl_obj &obj);
   void route_route_apply(const nl_obj &obj);
   void route_mdb_apply(const nl_obj &obj);
+  void route_bridge_vlan_apply(const nl_obj &obj);
 
   enum cnetlink_event_t {
     EVENT_NONE,

src/netlink/netlink-utils.h
@@ -10,6 +10,7 @@
 extern "C" {
 struct nl_addr;
 struct rtnl_addr;
+struct rtnl_bridge_vlan;
 struct rtnl_link;
 struct rtnl_neigh;
 struct rtnl_route;
@@ -21,6 +22,7 @@ struct rtnl_mdb;
 #define ROUTE_CAST(obj) reinterpret_cast<struct rtnl_route *>(obj)
 #define ADDR_CAST(obj) reinterpret_cast<struct rtnl_addr *>(obj)
 #define MDB_CAST(obj) reinterpret_cast<struct rtnl_mdb *>(obj)
+#define BRIDGE_VLAN_CAST(obj) reinterpret_cast<struct rtnl_bridge_vlan *>(obj)
 
 namespace basebox {
 

commit c29c11d234761c6ad19b62433abf061fdf949519

make bridge vlan support in libnl optional
The bridge vlan support is WIP and not yet submitted upstream. So to
allow building and using baseboxd without a patched libnl, add a check
for the new header and guard the code using it.

Fixes: 186326a ("cnetlink: add support for Linux RTM_NEW/DELVLAN messages;      * add libnl bridge-vlan cache support;")
Reported-by: Andreas Koepsel <andreas.koepsel@bisdn.de>
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

meson.build
@@ -120,6 +120,10 @@ if cppc.has_header_symbol('netlink/route/link/bonding.h', 'rtnl_link_bond_get_mo
   add_global_arguments('-DHAVE_RTNL_LINK_BOND_GET_MODE',  language: 'cpp')
 endif
 
+if cppc.has_header('netlink/route/bridge_vlan.h', dependencies: libnl, required: false)
+  add_global_arguments('-DHAVE_NETLINK_ROUTE_BRIDGE_VLAN_H',  language: 'cpp')
+endif
+
 libnl_route = dependency('libnl-route-3.0', required: false)
 if not libnl_route.found()
   # find the lib without pkg-config

src/netlink/cnetlink.cc
@@ -14,7 +14,9 @@
 #include <linux/if.h>
 #include <netlink/object.h>
 #include <netlink/route/addr.h>
+#ifdef HAVE_NETLINK_ROUTE_BRIDGE_VLAN_H
 #include <netlink/route/bridge_vlan.h>
+#endif
 #include <netlink/route/link.h>
 #include <netlink/route/link/bonding.h>
 #include <netlink/route/link/vlan.h>
@@ -179,6 +181,7 @@ void cnetlink::init_caches() {
   }
 #endif
 
+#ifdef HAVE_NETLINK_ROUTE_BRIDGE_VLAN_H
   /* init bridge-vlan cache */
   rc = rtnl_bridge_vlan_alloc_cache_flags(sock_mon, &caches[NL_BVLAN_CACHE],
                                           NL_CACHE_AF_ITER);
@@ -191,6 +194,7 @@ void cnetlink::init_caches() {
   if (0 != rc) {
     LOG(FATAL) << __FUNCTION__ << ": add route/bridge-vlan to cache mngr";
   }
+#endif
 
   try {
     thread.add_read_fd(this, nl_cache_mngr_get_fd(mngr), true, false);
@@ -735,10 +739,12 @@ void cnetlink::handle_wakeup(rofl::cthread &thread) {
       route_mdb_apply(obj);
       break;
 #endif
+#ifdef HAVE_NETLINK_ROUTE_BRIDGE_VLAN_H
     case RTM_NEWVLAN:
     case RTM_DELVLAN:
       route_bridge_vlan_apply(obj);
       break;
+#endif
     default:
       LOG(ERROR) << __FUNCTION__ << ": unexpected netlink type "
                  << obj.get_msg_type();

src/netlink/nl_bridge.cc
@@ -11,7 +11,9 @@
 
 #include <glog/logging.h>
 #include <netlink/route/link.h>
+#ifdef HAVE_NETLINK_ROUTE_BRIDGE_VLAN_H
 #include <netlink/route/bridge_vlan.h>
+#endif
 #ifdef HAVE_NETLINK_ROUTE_MDB_H
 #include <netlink/route/mdb.h>
 #endif
@@ -1078,6 +1080,7 @@ int nl_bridge::mdb_entry_remove(rtnl_mdb *mdb_entry) {
 int nl_bridge::set_pvlan_stp(struct rtnl_bridge_vlan *bvlan_info) {
   int err = 0;
 
+#ifdef HAVE_NETLINK_ROUTE_BRIDGE_VLAN_H
   if (get_stp_state() == STP_STATE_DISABLED)
     return err;
 
@@ -1102,6 +1105,7 @@ int nl_bridge::set_pvlan_stp(struct rtnl_bridge_vlan *bvlan_info) {
 
   err = sw->ofdpa_stg_state_port_set(nl->get_port_id(ifindex), vlan_id,
                                      stp_state_to_string(g_stp_state));
+#endif
   return err;
 }
 