bisdn/basebox

commit 8139c136579ea495f460d55c3970f5a5f0d9599d

controller: reuse deleted stg ids
Since we now actually free up STGs, we need to make sure to reuse the
freed STGs.

Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/of-dpa/controller.cc
@@ -2186,6 +2186,21 @@ int controller::lookup_stpid(uint32_t vlan_id) noexcept {
   return (it == vlan_to_stg.end()) ? 0 : it->second;
 }
 
+int controller::find_free_stgid() noexcept {
+  // 512 groups, 2 reserved (0 and 1)
+  if (vlan_to_stg.size() == (512 - 2))
+    return -ENOSPC;
+
+  while (stg_in_use[current_stg]) {
+    current_stg++;
+
+    if (current_stg == 512)
+      current_stg = 2;
+  }
+
+  return current_stg;
+}
+
 int controller::ofdpa_stg_destroy(uint16_t vlan_id) noexcept {
   int rv;
   int stg_id = lookup_stpid(vlan_id);
@@ -2227,23 +2242,28 @@ int controller::ofdpa_stg_create(uint16_t vlan_id) noexcept {
   if (stg_id != 0)
     return stg_id;
 
-  rv = ofdpa->ofdpaStgCreate(current_stg);
+  stg_id = find_free_stgid();
+  if (stg_id < 0) {
+    LOG(ERRROR) << _FUNCTION__ << ": failed to allocate STG ID: " << stg_id;
+    return stg_id;
+  }
+
+  rv = ofdpa->ofdpaStgCreate(stg_id);
   if (rv < 0) {
     LOG(ERROR) << __FUNCTION__ << ": failed to create the STP group";
     return rv;
   }
 
-  rv = ofdpa->ofdpaStgVlanAdd(vlan_id, current_stg);
+  rv = ofdpa->ofdpaStgVlanAdd(vlan_id, stg_id);
   if (rv < 0) {
     LOG(ERROR) << __FUNCTION__ << ": failed to add VLAN=" << vlan_id
-               << " to the STP group=" << current_stg;
+               << " to the STP group=" << stg_id;
     return rv;
   }
 
-  vlan_to_stg.emplace(std::make_pair(vlan_id, current_stg));
+  vlan_to_stg.emplace(std::make_pair(vlan_id, stg_id));
   stg_in_use[current_stg] = true;
 
-  current_stg++;
   return rv;
 }
 

src/of-dpa/controller.h
@@ -333,6 +333,8 @@ private:
   uint32_t current_stg = 2;
   std::bitset<512> stg_in_use;
 
+  int find_free_stgid(void) noexcept;
+
   struct multicast_entry {
     // mmac, vlan
     std::tuple<rofl::caddress_ll, uint16_t> key;

commit 0717b8a5fe10569204f8534c83aa6426a3e23da7

controller: fix typos that broke compilation
Fix the following build error:
| In file included from ../git/src/of-dpa/controller.h:17,
|                  from ../git/src/of-dpa/controller.cc:16:
| ../git/src/of-dpa/controller.cc: In member function 'virtual int basebox::controller::ofdpa_stg_create(uint16_t)':
| ../git/src/of-dpa/controller.cc:2247:5: error: 'COMPACT_GOOGLE_LOG_ERRROR' was not declared in this scope; did you mean 'COMPACT_GOOGLE_LOG_ERROR'?
|  2247 |     LOG(ERRROR) << _FUNCTION__ << ": failed to allocate STG ID: " << stg_id;
|       |     ^~~
| ../git/src/of-dpa/controller.cc:2247:20: error: '_FUNCTION__' was not declared in this scope
|  2247 |     LOG(ERRROR) << _FUNCTION__ << ": failed to allocate STG ID: " << stg_id;
|       |                    ^~~~~~~~~~~

Fixes: 8139c13 ("controller: reuse deleted stg ids")
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/of-dpa/controller.cc
@@ -2244,7 +2244,7 @@ int controller::ofdpa_stg_create(uint16_t vlan_id) noexcept {
 
   stg_id = find_free_stgid();
   if (stg_id < 0) {
-    LOG(ERRROR) << _FUNCTION__ << ": failed to allocate STG ID: " << stg_id;
+    LOG(ERROR) << __FUNCTION__ << ": failed to allocate STG ID: " << stg_id;
     return stg_id;
   }
 