bisdn/basebox

commit de0c37d6ee40055d19ce1a88a04d26ba49cd1a84

datamodel: remove ofdpaGlobalStpStatePortSet
Signed-off-by: Rubens Figueiredo <rubens.figueiredo@bisdn.de>

src/netlink/nl_bridge.cc
@@ -241,10 +241,10 @@ void nl_bridge::update_interface(rtnl_link *old_link, rtnl_link *new_link) {
     if (nbi::get_port_type(port_id) == nbi::port_type_lag) {
       auto members = nl->get_bond_members_by_lag(new_link);
       for (auto mem : members) {
-        sw->ofdpa_global_stp_state_port_set(mem, state);
+        sw->ofdpa_stg_state_port_set(mem, 1, state);
       }
     } else {
-      sw->ofdpa_global_stp_state_port_set(port_id, state);
+      sw->ofdpa_stg_state_port_set(port_id, 1, state);
     }
 
     return;
@@ -277,10 +277,10 @@ void nl_bridge::delete_interface(rtnl_link *link) {
   if (nbi::get_port_type(port_id) == nbi::port_type_lag) {
     auto members = nl->get_bond_members_by_lag(link);
     for (auto mem : members) {
-      sw->ofdpa_global_stp_state_port_set(mem, "forward");
+      sw->ofdpa_stg_state_port_set(mem, 1, "forward");
     }
   } else {
-    sw->ofdpa_global_stp_state_port_set(port_id, "forward");
+    sw->ofdpa_stg_state_port_set(port_id, 1, "forward");
   }
 }
 

src/netlink/nl_bond.cc
@@ -264,7 +264,7 @@ int nl_bond::add_lag_member(rtnl_link *bond, rtnl_link *link) {
         return rv;
       }
 
-      swi->ofdpa_global_stp_state_port_set(port_id, state);
+      swi->ofdpa_stg_state_port_set(port_id, 1, state);
     }
 
     rv = nl->set_bridge_port_vlan_tpid(br_link);
@@ -328,7 +328,7 @@ int nl_bond::remove_lag_member(rtnl_link *bond, rtnl_link *link) {
   lag_members.erase(lm_rv);
 
   if (nl->is_bridge_interface(bond)) {
-    swi->ofdpa_global_stp_state_port_set(port_id, "forward");
+    swi->ofdpa_stg_state_port_set(port_id, 1, "forward");
 
     auto br_link = nl->get_link(rtnl_link_get_ifindex(bond), AF_BRIDGE);
     rv = nl->unset_bridge_port_vlan_tpid(br_link);
@@ -346,7 +346,7 @@ int nl_bond::remove_lag_member(rtnl_link *bond, rtnl_link *link) {
       remove_l3_address(bond);
 
     if (nl->is_bridge_interface(bond))
-      swi->ofdpa_global_stp_state_port_set(port_id, "forward");
+      swi->ofdpa_stg_state_port_set(port_id, 1, "forward");
 
     for (auto vid : vlans) {
       swi->ingress_port_vlan_remove(port_id, vid, false);

src/of-dpa/controller.cc
@@ -2171,7 +2171,7 @@ int controller::tunnel_port_tenant_remove(uint32_t lport_id,
 int controller::lookup_stpid(uint32_t vlan_id) noexcept {
   auto it = vlan_to_stg.find(vlan_id);
 
-  return (it == vlan_to_stg.end()) ? 0 : it->second;
+  return (it == vlan_to_stg.end()) ? 1 : it->second;
 }
 
 int controller::ofdpa_stg_destroy(uint16_t vlan_id) noexcept { return 0; }
@@ -2212,18 +2212,6 @@ int controller::ofdpa_stg_create(uint16_t vlan_id) noexcept {
   vlan_to_stg.emplace(std::make_pair(vlan_id, current_stg));
 
   current_stg++;
-  return rv;
-}
-
-int controller::ofdpa_global_stp_state_port_set(uint32_t port_id,
-                                                std::string state) noexcept {
-  int rv;
-
-  rv = ofdpa->ofdpaGlobalStpStatePortSet(port_id, state);
-  if (rv < 0) {
-    LOG(ERROR) << __FUNCTION__ << ": failed to set the STP state";
-  }
-
   return rv;
 }
 

src/of-dpa/controller.h
@@ -311,8 +311,6 @@ public:
 
   int ofdpa_stg_state_port_set(uint32_t port_id, uint16_t vlan_id,
                                std::string state) noexcept override;
-  int ofdpa_global_stp_state_port_set(uint32_t port_id,
-                                      std::string state) noexcept override;
 
   /* print this */
   friend std::ostream &operator<<(std::ostream &os, const controller &box) {

src/of-dpa/ofdpa_client.cc
@@ -319,25 +319,6 @@ ofdpa_client::ofdpaStgStatePortSet(uint32_t port_id, std::string state,
 }
 
 ofdpa::OfdpaStatus::OfdpaStatusCode
-ofdpa_client::ofdpaGlobalStpStatePortSet(uint32_t port_id, std::string state) {
-  ::OfdpaStatus response;
-  ::ClientContext context;
-  ::StpInterfaceState request;
-
-  context.set_wait_for_ready(true);
-
-  request.set_port_num(port_id);
-  request.set_port_state(state);
-
-  ::Status rv = stub_->ofdpaGlobalStpStatePortSet(&context, request, &response);
-  if (not rv.ok()) {
-    return ofdpa::OfdpaStatus::OFDPA_E_RPC;
-  }
-
-  return response.status();
-}
-
-ofdpa::OfdpaStatus::OfdpaStatusCode
 ofdpa_client::ofdpaStgCreate(uint16_t stg_id) {
   ::OfdpaStatus response;
   ::ClientContext context;

src/of-dpa/ofdpa_client.h
@@ -65,8 +65,6 @@ public:
   ofdpa::OfdpaStatus::OfdpaStatusCode ofdpaStgStatePortSet(uint32_t port_id,
                                                            std::string state,
                                                            uint32_t stg_id = 0);
-  ofdpa::OfdpaStatus::OfdpaStatusCode
-  ofdpaGlobalStpStatePortSet(uint32_t port_id, std::string state);
 
   ofdpa::OfdpaStatus::OfdpaStatusCode
   OfdpaTrunkCreate(uint32_t lag_id, std::string name, uint8_t mode);

src/sai.h
@@ -232,8 +232,6 @@ public:
 
   virtual int ofdpa_stg_state_port_set(uint32_t port_id, uint16_t vlan_id,
                                        std::string state) noexcept = 0;
-  virtual int ofdpa_global_stp_state_port_set(uint32_t port_id,
-                                              std::string state) noexcept = 0;
   /* @} */
 };
 

commit 02b2a8fa0c1a68fb7ff7607780f3ee8298065487

nl_bridge: do not set global STP state as VID 1 stp state
If we update the a bridged port's global STP state we update all STGs
with the calculated effective states, and lastly want to set the default
group with STGID 1 to the global state.

But controller::ofdpa_stg_state_port_set takes the VID, not the STGID,
so passing 1 will actually modify VID 1's STP state (only), potentially
undoing a previous block or discard.

So pass it 0, which isn't a valid VID, and which will be resolved
internally to STGID 1.

Fixes: de0c37d ("datamodel: remove ofdpaGlobalStpStatePortSet")
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/netlink/nl_bridge.cc
@@ -1092,10 +1092,10 @@ void nl_bridge::set_port_stp_state(uint32_t port_id, uint8_t stp_state) {
   if (nbi::get_port_type(port_id) == nbi::port_type_lag) {
     auto members = nl->get_bond_members_by_port_id(port_id);
     for (auto mem : members) {
-      sw->ofdpa_stg_state_port_set(mem, 1, state);
+      sw->ofdpa_stg_state_port_set(mem, 0, state);
     }
   } else {
-    sw->ofdpa_stg_state_port_set(port_id, 1, state);
+    sw->ofdpa_stg_state_port_set(port_id, 0, state);
   }
 }
 