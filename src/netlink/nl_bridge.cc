25fdabf8e58452c72f8d81e5205e285885db96f5
Rubens Figueiredo <rubens.figueiredo@bisdn.de>
2021-10-20 16:38:54 +0200

    debug

--- src/netlink/nl_bridge.cc
+++ src/netlink/nl_bridge.cc
@@ -481,6 +481,8 @@ void nl_bridge::update_vlans(rtnl_link *old_link, rtnl_link *new_link) {
       done = 0;
       while (!done) {
         int j = find_next_bit(i, untagged_diff);
+	VLOG(1) << "DEBUG diff=" << untagged_diff
+		<< " j=" << j;
         if (j > 0) {
           // egress untagged changed
           int vid = j - 1 + base_bit;
@@ -490,6 +492,10 @@ void nl_bridge::update_vlans(rtnl_link *old_link, rtnl_link *new_link) {
           if (new_br_vlan->untagged_bitmap[k] & 1 << (j - 1)) {
             egress_untagged = true;
           }
+              VLOG(3) << __FUNCTION__ << ": change vid=" << vid
+                      << " on pport_no=" << pport_no
+		      << " egress untagged=" << egress_untagged
+                      << " link: " << OBJ_CAST(_link);
 
           sw->egress_port_vlan_add(pport_no, vid, true, true);
 
