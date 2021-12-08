bisdn/basebox

commit b5fe46ca64d4e260f1e887b82e68c6b4eec8c8a4

tap_manager: set port speed on tap interfaces
Signed-off-by: Rubens Figueiredo <rubens.figueiredo@bisdn.de>

src/netlink/nbi_impl.cc
@@ -38,6 +38,7 @@ void nbi_impl::port_notification(
       switch (get_port_type(ntfy.port_id)) {
       case nbi::port_type_physical:
         tap_man->change_port_status(ntfy.name, ntfy.status);
+	tap_man->set_port_speed(ntfy.name, ntfy.speed, ntfy.duplex);
         break;
       default:
         LOG(ERROR) << __FUNCTION__ << ": unknown port";
@@ -49,6 +50,7 @@ void nbi_impl::port_notification(
       case nbi::port_type_physical:
         tap_man->create_tapdev(ntfy.port_id, ntfy.name, *this);
         tap_man->change_port_status(ntfy.name, ntfy.status);
+        tap_man->set_port_speed(ntfy.name, ntfy.speed, ntfy.duplex);
         break;
       case nbi::port_type_vxlan:
         // XXX TODO notify this?

src/netlink/tap_manager.cc
@@ -8,11 +8,16 @@
 #include <netlink/route/link.h>
 #include <sys/ioctl.h>
 #include <utility>
+#include <linux/ethtool.h>
+#include <linux/sockios.h>
 
 #include "cnetlink.h"
 #include "ctapdev.h"
 #include "tap_io.h"
 #include "tap_manager.h"
+
+#define ETHTOOL_LINK_MODE_MASK_MAX_KERNEL_NU32 (SCHAR_MAX)
+#define ETHTOOL_SPEED(speed) speed / 1000 // conversion to Mbit
 
 namespace basebox {
 
@@ -324,6 +329,63 @@ int tap_manager::change_port_status(const std::string name, bool status) {
   error = ioctl(fd_it->second, TUNSETCARRIER, &carrier);
   if (error) {
     LOG(ERROR) << __FUNCTION__ << ": ioctl failed with error code " << error;
+  }
+
+  return error;
+}
+
+int tap_manager::set_port_speed(const std::string name, uint32_t speed,
+                                uint8_t duplex) {
+  LOG(INFO) << __FUNCTION__ << ": changing port= " << name
+            << " speed=" << ETHTOOL_SPEED(speed);
+  struct ifreq ifr = {};
+
+  auto sockFd = socket(AF_INET, SOCK_DGRAM, 0);
+  strncpy(ifr.ifr_name, name.c_str(), IFNAMSIZ);
+
+  // The ethtool API requires an handshake via ETHTOOL_GLINKSETTINGS
+  // before getting the "real" value to agree on the length of link
+  // mode bitmaps.
+
+  struct {
+    struct ethtool_link_settings req;
+    __u32 link_mode_data[3 * ETHTOOL_LINK_MODE_MASK_MAX_KERNEL_NU32];
+  } link_settings;
+
+  memset(&link_settings, 0, sizeof(link_settings));
+  link_settings.req.cmd = ETHTOOL_GLINKSETTINGS;
+
+  ifr.ifr_data = reinterpret_cast<char *>(&link_settings);
+  int error = ioctl(sockFd, SIOCETHTOOL, static_cast<void *>(&ifr));
+  if (error < 0) {
+    LOG(ERROR) << __FUNCTION__ << " handshake failed error=" << error;
+    return error;
+  }
+
+  if (link_settings.req.link_mode_masks_nwords >= 0 ||
+      link_settings.req.cmd != ETHTOOL_GLINKSETTINGS)
+    return -EOPNOTSUPP;
+
+  link_settings.req.link_mode_masks_nwords =
+      -link_settings.req.link_mode_masks_nwords;
+
+  ifr.ifr_data = reinterpret_cast<char *>(&link_settings);
+  error = ioctl(sockFd, SIOCETHTOOL, static_cast<void *>(&ifr));
+  if (error < 0) {
+    LOG(ERROR) << __FUNCTION__ << " failed to get port= " << name
+               << " error=" << error;
+    return error;
+  }
+
+  link_settings.req.duplex = duplex;
+  link_settings.req.speed = ETHTOOL_SPEED(speed);
+  link_settings.req.cmd = ETHTOOL_SLINKSETTINGS;
+
+  ifr.ifr_data = reinterpret_cast<char *>(&link_settings);
+  error = ioctl(sockFd, SIOCETHTOOL, static_cast<void *>(&ifr));
+  if (error < 0) {
+    LOG(ERROR) << __FUNCTION__ << " failed to set port= " << name
+               << " speed, error=" << error;
   }
 
   return error;

src/netlink/tap_manager.h
@@ -11,6 +11,7 @@
 #include <mutex>
 
 #include "sai.h"
+#include <linux/ethtool.h>
 
 extern "C" {
 struct rtnl_link;
@@ -76,6 +77,7 @@ public:
   int get_fd(uint32_t port_id) const noexcept;
 
   int change_port_status(const std::string name, bool status);
+  int set_port_speed(const std::string name, uint32_t speed, uint8_t duplex);
 
   // access from northbound (cnetlink)
   int tapdev_removed(int ifindex, const std::string &portname);

src/of-dpa/controller.cc
@@ -239,10 +239,12 @@ void controller::handle_port_status(rofl::crofdpt &dpt,
 
   bool status = (!(port.get_config() & rofl::openflow13::OFPPC_PORT_DOWN) &&
                  !(port.get_state() & rofl::openflow13::OFPPS_LINK_DOWN));
+  uint32_t speed = port.get_ethernet().get_curr_speed();
+  uint8_t duplex = get_duplex(port.get_ethernet().get_curr());
 
   ntfys.emplace_back(nbi::port_notification_data{
       (nbi::port_event)msg.get_reason(), port.get_port_no(),
-      msg.get_port().get_name(), status});
+      msg.get_port().get_name(), status, speed, duplex});
   nb->port_notification(ntfys);
 }
 
@@ -274,9 +276,12 @@ void controller::handle_port_desc_stats_reply(
 
     bool status = (!(port.get_config() & rofl::openflow13::OFPPC_PORT_DOWN) &&
                    !(port.get_state() & rofl::openflow13::OFPPS_LINK_DOWN));
+    uint32_t speed = port.get_ethernet().get_curr_speed();
+    uint8_t duplex = get_duplex(port.get_ethernet().get_curr());
 
-    notifications.emplace_back(nbi::port_notification_data{
-        nbi::PORT_EVENT_ADD, port.get_port_no(), port.get_name(), status});
+    notifications.emplace_back(
+        nbi::port_notification_data{nbi::PORT_EVENT_ADD, port.get_port_no(),
+                                    port.get_name(), status, speed, duplex});
   }
 
   /* init 1:1 port mapping */

src/of-dpa/controller.h
@@ -22,6 +22,7 @@
 
 #include "sai.h"
 
+#define CHECK_BIT(var, pos) (((var) >> (pos)) & 1)
 namespace basebox {
 
 // forward declarations
@@ -303,6 +304,13 @@ private:
   bool connected;
   std::shared_ptr<ofdpa_client> ofdpa;
   uint16_t ofdpa_grpc_port;
+
+  uint8_t get_duplex(uint32_t current) {
+    if (CHECK_BIT(current, 0) or CHECK_BIT(current, 2) or CHECK_BIT(current, 4))
+      return 0;
+    else
+      return 1;
+  }
 
   enum timer_t {
     /* handle_timeout will be called as well from crofbase, hence we need some

src/sai.h
@@ -198,6 +198,8 @@ public:
     uint32_t port_id;
     std::string name;
     bool status;
+    uint32_t speed;
+    uint8_t duplex;
   };
 
   enum port_type {

commit 333f9fe6d3d4da79b58d2e30aefadc52c0b39075

nbi_impl: set link state last for tap devices
Software or drivers like the bonding may try to read out the speed only
at link state changes, so we need to make sure that the link speed is
already set to the expected value when setting the link itself.

Else it may lead to e.g. bond interfaces with wrong link speeds with 10
Mbit/s, despite both links being up with 100Gbit/s.

Fixes: b5fe46c ("tap_manager: set port speed on tap interfaces")
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/netlink/nbi_impl.cc
@@ -37,8 +37,8 @@ void nbi_impl::port_notification(
     case PORT_EVENT_MODIFY:
       switch (get_port_type(ntfy.port_id)) {
       case nbi::port_type_physical:
-        tap_man->change_port_status(ntfy.name, ntfy.status);
         tap_man->set_port_speed(ntfy.name, ntfy.speed, ntfy.duplex);
+        tap_man->change_port_status(ntfy.name, ntfy.status);
         break;
       default:
         LOG(ERROR) << __FUNCTION__ << ": unknown port";
@@ -49,8 +49,8 @@ void nbi_impl::port_notification(
       switch (get_port_type(ntfy.port_id)) {
       case nbi::port_type_physical:
         tap_man->create_tapdev(ntfy.port_id, ntfy.name, *this);
-        tap_man->change_port_status(ntfy.name, ntfy.status);
         tap_man->set_port_speed(ntfy.name, ntfy.speed, ntfy.duplex);
+        tap_man->change_port_status(ntfy.name, ntfy.status);
         break;
       case nbi::port_type_vxlan:
         // XXX TODO notify this?