bisdn/basebox

commit 50fe194b8f79308455b80c2bf7c10007ee090e91

cnetlink: fetch bridge object with ifindex and address family
libnl 3.6.0 has introduced the following
https://github.com/thom311/libnl/pull/264 patch. The added group to the
dict has an effect of adding a second bridge interfaces entry to the cache,
refering to the inet6 link notification.

By fetching the object with the ifindex and AF_BRIDGE, we filter for the
correct object type.

Avoids the behaviour after the following instructions.

```
baseboxd up -> all ports created
ip link add swbridge type bridge vlan_filtering 1
ip link set port1 master swbridge
```

Good:
```
route_link_apply: new link bridge port1 ether 96:cf:87:84:86:23 master swbridge <broadcast,multicast>
link_created: using *bridge* bridge swbridge ether 0a:39:85:5b:25:67 <broadcast,multicast> group 0
```

Wrong:

```
link_created: using bridge inet6 swbridge ether 0a:39:85:5b:25:67 <broadcast,multicast>
baseboxd: ../src/netlink/nl_bridge.cc:61: void basebox::nl_bridge::set_bridge_interface(rtnl_link*): Assertion `rtnl_link_is_bridge(bridge)' failed.
```

Signed-off-by: Rubens Figueiredo <rubens.figueiredo@bisdn.de>

src/netlink/cnetlink.cc
@@ -246,7 +246,24 @@ void cnetlink::destroy_caches() { nl_cache_mngr_free(mngr); }
 
 std::unique_ptr<struct rtnl_link, decltype(&rtnl_link_put)>
 cnetlink::get_link_by_ifindex(int ifindex) const {
-  rtnl_link *link = rtnl_link_get(caches[NL_LINK_CACHE], ifindex);
+  struct rtnl_link *link = nullptr;
+  std::unique_ptr<rtnl_link, decltype(&rtnl_link_put)> filter(rtnl_link_alloc(),
+                                                              &rtnl_link_put);
+
+  rtnl_link_set_ifindex(filter.get(), ifindex);
+
+  // search link by filter
+  nl_cache_foreach_filter(
+      caches[NL_LINK_CACHE], OBJ_CAST(filter.get()),
+      [](struct nl_object *obj, void *arg) {
+        assert(arg);
+
+        if (rtnl_link_get_family(LINK_CAST(arg)) != AF_INET6) {
+          nl_object_get(obj);
+          *static_cast<nl_object **>(arg) = obj;
+        }
+      },
+      &link);
 
   // check the garbage
   if (link == nullptr) {

commit 627ff59e8b8a811a53176f244ee80b1989e677eb

cnetlink: get_link_by_ifindex: fix af family check
We need to check the family of the passed cache object, not the
argument. Else we read random garbage from the stack, and sometimes it
might or might not be equal to AF_INET6.

Fixes get_link_by_ifindex sometimes failing to find a link object, which
may even crash baseboxd:

Jul 07 23:31:22 accton-as4610 baseboxd[1121]: I0707 23:31:22.864306  1129 cnetlink.cc:679] is_bridge_interface: vlan ok
Jul 07 23:31:22 accton-as4610 baseboxd[1121]: baseboxd: ../git/src/netlink/cnetlink.cc:660: bool basebox::cnetlink::is_bridge_interface(rtnl_link*) const: Assertion `l' failed.
Jul 07 23:31:22 accton-as4610 systemd[1]: Created slice system-systemd\x2dcoredump.slice.
Jul 07 23:31:22 accton-as4610 systemd[1]: Started Process Core Dump (PID 1741/UID 0).

Fixes: 50fe194b8f79 ("cnetlink: fetch bridge object with ifindex and address family")
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/netlink/cnetlink.cc
@@ -258,7 +258,7 @@ cnetlink::get_link_by_ifindex(int ifindex) const {
       [](struct nl_object *obj, void *arg) {
         assert(arg);
 
-        if (rtnl_link_get_family(LINK_CAST(arg)) != AF_INET6) {
+        if (rtnl_link_get_family(LINK_CAST(obj)) != AF_INET6) {
           nl_object_get(obj);
           *static_cast<nl_object **>(arg) = obj;
         }