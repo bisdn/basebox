bisdn/basebox

commit ee54b7ae49d239370bc59c21020137ea333b4bc0

nl_bridge: implement tracking global and pervlan stp state; 	* adds struct to track g/pv stp states; 	* adds setting of stp state in the ASIC
Signed-off-by: Rubens Figueiredo <rubens.figueiredo@bisdn.de>

src/netlink/nl_bridge.cc
@@ -7,10 +7,11 @@
 #include <map>
 #include <utility>
 #include <linux/if_packet.h>
+#include <linux/if_bridge.h>
 
 #include <glog/logging.h>
-#include <linux/if_bridge.h>
 #include <netlink/route/link.h>
+#include <netlink/route/bridge_vlan.h>
 #ifdef HAVE_NETLINK_ROUTE_MDB_H
 #include <netlink/route/mdb.h>
 #endif
@@ -218,38 +219,28 @@ void nl_bridge::update_interface(rtnl_link *old_link, rtnl_link *new_link) {
   std::string state;
 
   if (old_state != new_state) {
-    LOG(INFO) << __FUNCTION__ << "STP state changed, old=" << old_state
+    if (get_stp_state() == BR_STATE_DISABLED)
+      return;
+
+    LOG(INFO) << __FUNCTION__ << " STP state changed, old=" << old_state
               << " new=" << new_state;
 
-    switch (new_state) {
-    case BR_STATE_FORWARDING:
-      state = "forward";
-      break;
-    case BR_STATE_BLOCKING:
-      state = "block";
-      break;
-    case BR_STATE_DISABLED:
-      state = "disable";
-      break;
-    case BR_STATE_LISTENING:
-      state = "listen";
-      break;
-    case BR_STATE_LEARNING:
-      state = "learn";
-      break;
-    default:
-      VLOG(1) << __FUNCTION__ << ": stp state change not supported";
-      return;
-    }
-
     auto port_id = nl->get_port_id(new_link);
+    bridge_stp_states.add_global_state(port_id, new_state);
+
+    auto pv_states = bridge_stp_states.get_min_states(port_id);
+    for (auto it : pv_states)
+      sw->ofdpa_stg_state_port_set(port_id, it.first,
+                                   stp_state_to_string(it.second));
+
+    state = stp_state_to_string(new_state);
     if (nbi::get_port_type(port_id) == nbi::port_type_lag) {
       auto members = nl->get_bond_members_by_lag(new_link);
       for (auto mem : members) {
-        sw->ofdpa_stg_state_port_set(mem, state);
+        sw->ofdpa_global_stp_state_port_set(mem, state);
       }
     } else {
-      sw->ofdpa_stg_state_port_set(port_id, state);
+      sw->ofdpa_global_stp_state_port_set(port_id, state);
     }
 
     return;
@@ -282,10 +273,10 @@ void nl_bridge::delete_interface(rtnl_link *link) {
   if (nbi::get_port_type(port_id) == nbi::port_type_lag) {
     auto members = nl->get_bond_members_by_lag(link);
     for (auto mem : members) {
-      sw->ofdpa_stg_state_port_set(mem, "forward");
+      sw->ofdpa_global_stp_state_port_set(mem, "forward");
     }
   } else {
-    sw->ofdpa_stg_state_port_set(port_id, "forward");
+    sw->ofdpa_global_stp_state_port_set(port_id, "forward");
   }
 }
 
@@ -1069,6 +1060,73 @@ int nl_bridge::mdb_entry_remove(rtnl_mdb *mdb_entry) {
 #endif
 
   return rv;
+}
+
+// struct rtnl_bridge_vlan {
+//	NLHDR_COMMON
+//	uint32_t ifindex;
+//	uint8_t family;
+//
+//	uint16_t vlan_id;
+//	uint16_t flags;
+//	uint16_t range;
+//	uint8_t state;
+int nl_bridge::set_pvlan_stp(struct rtnl_bridge_vlan *bvlan_info) {
+  int err = 0;
+
+  if (get_stp_state() == BR_STATE_DISABLED)
+    return err;
+
+  uint32_t ifindex = rtnl_bridge_vlan_get_ifindex(bvlan_info);
+  int port_id = nl->get_port_id(ifindex);
+  struct rtnl_bvlan_entry *entry = rtnl_bridge_vlan_get_entry_head(bvlan_info);
+  uint16_t vlan_id = rtnl_bridge_vlan_entry_get_vlan_id(entry);
+  uint8_t stp_state = rtnl_bridge_vlan_entry_get_state(entry);
+
+  if (is_bridge_interface(ifindex))
+    return err;
+
+  err = sw->ofdpa_stg_create(vlan_id);
+  if (err < 0)
+    return err;
+
+  int pv_state = bridge_stp_states.get_pvlan_state(port_id, vlan_id);
+  if (pv_state == 0)
+    bridge_stp_states.add_pvlan_state(port_id, vlan_id, stp_state);
+
+  auto g_stp_state = bridge_stp_states.get_min_state(port_id, vlan_id);
+
+  LOG(INFO) << __FUNCTION__ << ": set state=" << g_stp_state
+            << " ifindex= " << ifindex << " VLAN =" << vlan_id;
+
+  err = sw->ofdpa_stg_state_port_set(nl->get_port_id(ifindex), vlan_id,
+                                     stp_state_to_string(g_stp_state));
+  return err;
+}
+
+std::string nl_bridge::stp_state_to_string(uint8_t state) {
+  std::string ret;
+  switch (state) {
+  case BR_STATE_FORWARDING:
+    ret = "forward";
+    break;
+  case BR_STATE_BLOCKING:
+    ret = "block";
+    break;
+  case BR_STATE_DISABLED:
+    ret = "disable";
+    break;
+  case BR_STATE_LISTENING:
+    ret = "listen";
+    break;
+  case BR_STATE_LEARNING:
+    ret = "learn";
+    break;
+  default:
+    ret = "";
+  }
+
+  return ret;
 }
 
 } /* namespace basebox */

src/netlink/nl_bridge.h
@@ -15,8 +15,18 @@
 
 #include "netlink-utils.h"
 
+#define BR_STATE_DISABLED 0
+#define BR_STATE_LISTENING 1
+#define BR_STATE_LEARNING 2
+#define BR_STATE_FORWARDING 3
+#define BR_STATE_BLOCKING 4
+
+#define STP_STATE_DISABLED 0
+#define STP_STATE_KERNEL 1
+#define STP_STATE_USERSPACE 2
+
 extern "C" {
-struct rtnl_link_bridge_vlan;
+struct rtnl_bridge_vlan;
 struct rtnl_link;
 struct rtnl_neigh;
 struct rtnl_mdb;
@@ -36,6 +46,97 @@ class switch_interface;
 class tap_manager;
 struct packet;
 
+struct key {
+  int port_id;
+  uint16_t vid;
+
+  key(int port_id, uint16_t vid) : port_id(port_id), vid(vid) {}
+  bool operator<(const struct key &rhs) const {
+    return std::tie(port_id, vid) < std::tie(rhs.port_id, rhs.vid);
+  }
+};
+
+struct bridge_stp_states {
+  // Global STP state: KEY : port_id + vid, state
+  std::map<int, uint8_t> gl_states;
+  std::map<uint16_t, std::map<uint16_t, uint8_t>> pv_states;
+
+  bridge_stp_states() = default;
+  void add_pvlan_state(int port_id, uint16_t vlan_id, uint8_t state) {
+    auto it = pv_states.find(vlan_id);
+    if (it == pv_states.end()) {
+      std::map<uint16_t, uint8_t> pv_map;
+      pv_map.emplace(port_id, state);
+      pv_states.emplace(vlan_id, pv_map);
+      return;
+    }
+
+    auto pv_it = it->second.find(port_id);
+    if (pv_it != it->second.end())
+      pv_it = it->second.erase(pv_it);
+
+    it->second.emplace(port_id, state);
+  }
+
+  void add_global_state(int port_id, uint8_t state) {
+    auto it = gl_states.find(port_id);
+    if (it != gl_states.end())
+      gl_states.erase(it);
+
+    gl_states.emplace(port_id, state);
+  }
+
+  uint8_t get_global_state(int port_id) {
+    auto it = gl_states.find(port_id);
+
+    return (it == gl_states.end()) ? 0 : it->second;
+  }
+
+  uint8_t get_pvlan_state(int port_id, uint16_t vid) {
+    auto it = pv_states.find(vid);
+
+    // no vlan found
+    if (it == pv_states.end())
+      return 0;
+
+    // no port found
+    auto pv_state = it->second.find(port_id);
+    if (pv_state == it->second.end())
+      return 0;
+
+    return pv_state->second;
+  }
+
+  uint8_t get_min(uint8_t g_state, uint8_t pv_state) {
+    return (g_state < pv_state) ? g_state : pv_state;
+  }
+
+  std::map<uint16_t, uint8_t> get_min_states(int port_id) {
+    std::map<uint16_t, uint8_t> ret;
+    for (auto it : pv_states) {
+      auto pv_it = it.second.find(port_id);
+      if (pv_it == it.second.end())
+        continue;
+
+      ret.emplace(it.first, pv_it->second);
+    }
+    return ret;
+  }
+
+  int get_min_state(int port_id, uint16_t vid) {
+    int g_state = get_global_state(port_id);
+    int pv_state = get_pvlan_state(port_id, vid);
+
+    if (pv_state == 0)
+      return 0;
+
+    if (g_state == BR_STATE_BLOCKING || pv_state == BR_STATE_BLOCKING)
+      return g_state;
+
+    return get_min(g_state, pv_state);
+  }
+};
+
 class nl_bridge {
 public:
   nl_bridge(switch_interface *sw, std::shared_ptr<tap_manager> tap_man,
@@ -44,7 +145,10 @@ public:
 
   virtual ~nl_bridge();
 
+  int set_pvlan_stp(struct rtnl_bridge_vlan *bvlan_info);
+
   void set_bridge_interface(rtnl_link *);
+  bool is_bridge_interface(int ifindex);
   bool is_bridge_interface(rtnl_link *);
 
   void add_interface(rtnl_link *);
@@ -87,12 +191,16 @@ public:
                           const uint32_t tunnel_id, bool add);
 
 private:
+  struct bridge_stp_states bridge_stp_states;
+  std::string stp_state_to_string(uint8_t state);
+
   void update_vlans(rtnl_link *, rtnl_link *);
 
   void update_access_ports(rtnl_link *vxlan_link, rtnl_link *br_link,
                            const uint16_t vid, const uint32_t tunnel_id,
                            const std::deque<rtnl_link *> &bridge_ports,
                            bool add);
+
   rtnl_link *bridge;
   switch_interface *sw;
   std::shared_ptr<tap_manager> tap_man;

commit 02402b9cb452181e2e002af7e0b69393ccaa6114

nl_bridge: always set port stp state
The kernel will set ports to disabled while the bridge interface is
down to inhibit forwarding regardless wether STP is enabled for the
bridge, and we should be doing the same.

So do not ignore port STP state changes when STP is disabled.

Fixes: ee54b7ae49d2 ("nl_bridge: implement tracking global and pervlan stp state;       * adds struct to track g/pv stp states;         * adds setting of stp state in the ASIC")
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/netlink/cnetlink.h
@@ -82,10 +82,6 @@ public:
   int get_port_id(rtnl_link *l) const;
   int get_port_id(int ifindex) const;
   int get_ifindex_by_port_id(uint32_t port_id) const;
-  int get_bridge_stp_state() {
-    assert(bridge);
-    return bridge->get_stp_state();
-  }
 
   std::map<uint16_t, uint8_t> get_port_vlan_stp_states(rtnl_link *link) {
     assert(bridge);

src/netlink/nl_bond.cc
@@ -235,9 +235,6 @@ int nl_bond::add_lag_member(rtnl_link *bond, rtnl_link *link) {
               << ": bond was already bridge slave: " << OBJ_CAST(br_link);
       nl->link_created(br_link);
 
-      if (nl->get_bridge_stp_state() == 0)
-        return rv;
-
       auto new_state = rtnl_link_bridge_get_port_state(br_link);
       swi->ofdpa_stg_state_port_set(port_id, 0, new_state);
       auto pv_states = nl->get_port_vlan_stp_states(bond);

src/netlink/nl_bridge.cc
@@ -84,12 +84,6 @@ bool nl_bridge::is_bridge_interface(rtnl_link *link) {
   return is_bridge_interface(rtnl_link_get_ifindex(link));
 }
 
-// Read sysfs to obtain the value for STP state on a bridge
-uint32_t nl_bridge::get_stp_state() {
-  std::string portname(rtnl_link_get_name(bridge));
-  return nl->load_from_file("/sys/class/net/" + portname + "/bridge/stp_state");
-}
-
 // Read sysfs to obtain the value for the VLAN protocol on the switch.
 // Only two values are suported: 0x8100 and 0x88a8
 uint32_t nl_bridge::get_vlan_proto() {
@@ -199,11 +193,9 @@ void nl_bridge::add_interface(rtnl_link *link) {
     return;
   }
 
-  if (get_stp_state() != STP_STATE_DISABLED) {
-    auto state = rtnl_link_bridge_get_port_state(link);
-    auto port_id = nl->get_port_id(link);
-    set_port_stp_state(port_id, state);
-  }
+  auto state = rtnl_link_bridge_get_port_state(link);
+  auto port_id = nl->get_port_id(link);
+  set_port_stp_state(port_id, state);
 
   // configure bonds and physical ports (non members of bond)
   update_vlans(nullptr, link);
@@ -232,9 +224,6 @@ void nl_bridge::update_interface(rtnl_link *old_link, rtnl_link *new_link) {
   std::string state;
 
   if (old_state != new_state) {
-    if (get_stp_state() == STP_STATE_DISABLED)
-      return;
-
     LOG(INFO) << __FUNCTION__ << " STP state changed, old=" << old_state
               << " new=" << new_state;
 
@@ -349,7 +338,6 @@ void nl_bridge::update_vlans(rtnl_link *old_link, rtnl_link *new_link) {
     }
   }
   auto members = nl->get_bond_members_by_lag(_link);
-  bool stp_enabled = (get_stp_state() != STP_STATE_DISABLED);
 
   // check for pvid changes
   if (new_br_vlan->pvid != old_br_vlan->pvid &&
@@ -438,7 +426,7 @@ void nl_bridge::update_vlans(rtnl_link *old_link, rtnl_link *new_link) {
             // we might have missed the initial port vlan message since it
             // may come before the attachment message, make sure that any
             // existing vlans have their expected stp states.
-            if (stp_enabled && old_link == nullptr &&
+            if (old_link == nullptr &&
                 (bridge_stp_states.get_pvlan_state(pport_no, vid) == -EINVAL)) {
               // default state is FORWARDING in the kernel
               int state = BR_STATE_FORWARDING;
@@ -1143,9 +1131,6 @@ int nl_bridge::set_pvlan_stp(struct rtnl_bridge_vlan *bvlan_info) {
   int err = 0;
 
 #ifdef HAVE_NETLINK_ROUTE_BRIDGE_VLAN_H
-  if (get_stp_state() == STP_STATE_DISABLED)
-    return err;
-
   uint32_t ifindex = rtnl_bridge_vlan_get_ifindex(bvlan_info);
   int port_id = nl->get_port_id(ifindex);
   uint16_t vlan_id = rtnl_bridge_vlan_get_vlan_id(bvlan_info);
@@ -1163,9 +1148,6 @@ int nl_bridge::drop_pvlan_stp(struct rtnl_bridge_vlan *bvlan_info) {
   int err = 0;
 
 #ifdef HAVE_NETLINK_ROUTE_BRIDGE_VLAN_H
-  if (get_stp_state() == STP_STATE_DISABLED)
-    return err;
-
   uint32_t ifindex = rtnl_bridge_vlan_get_ifindex(bvlan_info);
   int port_id = nl->get_port_id(ifindex);
   uint16_t vlan_id = rtnl_bridge_vlan_get_vlan_id(bvlan_info);
@@ -1180,9 +1162,6 @@ int nl_bridge::drop_pvlan_stp(struct rtnl_bridge_vlan *bvlan_info) {
 
 std::map<uint16_t, uint8_t>
 nl_bridge::get_port_vlan_stp_states(rtnl_link *link) {
-  if (get_stp_state() == STP_STATE_DISABLED)
-    return {};
-
   uint32_t port_id = nl->get_port_id(link);
   if (port_id == 0)
     return {};

src/netlink/nl_bridge.h
@@ -21,10 +21,6 @@
 #define BR_STATE_FORWARDING 3
 #define BR_STATE_BLOCKING 4
 
-#define STP_STATE_DISABLED 0
-#define STP_STATE_KERNEL 1
-#define STP_STATE_USERSPACE 2
-
 extern "C" {
 struct rtnl_bridge_vlan;
 struct rtnl_link;
@@ -177,7 +173,6 @@ public:
                   const rofl::caddress_ll &mac);
   int get_ifindex() { return bridge ? rtnl_link_get_ifindex(bridge) : 0; }
 
-  uint32_t get_stp_state();
   uint32_t get_vlan_proto();
   int set_vlan_proto(rtnl_link *link);
   int delete_vlan_proto(rtnl_link *link);