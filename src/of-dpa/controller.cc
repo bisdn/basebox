bisdn/basebox

commit c9f6de2b6f6273425cd22591a9f7dc5e4c196f2c

bond: expand bonding feature in baseboxd
The current bonding support in baseboxd does not implement
the correct calls to the OF-DPA environment. This commit
adds the necessary calls to the switch so that trunk ports can be
handled in ASIC

Signed-off-by: Rubens Figueiredo <rubens.figueiredo@bisdn.de>

src/of-dpa/controller.cc
@@ -256,7 +256,7 @@ void controller::handle_error_message(rofl::crofdpt &dpt,
           << " pkt received: " << std::endl
           << msg;
 
-  LOG(WARNING) << __FUNCTION__ << ": not implemented";
+  LOG(WARNING) << __FUNCTION__ << msg;
 }
 
 void controller::handle_port_desc_stats_reply(
@@ -492,7 +492,8 @@ errout:
   return rv;
 }
 
-int controller::lag_create(uint32_t *lag_id) noexcept {
+int controller::lag_create(uint32_t *lag_id, std::string name,
+                           uint8_t mode) noexcept {
   if (!connected) {
     VLOG(1) << __FUNCTION__ << ": not connected";
     return -EAGAIN;
@@ -512,8 +513,10 @@ int controller::lag_create(uint32_t *lag_id) noexcept {
 
   assert(lag_id);
   *lag_id = nbi::combine_port_type(_lag_id, nbi::port_type_lag);
-
   _lag_id++;
+
+  ofdpa->OfdpaTrunkCreate(*lag_id, name, mode);
+
   return 0;
 }
 
@@ -553,15 +556,9 @@ int controller::lag_add_member(uint32_t lag_id, uint32_t port_id) noexcept {
     return -ENODATA;
   }
 
-  // currently we can deal only with a single slave
-  if (it->second.size()) {
-    LOG(ERROR) << __FUNCTION__
-               << ": lag already has a member lag_id=" << std::showbase
-               << std::hex << lag_id << ", member=" << *it->second.begin();
-    return -EINVAL;
-  }
-
   it->second.emplace(port_id);
+
+  ofdpa->OfdpaTrunkPortMemberSet(port_id, lag_id);
 
   return 0;
 }
@@ -593,6 +590,28 @@ int controller::lag_remove_member(uint32_t lag_id, uint32_t port_id) noexcept {
     return -EINVAL;
   }
 
+  return 0;
+}
+
+int controller::lag_set_active_member(uint32_t lag_id, uint32_t port_id,
+                                      uint8_t active) noexcept {
+  if (!connected) {
+    VLOG(1) << __FUNCTION__ << ": not connected";
+    return -EAGAIN;
+  }
+
+  ofdpa->OfdpaTrunkPortMemberActiveSet(port_id, lag_id, active);
+
+  return 0;
+}
+
+int controller::lag_set_mode(uint32_t lag_id, uint8_t mode) noexcept {
+  if (!connected) {
+    VLOG(1) << __FUNCTION__ << ": not connected";
+    return -EAGAIN;
+  }
+
+  ofdpa->ofdpaTrunkPortPSCSet(lag_id, mode);
   return 0;
 }
 
@@ -660,7 +679,7 @@ int controller::l2_addr_remove_all_in_vlan(uint32_t port,
 
 int controller::l2_addr_add(uint32_t port, uint16_t vid,
                             const rofl::caddress_ll &mac, bool filtered,
-                            bool permanent) noexcept {
+                            bool permanent, bool lag) noexcept {
   int rv = 0;
   try {
     rofl::crofdpt &dpt = set_dpt(dptid, true);
@@ -669,9 +688,9 @@ int controller::l2_addr_add(uint32_t port, uint16_t vid,
       fm_driver.set_idle_timeout(300);
 
     // XXX have the knowlege here about filtered/unfiltered?
-    dpt.send_flow_mod_message(rofl::cauxid(0),
-                              fm_driver.add_bridging_unicast_vlan(
-                                  dpt.get_version(), port, vid, mac, filtered));
+    dpt.send_flow_mod_message(
+        rofl::cauxid(0), fm_driver.add_bridging_unicast_vlan(
+                             dpt.get_version(), port, vid, mac, filtered, lag));
 
     if (!permanent)
       fm_driver.set_idle_timeout(default_idle_timeout);
@@ -1606,14 +1625,21 @@ int controller::egress_port_vlan_drop_accept_all(uint32_t port) noexcept {
   return rv;
 }
 
-int controller::egress_port_vlan_add(uint32_t port, uint16_t vid,
-                                     bool untagged) noexcept {
+int controller::egress_port_vlan_add(uint32_t port, uint16_t vid, bool untagged,
+                                     bool lag) noexcept {
   int rv = 0;
   try {
     // create filtered egress interface
     rofl::crofdpt &dpt = set_dpt(dptid, true);
-    rofl::openflow::cofgroupmod gm = fm_driver.enable_group_l2_interface(
-        dpt.get_version(), port, vid, untagged);
+    rofl::openflow::cofgroupmod gm;
+
+    if (lag) {
+      gm = fm_driver.enable_group_l2_trunk_interface(dpt.get_version(), port,
+                                                     vid, untagged);
+    } else {
+      gm = fm_driver.enable_group_l2_interface(dpt.get_version(), port, vid,
+                                               untagged);
+    }
     dpt.send_group_mod_message(rofl::cauxid(0), gm);
   } catch (rofl::eRofBaseNotFound &e) {
     LOG(ERROR) << ": caught rofl::eRofBaseNotFound";
@@ -1756,17 +1782,20 @@ int controller::del_l2_overlay_flood(uint32_t tunnel_id,
 }
 
 int controller::egress_bridge_port_vlan_add(uint32_t port, uint16_t vid,
-                                            bool untagged) noexcept {
+                                            bool untagged,
+                                            bool is_lag) noexcept {
   int rv = 0;
   try {
     rofl::crofdpt &dpt = set_dpt(dptid, true);
     std::set<uint32_t> l2_dom_set;
 
     // create filtered egress interface
-    rv = egress_port_vlan_add(port, vid, untagged);
+    rv = egress_port_vlan_add(port, vid, untagged, is_lag);
+    if (is_lag)
+      return 0;
+
     if (rv < 0)
       return rv;
-    dpt.send_barrier_request(rofl::cauxid(0));
 
     {
       std::lock_guard<std::mutex> lock(l2_domain_mutex);

src/netlink/cnetlink.cc
@@ -313,6 +313,10 @@ void cnetlink::get_bridge_ports(int br_ifindex,
   }
 }
 
+std::set<uint32_t> cnetlink::get_bond_members_by_lag(rtnl_link *bond_link) {
+  return bond->get_members(bond_link);
+}
+
 struct rtnl_neigh *cnetlink::get_neighbour(int ifindex,
                                            struct nl_addr *a) const {
   assert(ifindex);
@@ -392,6 +396,11 @@ int cnetlink::get_port_id(int ifindex) const {
   auto link = get_link_by_ifindex(ifindex);
 
   return get_port_id(link.get());
+}
+
+int cnetlink::get_ifindex_by_port_id(uint32_t port_id) const {
+
+    return tap_man->get_ifindex(port_id);
 }
 
 void cnetlink::handle_wakeup(rofl::cthread &thread) {
@@ -984,6 +993,12 @@ void cnetlink::link_updated(rtnl_link *old_link, rtnl_link *new_link) noexcept {
   }
 
   switch (lt_old) {
+  case LT_BOND_SLAVE:
+    if (lt_new == LT_BOND_SLAVE) { // bond updated
+      bond->update_lag_member(old_link, new_link);
+    } else if (lt_new == LT_TUN) { // link released
+    }
+    break;
   case LT_BRIDGE_SLAVE: // a bridge slave was changed
     if (bridge) {
       try {
@@ -1028,8 +1043,10 @@ void cnetlink::link_updated(rtnl_link *old_link, rtnl_link *new_link) noexcept {
     }
     iface->changed(old_link, new_link);
     break;
-  case LT_VRF: // No need to care about the vrf interface itself
   case LT_BOND:
+    bond->update_lag(old_link, new_link);
+    break;
+  case LT_VRF: // No need to care about the vrf interface itself
   case LT_BRIDGE:
   case LT_UNSUPPORTED:
     VLOG(2) << __FUNCTION__

src/netlink/cnetlink.h
@@ -47,6 +47,7 @@ public:
   struct rtnl_link *get_link(int ifindex, int family) const;
   void get_bridge_ports(int br_ifindex,
                         std::deque<rtnl_link *> *link_list) const noexcept;
+  std::set<uint32_t> get_bond_members_by_lag(rtnl_link *bond_link);
 
   /**
    * @return rtnl_neigh* which needs to be freed using rtnl_neigh_put
@@ -58,6 +59,7 @@ public:
   bool is_bridge_configured(rtnl_link *l);
   int get_port_id(rtnl_link *l) const;
   int get_port_id(int ifindex) const;
+  int get_ifindex_by_port_id(uint32_t port_id) const;
 
   nl_cache *get_cache(enum nl_cache_t id) { return caches[id]; }
 

src/netlink/nl_bond.cc
@@ -7,6 +7,7 @@
 #include <cassert>
 #include <glog/logging.h>
 #include <netlink/route/link.h>
+#include <netlink/route/link/bonding.h>
 
 #include "cnetlink.h"
 #include "nl_output.h"
@@ -30,25 +31,98 @@ uint32_t nl_bond::get_lag_id(rtnl_link *bond) {
     return 0;
   }
 
-  // XXX currently we do not deal with a real lag since we just deal with
-  // a single port in the lag. Thus we just mimic the lag behaviour and map
-  // everything on the linux bond/team to the single physical port
   auto rv_lm = lag_members.find(it->second);
   if (rv_lm == lag_members.end()) {
     VLOG(1) << ": no members in lag " << OBJ_CAST(bond);
     return 0;
   }
 
-  VLOG(3) << __FUNCTION__ << ": found id=" << rv_lm->second;
-  return rv_lm->second;
+  VLOG(3) << __FUNCTION__ << ": found id=" << rv_lm->first;
+  return rv_lm->first;
+}
+
+std::set<uint32_t> nl_bond::get_members(rtnl_link *bond) {
+  auto it = ifi2lag.find(rtnl_link_get_ifindex(bond));
+  if (it == ifi2lag.end()) {
+    LOG(WARNING) << __FUNCTION__ << ": lag does not exist for "
+                 << OBJ_CAST(bond);
+    return {};
+  }
+
+  auto mem_it = lag_members.find(it->second);
+  if (mem_it == lag_members.end()) {
+    LOG(WARNING) << __FUNCTION__ << ": lag does not exist for "
+                 << OBJ_CAST(bond);
+    return {};
+  }
+
+  return mem_it->second;
+}
+
+int nl_bond::update_lag(rtnl_link *old_link, rtnl_link *new_link) {
+  VLOG(1) << __FUNCTION__ << ": updating bond interface ";
+  int rv = 0;
+
+  uint8_t state;
+  uint32_t o_active;
+  uint32_t n_active;
+  uint8_t o_mode;
+  uint8_t n_mode;
+  uint32_t lag_id = nl->get_port_id(new_link);
+
+  rtnl_link_bond_get_active_slave(old_link, &o_active);
+  rtnl_link_bond_get_active_slave(new_link, &n_active);
+
+  if (o_active == 0 || n_active == 0) {
+    VLOG(1) << __FUNCTION__ << ": no active bond slave";
+    return 0;
+  }
+
+  if (o_active != n_active) {
+    rv = swi->lag_set_active_member(lag_id, nl->get_port_id(n_active), 1);
+    if (rv < 0) {
+      VLOG(1) << __FUNCTION__ << ": failed to set active state for "
+              << OBJ_CAST(new_link);
+    }
+
+    rv = swi->lag_set_active_member(lag_id, nl->get_port_id(o_active), 0);
+
+    if (rv < 0) {
+      VLOG(1) << __FUNCTION__ << ": failed to set active state for "
+              << OBJ_CAST(new_link);
+    }
+
+    return 0;
+  }
+
+  rtnl_link_bond_get_mode(old_link, &o_mode);
+  rtnl_link_bond_get_mode(new_link, &n_mode);
+
+  if (o_mode != n_mode) {
+    VLOG(1) << __FUNCTION__ << ": bond mode updated "
+            << static_cast<uint32_t>(n_mode);
+    rv = swi->lag_set_mode(lag_id, n_mode);
+
+    if (rv < 0) {
+      VLOG(1) << __FUNCTION__ << ": failed to set active state for "
+              << OBJ_CAST(new_link);
+    }
+
+    return 0;
+  }
+
+  return 0;
 }
 
 int nl_bond::add_lag(rtnl_link *bond) {
   uint32_t lag_id = 0;
   int rv = 0;
+  uint8_t mode;
+
+  rtnl_link_bond_get_mode(bond, &mode);
 
   assert(bond);
-  rv = swi->lag_create(&lag_id);
+  rv = swi->lag_create(&lag_id, rtnl_link_get_name(bond), mode);
   if (rv < 0) {
     LOG(ERROR) << __FUNCTION__ << ": failed to create lag for "
                << OBJ_CAST(bond);
@@ -99,6 +173,8 @@ int nl_bond::remove_lag(rtnl_link *bond) {
 int nl_bond::add_lag_member(rtnl_link *bond, rtnl_link *link) {
   int rv = 0;
   uint32_t lag_id;
+  uint8_t state = 0;
+
   auto it = ifi2lag.find(rtnl_link_get_ifindex(bond));
   if (it == ifi2lag.end()) {
     VLOG(1) << __FUNCTION__ << ": no lag_id found creating new for "
@@ -119,13 +195,24 @@ int nl_bond::add_lag_member(rtnl_link *bond, rtnl_link *link) {
     return -EINVAL;
   }
 
-  auto lm_rv = lag_members.emplace(lag_id, port_id);
-  if (!lm_rv.second) {
-    LOG(ERROR) << __FUNCTION__ << ": cannot add multiple ports to a lag";
-    return -EINVAL;
+  auto mem_it = lag_members.find(it->second);
+  if (mem_it == lag_members.end()) { // No ports in lag
+    std::set<uint32_t> members;
+    members.insert(port_id);
+    auto lm_rv = lag_members.emplace(lag_id, members);
+  } else {
+    mem_it->second.insert(port_id);
+  }
+
+  rv = rtnl_link_bond_slave_get_state(link, &state);
+  if (rv < 0) {
+    VLOG(1) << __FUNCTION__ << ": failed to get slave state for "<<  OBJ_CAST(link);
   }
 
   rv = swi->lag_add_member(lag_id, port_id);
+
+  VLOG(1) << " SET ACTIVE MEMBER " << lag_id << " " << port_id << " " << static_cast<uint32_t>(state);
+  rv = swi->lag_set_active_member(lag_id, port_id, state == 0);
 
   if (rtnl_link_get_master(bond)) {
     // check bridge attachement
@@ -166,6 +253,37 @@ int nl_bond::remove_lag_member(rtnl_link *bond, rtnl_link *link) {
   lag_members.erase(lm_rv);
 
   return rv;
+}
+
+int nl_bond::update_lag_member(rtnl_link *old_slave, rtnl_link *new_slave) {
+  int rv;
+  uint8_t new_state;
+  uint8_t old_state;
+
+  int n_master_id = rtnl_link_get_master(new_slave);
+  int o_master_id = rtnl_link_get_master(old_slave);
+  auto new_master = nl->get_link(n_master_id, AF_UNSPEC);
+  auto old_master = nl->get_link(o_master_id, AF_UNSPEC);
+  auto port_id = nl->get_port_id(new_slave);
+
+  if (old_master != new_master || new_master == 0) {
+    return -EINVAL;
+  }
+
+  rv = rtnl_link_bond_slave_get_state(new_slave, &new_state);
+  if (rv != 0) {
+    VLOG(1) << __FUNCTION__ << ": failed to get state";
+    return -EINVAL;
+  }
+  rtnl_link_bond_slave_get_state(old_slave, &old_state);
+  if (rv != 0) {
+    VLOG(1) << __FUNCTION__ << ": failed to get state";
+    return -EINVAL;
+  }
+
+  rv = swi->lag_set_active_member(nl->get_port_id(new_master), port_id,
+                                  new_state == 0);
+  return 0;
 }
 
 } // namespace basebox

src/netlink/nl_bond.h
@@ -31,6 +31,12 @@ public:
    * @returns id > 0 if found, 0 if not found
    */
   uint32_t get_lag_id(rtnl_link *bond);
+  /**
+   * returns the members of the lag
+   *
+   * @returns id > 0 if found, 0 if not found
+   */
+  std::set<uint32_t> get_members(rtnl_link *bond);
 
   /**
    * create a new lag
@@ -38,17 +44,19 @@ public:
    * @returns <0 on error, lag_id on success
    */
   int add_lag(rtnl_link *bond);
-
   int remove_lag(rtnl_link *bond);
   int add_lag_member(rtnl_link *bond, rtnl_link *link);
   int remove_lag_member(rtnl_link *bond, rtnl_link *link);
+  int update_lag(rtnl_link *old_link, rtnl_link *new_link);
+
+  int update_lag_member(rtnl_link *old_slave, rtnl_link *new_slave);
 
 private:
   switch_interface *swi;
   cnetlink *nl;
 
   std::map<int, uint32_t> ifi2lag;
-  std::map<int, uint32_t> lag_members;
+  std::map<int, std::set<uint32_t>> lag_members;
 };
 
 } // namespace basebox

src/netlink/nl_bridge.cc
@@ -144,7 +144,16 @@ void nl_bridge::add_interface(rtnl_link *link) {
     return;
   }
 
+  // configure bonds and physical ports (non members of bond)
   update_vlans(nullptr, link);
+
+  // configure bond slaves
+  auto members = nl->get_bond_members_by_lag(link);
+  for (auto mem : members) {
+    VLOG(1) << " MEMBER ID " << mem;
+    auto _link = nl->get_link_by_ifindex(nl->get_ifindex_by_port_id(mem));
+    update_vlans(nullptr, _link.get());
+  }
 
   if (get_vlan_proto() == ETH_P_8021AD)
     sw->set_egress_tpid(nl->get_port_id(link));
@@ -269,6 +278,14 @@ void nl_bridge::update_vlans(rtnl_link *old_link, rtnl_link *new_link) {
     return;
   }
 
+  // bond slave added
+  if (/* old_link == nullptr && */ get_link_type(_link) == LT_BOND_SLAVE) {
+    int master = rtnl_link_get_master(_link);
+    auto _l = nl->get_link(master, AF_BRIDGE);
+    old_br_vlan = &empty_br_vlan;
+    new_br_vlan = rtnl_link_bridge_get_port_vlan(_l);
+  }
+
   // check for vid changes
   if (br_vlan_equal(old_br_vlan, new_br_vlan)) {
     VLOG(2) << __FUNCTION__ << ": vlans did not change";
@@ -288,7 +305,6 @@ void nl_bridge::update_vlans(rtnl_link *old_link, rtnl_link *new_link) {
       LOG(ERROR) << __FUNCTION__ << ": failed to get vni of link "
                  << OBJ_CAST(_link);
     }
-
   } else {
     pport_no = nl->get_port_id(_link);
     if (pport_no == 0) {
@@ -367,7 +383,9 @@ void nl_bridge::update_vlans(rtnl_link *old_link, rtnl_link *new_link) {
               VLOG(3) << __FUNCTION__ << ": add vid=" << vid
                       << " on pport_no=" << pport_no
                       << " link: " << OBJ_CAST(_link);
-              sw->egress_bridge_port_vlan_add(pport_no, vid, egress_untagged);
+              bool lag = nbi::get_port_type(pport_no) == nbi::port_type_lag;
+              sw->egress_bridge_port_vlan_add(pport_no, vid, egress_untagged,
+                                              lag);
               sw->ingress_port_vlan_add(pport_no, vid,
                                         new_br_vlan->pvid == vid);
             }
@@ -608,6 +626,7 @@ void nl_bridge::add_neigh_to_fdb(rtnl_neigh *neigh) {
 
   nl_addr *mac = rtnl_neigh_get_lladdr(neigh);
   int vlan = rtnl_neigh_get_vlan(neigh);
+  bool lag = nbi::get_port_type(port) == nbi::port_type_lag;
 
   bool permanent = true;
 
@@ -629,7 +648,7 @@ void nl_bridge::add_neigh_to_fdb(rtnl_neigh *neigh) {
             << rtnl_link_get_name(bridge) << " on port=" << port
             << " vlan=" << (unsigned)vlan << ", permanent=" << permanent;
   LOG(INFO) << __FUNCTION__ << ": object: " << OBJ_CAST(neigh);
-  sw->l2_addr_add(port, vlan, _mac, true, permanent);
+  sw->l2_addr_add(port, vlan, _mac, true, permanent, lag);
 }
 
 void nl_bridge::remove_neigh_from_fdb(rtnl_neigh *neigh) {

src/netlink/nl_l3.cc
@@ -250,6 +250,22 @@ int nl_l3::add_l3_addr(struct rtnl_addr *a) {
     }
   }
 
+  if (auto members = nl->get_bond_members_by_lag(link); !members.empty()) {
+    VLOG(1) << __FUNCTION__ << ": BOND configuring slaves";
+
+    for (auto mem : members) {
+      auto _link = nl->get_link_by_ifindex(nl->get_ifindex_by_port_id(mem));
+      bool tagged = !!rtnl_link_is_vlan(link);
+
+      rv = vlan->add_vlan(_link.get(), vid, tagged, vrf_id);
+
+      auto addr = rtnl_link_get_addr(_link.get());
+      rofl::caddress_ll mac = libnl_lladdr_2_rofl(addr);
+
+      rv = add_l3_termination(mem, vid, mac, AF_INET);
+    }
+  }
+
   return rv;
 }
 

src/netlink/nl_vlan.cc
@@ -44,7 +44,12 @@ int nl_vlan::add_vlan(rtnl_link *link, uint16_t vid, bool tagged,
   }
 
   // setup egress interface
-  rv = swi->egress_port_vlan_add(port_id, vid, !tagged);
+  auto type = nbi::get_port_type(port_id);
+  if (type == nbi::port_type_lag) {
+    rv = swi->egress_port_vlan_add(port_id, vid, !tagged);
+  } else {
+    rv = swi->egress_port_vlan_add(port_id, vid, !tagged);
+  }
 
   if (rv < 0) {
     LOG(ERROR) << __FUNCTION__

commit 7c6adeecbe8100b7b6e1eef4c9c100dcd5ac66e9

controller: fix ::egress_port_vlan_remove() for bonds
Bond interfaces use a different L2 Interface group type, so we need to
check for bond and handle them in controller::egress_port_vlan_remove(),
like we do in controller::egress_port_vlan_add().

Fixes: c9f6de2b6f62 ("bond: expand bonding feature in baseboxd")
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/of-dpa/controller.cc
@@ -1761,9 +1761,15 @@ int controller::egress_port_vlan_remove(uint32_t port, uint16_t vid) noexcept {
   try {
     // remove filtered egress interface
     rofl::crofdpt &dpt = set_dpt(dptid, true);
-    dpt.send_group_mod_message(
-        rofl::cauxid(0),
-        fm_driver.disable_group_l2_interface(dpt.get_version(), port, vid));
+    rofl::openflow::cofgroupmod gm;
+
+    if (nbi::get_port_type(port) == nbi::port_type_lag) {
+      gm = fm_driver.disable_group_l2_trunk_interface(dpt.get_version(), port,
+                                                      vid);
+    } else {
+      gm = fm_driver.disable_group_l2_interface(dpt.get_version(), port, vid);
+    }
+    dpt.send_group_mod_message(rofl::cauxid(0), gm);
     uint32_t xid = 0;
     dpt.send_barrier_request(rofl::cauxid(0), 1, &xid);
     VLOG(2) << __FUNCTION__ << ": sent barrier with xid=" << (unsigned)xid;