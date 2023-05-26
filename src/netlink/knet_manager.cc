bisdn/basebox

commit 4e4576a798bcf7a6225e3c0fc07c817679cf44eb

port_manager: keep track of original MAC address of ports
OF-DPA requires the correct (internal) MAC address set for all port_mod
messages, so we need to store it when we first create the port
interfaces.

Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/netlink/knet_manager.cc
@@ -84,8 +84,15 @@ int knet_manager::create_portdev(uint32_t port_id, const std::string &port_name,
         auto rv = port_names2id.emplace(std::make_pair(port_name, port_id));
 
         if (!rv.second) {
-          LOG(FATAL) << __FUNCTION__ << ": failed to insert";
+          LOG(FATAL) << __FUNCTION__ << ": failed to insert port name";
         }
+
+        auto rv2 = id_to_hwaddr.emplace(std::make_pair(port_id, hwaddr));
+
+        if (!rv2.second) {
+          LOG(FATAL) << __FUNCTION__ << ": failed to insert hwaddr";
+        }
+
         r = system(("/usr/sbin/client_drivshell knet netif create port=" +
                     std::to_string(port_id) + " ifname=" + port_name +
                     " mac=" + mac_string + " keeprxtag=yes")

src/netlink/tap_manager.cc
@@ -64,7 +64,13 @@ int tap_manager::create_portdev(uint32_t port_id, const std::string &port_name,
         auto rv = port_names2id.emplace(std::make_pair(port_name, port_id));
 
         if (!rv.second) {
-          LOG(FATAL) << __FUNCTION__ << ": failed to insert";
+          LOG(FATAL) << __FUNCTION__ << ": failed to insert port name";
+        }
+
+        auto rv2 = id_to_hwaddr.emplace(std::make_pair(port_id, hwaddr));
+
+        if (!rv2.second) {
+          LOG(FATAL) << __FUNCTION__ << ": failed to insert hwaddr";
         }
 
         dev->tap_open();

src/netlink/port_manager.h
@@ -68,10 +68,21 @@ public:
     }
   }
 
+  const rofl::caddress_ll get_hwaddr(uint32_t port_id) const noexcept {
+    // XXX TODO add assert wrt threading
+    auto it = id_to_hwaddr.find(port_id);
+    if (it == id_to_hwaddr.end()) {
+      return nulladdr;
+    } else {
+      return it->second;
+    }
+  }
+
   void clear() noexcept {
     std::lock_guard<std::mutex> lock(tn_mutex);
     ifindex_to_id.clear();
     id_to_ifindex.clear();
+    id_to_hwaddr.clear();
   }
 
   virtual int change_port_status(const std::string name, bool status) = 0;
@@ -95,6 +106,9 @@ protected:
   // only accessible from cnetlink
   std::map<int, uint32_t> ifindex_to_id;
   std::map<uint32_t, int> id_to_ifindex;
+  std::map<uint32_t, rofl::caddress_ll> id_to_hwaddr;
+
+  const rofl::caddress_ll nulladdr;
 };
 
 } // namespace basebox

commit 56845cda6a97d721739c92351aadc7a4aad77dcb

{tap,port}_manager: drop id_hwaddr entry on port removal
Make sure to remove id_hwaddr entries on in ::portdev_destroy(), else
baseboxd will crash when trying to recreate them after a OpenFlow
disconnect.

Easily forcable by restarting ofagent.

Fixes the following error:

    May 26 13:46:38 delta-ag7648 baseboxd[3470]: F20230526 13:46:38.699759  3485 tap_manager.cc:73] create_portdev: failed to insert hwaddr
    May 26 13:46:38 delta-ag7648 baseboxd[3470]: *** Check failure stack trace: ***
    May 26 13:46:38 delta-ag7648 baseboxd[3470]:     @     0x7f4c92e7804d  google::LogMessage::Fail()
    May 26 13:46:38 delta-ag7648 baseboxd[3470]:     @     0x7f4c92e7a5c6  google::LogMessage::SendToLog()
    May 26 13:46:38 delta-ag7648 baseboxd[3470]:     @     0x7f4c92e77b8a  google::LogMessage::Flush()
    May 26 13:46:38 delta-ag7648 baseboxd[3470]:     @     0x7f4c92e7ac39  google::LogMessageFatal::~LogMessageFatal()
    May 26 13:46:38 delta-ag7648 baseboxd[3470]:     @     0x561eca1cd60c  (unknown)
    May 26 13:46:38 delta-ag7648 baseboxd[3470]:     @     0x561eca18f0e5  (unknown)
    May 26 13:46:38 delta-ag7648 baseboxd[3470]:     @     0x561eca1db4d1  (unknown)
    May 26 13:46:38 delta-ag7648 baseboxd[3470]:     @     0x7f4c922e20cf  rofl::crofdpt::handle_recv()
    May 26 13:46:38 delta-ag7648 baseboxd[3470]:     @     0x7f4c922e6505  (unknown)
    May 26 13:46:38 delta-ag7648 baseboxd[3470]:     @     0x7f4c9230624b  rofl::crofconn::handle_rx_messages()
    May 26 13:46:38 delta-ag7648 baseboxd[3470]:     @     0x7f4c92339093  rofl::cthread::handle_wakeup()
    May 26 13:46:38 delta-ag7648 baseboxd[3470]:     @     0x7f4c92339728  rofl::cthread::run_loop()
    May 26 13:46:38 delta-ag7648 baseboxd[3470]:     @     0x7f4c91913a72  (unknown)
    May 26 13:46:38 delta-ag7648 baseboxd[3470]:     @     0x7f4c91995930  (unknown)

Fixes: 4e4576a798bc ("port_manager: keep track of original MAC address of ports")
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/netlink/knet_manager.cc
@@ -142,9 +142,16 @@ int knet_manager::destroy_portdev(uint32_t port_id,
 
   uint32_t netif_id = it->second;
 
-  // drop port from name mapping
   std::lock_guard<std::mutex> lock{tn_mutex};
   port_deleted.push_back(port_id);
+
+  // drop port from hw addr mapping
+  auto hw_addr_it = id_to_hwaddr.find(port_id);
+  if (hw_addr_it != id_to_hwaddr.end()) {
+    id_to_hwaddr.erase(hw_addr_it);
+  }
+
+  // drop port from name mapping
   auto port_names_it = port_names2id.find(port_name);
 
   if (port_names_it != port_names2id.end()) {

src/netlink/tap_manager.cc
@@ -107,9 +107,16 @@ int tap_manager::destroy_portdev(uint32_t port_id,
     return 0;
   }
 
-  // drop port from name mapping
   std::lock_guard<std::mutex> lock{tn_mutex};
   port_deleted.push_back(port_id);
+
+  // drop port from hw addr mapping
+  auto hw_addr_it = id_to_hwaddr.find(port_id);
+  if (hw_addr_it != id_to_hwaddr.end()) {
+    id_to_hwaddr.erase(hw_addr_it);
+  }
+
+  // drop port from name mapping
   auto tap_names_it = port_names2id.find(port_name);
 
   if (tap_names_it != port_names2id.end()) {