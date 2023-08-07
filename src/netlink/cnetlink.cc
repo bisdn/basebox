bisdn/basebox

commit 935d0e6fbd9a958a32189638be78ff5fbc7a2cc4

cnetlink: ignore bridge attachment for untracked interfaces
Our code adding IP addresses and routes has checks for the bridge having
actual port or bond interfaces attached, else they will get ignored.

So we need to make sure that we do not create the bridge too early, and
only do so when the attached interface is one we track, i.e. has a
port_id.

Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/netlink/cnetlink.cc
@@ -1196,6 +1196,14 @@ void cnetlink::link_created(rtnl_link *link) noexcept {
         return;
       }
 
+      // we only care if we attach a link that is backed by openflow,
+      // i.e. a tap device or a bond with attached tap devices
+      if (get_port_id(link) == 0) {
+        VLOG(1) << __FUNCTION__ << ": ignoring untracked interface "
+                << rtnl_link_get_name(link);
+        break;
+      }
+
       auto br_link = get_link_by_ifindex(rtnl_link_get_master(link));
       LOG(INFO) << __FUNCTION__ << ": using bridge "
                 << OBJ_CAST(br_link.get());

commit 7275df823a6f39324f53a0eb182f050e2319a656

cnetlink::link_created(): use is_switch_interface()
When attaching VxLAN interfaces to a bridge, we need to inform the
bridge, and by extension, the VxLAN handling code. With commit
935d0e6fbd9a ("cnetlink: ignore bridge attachment for untracked
interfaces"), this was broken since we started checking for a port_id,
which VxLAN interfaces do not have.

This broke VxLAN setup in various instances, e.g. when the VxLAN
interface was only attached after the access port as added to the
bridge.

To make sure we properly detect VxLAN interfaces, we need to convert the
bridge member link to a base link, as the bridge member link does not
have any of the link type specific attributes like VNI for VxLAN
interfaces.

Fixes: 935d0e6fbd9a ("cnetlink: ignore bridge attachment for untracked interfaces")
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/netlink/cnetlink.cc
@@ -1299,9 +1299,13 @@ void cnetlink::link_created(rtnl_link *link) noexcept {
         return;
       }
 
+      // get the base link instead of the bridged link object
+      rtnl_link *base_link = get_link(rtnl_link_get_ifindex(link), AF_UNSPEC);
+
       // we only care if we attach a link that is backed by openflow,
-      // i.e. a tap device or a bond with attached tap devices
-      if (get_port_id(link) == 0) {
+      // i.e. a tap device, a bond with attached tap devices, or vxlan
+      // interfaces
+      if (!is_switch_interface(base_link)) {
         VLOG(1) << __FUNCTION__ << ": ignoring untracked interface "
                 << rtnl_link_get_name(link);
         break;