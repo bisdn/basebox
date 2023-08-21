bisdn/basebox

commit 3e4adbbf862a82f923baae3dc92511cb75ca8532

nbi_impl: start netlink handling as early as possible
Starting netlink handling by the time the switch goes to STATE_UP is too
late, by that time we might have already received multiple port_status
messages.

Instead just start it on instantiation, like we stop it in destruction.
The additional netlink messages we capture should be harmless, as they
will be for devices we do not care about.

This should really fix the missed netlink messages on connection
establishment, as netlink handling will now be always active.

Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/netlink/nbi_impl.cc
@@ -17,6 +17,7 @@ nbi_impl::nbi_impl(std::shared_ptr<cnetlink> nl,
                    std::shared_ptr<tap_manager> tap_man)
     : nl(nl), tap_man(tap_man) {
   nl->set_tapmanager(tap_man);
+  nl->start();
 }
 
 nbi_impl::~nbi_impl() { nl->stop(); }
@@ -31,12 +32,9 @@ void nbi_impl::register_switch(switch_interface *swi) noexcept {
 void nbi_impl::switch_state_notification(enum switch_state state) noexcept {
   switch (state) {
   case SWITCH_STATE_UP:
-    nl->start();
-    break;
   case SWITCH_STATE_DOWN:
   case SWITCH_STATE_FAILED:
   case SWITCH_STATE_UNKNOWN:
-    nl->stop();
     break;
   default:
     LOG(FATAL) << __FUNCTION__ << ": invalid state";

commit 5762018c26708cfed7aad341da79a4605e8b471d

cnetlink: defer lo scan until switch is connected
Don't scan lo for ip addresses before we have a working openflow
connection, we need it to setup proper flows for its IP addresses.

Fixes: 3e4adbbf862a ("nbi_impl: start netlink handling as early as possible")
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/netlink/cnetlink.cc
@@ -795,7 +795,6 @@ void cnetlink::handle_wakeup(rofl::cthread &thread) {
 
   switch (state) {
   case NL_STATE_INIT:
-    init_subsystems();
     state = NL_STATE_RUNNING;
     break;
   case NL_STATE_RUNNING:
@@ -1724,6 +1723,8 @@ void cnetlink::stop() noexcept {
   state = NL_STATE_SHUTDOWN;
   thread.wakeup(this);
 }
+
+void cnetlink::switch_connected() noexcept { init_subsystems(); }
 
 bool cnetlink::is_bridge_configured(rtnl_link *l) {
   assert(l);

src/netlink/cnetlink.h
@@ -96,6 +96,7 @@ public:
   void unregister_switch(switch_interface *) noexcept;
   void start() noexcept;
   void stop() noexcept;
+  void switch_connected() noexcept;
 
   static void nl_cb_v2(struct nl_cache *cache, struct nl_object *old_obj,
                        struct nl_object *new_obj, uint64_t diff, int action,

src/netlink/nbi_impl.cc
@@ -32,6 +32,8 @@ void nbi_impl::register_switch(switch_interface *swi) noexcept {
 void nbi_impl::switch_state_notification(enum switch_state state) noexcept {
   switch (state) {
   case SWITCH_STATE_UP:
+    nl->switch_connected();
+    break;
   case SWITCH_STATE_DOWN:
   case SWITCH_STATE_FAILED:
   case SWITCH_STATE_UNKNOWN: