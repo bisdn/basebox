cb461a4ab4590b9e386afea33555e2d041b2b1fa
Jonas Gorski <jonas.gorski@bisdn.de>
2023-11-13 12:24:49 +0100

    nl_output: add helpers for various rtnl_* structs
    Instead of forcing users to OBJ_CAST() depending on the type, add
    helpers for the types to automatically OBJ_CAST().
    
    Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

--- src/netlink/nl_output.h
+++ src/netlink/nl_output.h
@@ -14,6 +14,24 @@ std::ostream &operator<<(std::ostream &stream, struct nl_object *n);
 
 std::ostream &operator<<(std::ostream &stream, struct rtnl_nexthop *nh);
 
+inline std::ostream &operator<<(std::ostream &stream, struct rtnl_addr *addr) {
+  return operator<<(stream, OBJ_CAST(addr));
+}
+
+inline std::ostream &operator<<(std::ostream &stream, struct rtnl_link *link) {
+  return operator<<(stream, OBJ_CAST(link));
+}
+
+inline std::ostream &operator<<(std::ostream &stream,
+                                struct rtnl_neigh *neigh) {
+  return operator<<(stream, OBJ_CAST(neigh));
+}
+
+inline std::ostream &operator<<(std::ostream &stream,
+                                struct rtnl_route *route) {
+  return operator<<(stream, OBJ_CAST(route));
+}
+
 inline std::string_view safe_string_view(const char *str) {
   return std::string_view(str ? str : "(null)");
 }
