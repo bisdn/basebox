bisdn/basebox

commit ec04fe2d229efb776ac90db1b0eda6761d6d7810

vxlan: add existing neighbors for new endpoint

src/netlink/nl_vxlan.cc
@@ -253,7 +253,7 @@ int nl_vxlan::init() {
       rv = add_l2_neigh(neigh, link, br_link);
       if (rv < 0) {
         LOG(ERROR) << __FUNCTION__ << ": failed (rv=" << rv
-                   << ") to add l2 neigh" << OBJ_CAST(link);
+                   << ") to add l2 neigh " << OBJ_CAST(link);
       }
     }
   }
@@ -642,6 +642,40 @@ int nl_vxlan::create_endpoint(rtnl_link *vxlan_link, rtnl_link *br_link,
       sw->tunnel_port_tenant_remove(lport_id, tunnel_id);
       delete_endpoint(vxlan_link, local_.get(), remote_addr);
       delete_next_hop(next_hop_id);
+    }
+  }
+
+  // get neighs on vxlan link
+  std::deque<rtnl_neigh *> neighs;
+  std::unique_ptr<rtnl_neigh, decltype(&rtnl_neigh_put)> neigh_filter(
+      rtnl_neigh_alloc(), &rtnl_neigh_put);
+
+  rtnl_neigh_set_ifindex(neigh_filter.get(), rtnl_link_get_ifindex(vxlan_link));
+  // dst must be before setting family
+  rtnl_neigh_set_dst(neigh_filter.get(), remote_addr);
+  rtnl_neigh_set_family(neigh_filter.get(), AF_BRIDGE);
+
+  auto c = nl->get_cache(cnetlink::NL_NEIGH_CACHE);
+  nl_cache_foreach_filter(c, OBJ_CAST(neigh_filter.get()),
+                          [](struct nl_object *obj, void *arg) {
+                            auto n = NEIGH_CAST(obj);
+
+                            // ignore remotes
+                            if (nl_addr_iszero(rtnl_neigh_get_lladdr(n)))
+                              return;
+
+                            VLOG(3) << "found neigh for endpoint " << obj;
+                            auto neighs =
+                                static_cast<std::deque<rtnl_neigh *> *>(arg);
+                            neighs->push_back(n);
+                          },
+                          &neighs);
+
+  for (auto neigh : neighs) {
+    rv = add_l2_neigh(neigh, lport_id, tunnel_id);
+    if (rv < 0) {
+      LOG(ERROR) << __FUNCTION__ << ": failed (rv=" << rv
+                 << ") to add l2 neigh " << OBJ_CAST(link);
     }
   }
 

commit 053974555aaeb508a3af3dcf61b44f9ebc9774d1

nl_vxlan::create_endpoint(): fix error log on add neigh failure
There is no link variable in this nl_vxlan::create_endpoint(), only
vxlan_link and br_link. This was hidden due to the OBJ_CAST(), but when
the OBJ_CAST() was dropped, g++ started to complain.

Fix this by replacing link with neigh, since neigh is what the text
before the variable says.

Fixes the following warning:

../git/src/netlink/nl_vxlan.cc: In member function 'int basebox::nl_vxlan::create_endpoint(rtnl_link*, rtnl_link*, nl_addr*)':
../git/src/netlink/nl_vxlan.cc:674:45: warning: the address of 'int link(const char*, const char*)' will never be NULL [-Waddress]
  674 |                  << ") to add l2 neigh " << link;
      |                                             ^~~~

Fixes: ec04fe2d229e ("vxlan: add existing neighbors for new endpoint")
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/netlink/nl_vxlan.cc
@@ -671,7 +671,7 @@ int nl_vxlan::create_endpoint(rtnl_link *vxlan_link, rtnl_link *br_link,
     rv = add_l2_neigh(neigh, lport_id, tunnel_id);
     if (rv < 0) {
       LOG(ERROR) << __FUNCTION__ << ": failed (rv=" << rv
-                 << ") to add l2 neigh " << link;
+                 << ") to add l2 neigh " << neigh;
     }
   }
 