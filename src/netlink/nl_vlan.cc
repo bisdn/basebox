bisdn/basebox

commit 57832c0c95643a9e4e5a0a51770abd0111f3accc

nl_vlan: factor out enabling/disabling vlan flows/groups
To be able to reuse the enabling/disabling vlan flows code, move it into
its own function.

Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/netlink/nl_vlan.cc
@@ -46,50 +46,15 @@ int nl_vlan::add_vlan(rtnl_link *link, uint16_t vid, bool tagged) {
     return 0;
   }
 
-  int rv = add_ingress_vlan(port_id, vid, tagged, vrf_id);
+  int rv = enable_vlan(port_id, vid, tagged, vrf_id);
   if (rv < 0) {
-    LOG(ERROR) << __FUNCTION__ << ": failed to setup ingress vlan " << vid
+    LOG(ERROR) << __FUNCTION__ << ": failed to enable vlan " << vid
                << (tagged ? " (tagged)" : " (untagged)")
                << " on port_id=" << port_id << "; rv=" << rv;
-    return rv;
-  }
-
-  // setup egress interface
-  rv = swi->egress_port_vlan_add(port_id, vid, !tagged);
-
-  if (rv < 0) {
-    LOG(ERROR) << __FUNCTION__ << ": failed to setup egress vlan " << vid
-               << (tagged ? " (tagged)" : " (untagged)")
-               << " on port_id=" << port_id << "; rv=" << rv;
-    (void)remove_ingress_vlan(port_id, vid, tagged, vrf_id);
-
     return rv;
   }
 
   port_vlan.emplace(key, 1);
-
-  if (nbi::get_port_type(port_id) == nbi::port_type_lag) {
-    auto members = nl->get_bond_members_by_port_id(port_id);
-    for (auto mem : members) {
-      rv = swi->egress_port_vlan_add(mem, vid, !tagged);
-      if (rv < 0) {
-        LOG(ERROR) << __FUNCTION__ << ": failed to setup egress vlan " << vid
-                   << (tagged ? " (tagged)" : " (untagged)")
-                   << " on port_id=" << port_id << "; rv=" << rv;
-        break;
-      }
-    }
-
-    if (rv < 0) {
-      for (auto mem : members) {
-        (void)swi->egress_port_vlan_remove(mem, vid);
-      }
-      (void)swi->egress_port_vlan_remove(port_id, vid);
-      (void)remove_ingress_vlan(port_id, vid, tagged, vrf_id);
-
-      return rv;
-    }
-  }
 
   return rv;
 }
@@ -226,7 +191,72 @@ int nl_vlan::remove_vlan(rtnl_link *link, uint16_t vid, bool tagged) {
   port_vlan.erase(refcount);
 
   // remove vid at ingress
-  rv = remove_ingress_vlan(port_id, vid, tagged, vrf_id);
+  rv = disable_vlan(port_id, vid, tagged, vrf_id);
+  if (rv < 0) {
+    LOG(ERROR) << __FUNCTION__ << ": failed with rv=" << rv
+               << " to remove vid=" << vid << "(tagged=" << tagged
+               << ") of link " << OBJ_CAST(link);
+  }
+
+  return rv;
+}
+
+int nl_vlan::enable_vlan(uint32_t port_id, uint16_t vid, bool tagged,
+                         uint16_t vrf_id) {
+  assert(swi);
+
+  int rv = add_ingress_vlan(port_id, vid, tagged, vrf_id);
+  if (rv < 0) {
+    LOG(ERROR) << __FUNCTION__ << ": failed to setup ingress vlan " << vid
+               << (tagged ? " (tagged)" : " (untagged)")
+               << " on port_id=" << port_id << "; rv=" << rv;
+    return rv;
+  }
+
+  // setup egress interface
+  rv = swi->egress_port_vlan_add(port_id, vid, !tagged);
+
+  if (rv < 0) {
+    LOG(ERROR) << __FUNCTION__ << ": failed to setup egress vlan " << vid
+               << (tagged ? " (tagged)" : " (untagged)")
+               << " on port_id=" << port_id << "; rv=" << rv;
+    (void)remove_ingress_vlan(port_id, vid, tagged, vrf_id);
+
+    return rv;
+  }
+
+  if (nbi::get_port_type(port_id) == nbi::port_type_lag) {
+    auto members = nl->get_bond_members_by_port_id(port_id);
+    for (auto mem : members) {
+      rv = swi->egress_port_vlan_add(mem, vid, !tagged);
+      if (rv < 0) {
+        LOG(ERROR) << __FUNCTION__ << ": failed to setup egress vlan " << vid
+                   << (tagged ? " (tagged)" : " (untagged)")
+                   << " on port_id=" << port_id << "; rv=" << rv;
+        break;
+      }
+    }
+
+    if (rv < 0) {
+      for (auto mem : members) {
+        (void)swi->egress_port_vlan_remove(mem, vid);
+      }
+      (void)swi->egress_port_vlan_remove(port_id, vid);
+      (void)remove_ingress_vlan(port_id, vid, tagged, vrf_id);
+
+      return rv;
+    }
+  }
+
+  return rv;
+}
+
+int nl_vlan::disable_vlan(uint32_t port_id, uint16_t vid, bool tagged,
+                          uint16_t vrf_id) {
+  assert(swi);
+
+  // remove vid at ingress
+  int rv = remove_ingress_vlan(port_id, vid, tagged, vrf_id);
   if (rv < 0) {
     LOG(ERROR) << __FUNCTION__ << ": failed with rv=" << rv
                << " to remove vid=" << vid << "(tagged=" << tagged

src/netlink/nl_vlan.h
@@ -57,6 +57,11 @@ private:
   static const uint16_t vid_high = 0xfff;
   static const uint16_t default_vid = vid_low;
 
+  int enable_vlan(uint32_t port_id, uint16_t vid, bool tagged,
+                  uint16_t vrf_id = 0);
+  int disable_vlan(uint32_t port_id, uint16_t vid, bool tagged,
+                   uint16_t vrf_id = 0);
+
   // ifindex - vlan - refcount
   std::map<std::pair<uint32_t, uint16_t>, uint32_t> port_vlan;
   // vlan - vrf

commit 3369772feee8d482957b5f5dbffa42197935574a

nl_vlan::disable_vlan(): properly log port in error log
When factoring out the disable vlan code into its functions the error
logs weren't updated from link to port_id, but due to the OBJ_CAST()
this was hidden. After dropping the OBJ_CAST(), g++ started complaining,
so fix the error logs to use port_id.

Fixes the following build warnings:

../git/src/netlink/nl_vlan.cc: In member function 'int basebox::nl_vlan::disable_vlan(uint32_t, uint16_t, bool, uint16_t)':
../git/src/netlink/nl_vlan.cc:263:35: warning: the address of 'int link(const char*, const char*)' will never be NULL [-Waddress]
  263 |                << ") of link " << link;
      |                                   ^~~~
../git/src/netlink/nl_vlan.cc:273:19: warning: the address of 'int link(const char*, const char*)' will never be NULL [-Waddress]
  273 |                << link;
      |                   ^~~~
../git/src/netlink/nl_vlan.cc:288:62: warning: the address of 'int link(const char*, const char*)' will never be NULL [-Waddress]
  288 |                << " to remove vid=" << vid << " of link " << link;
      |

Fixes: 57832c0c9564 ("nl_vlan: factor out enabling/disabling vlan flows/groups")
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/netlink/nl_vlan.cc
@@ -260,7 +260,7 @@ int nl_vlan::disable_vlan(uint32_t port_id, uint16_t vid, bool tagged,
   if (rv < 0) {
     LOG(ERROR) << __FUNCTION__ << ": failed with rv=" << rv
                << " to remove vid=" << vid << "(tagged=" << tagged
-               << ") of link " << link;
+               << ") of port_id " << port_id;
     return rv;
   }
 
@@ -269,8 +269,8 @@ int nl_vlan::disable_vlan(uint32_t port_id, uint16_t vid, bool tagged,
 
   if (rv < 0) {
     LOG(ERROR) << __FUNCTION__ << ": failed with rv= " << rv
-               << " to remove all bridge entries in vid=" << vid << " link "
-               << link;
+               << " to remove all bridge entries in vid=" << vid << " port_id "
+               << port_id;
     return rv;
   }
 
@@ -285,7 +285,7 @@ int nl_vlan::disable_vlan(uint32_t port_id, uint16_t vid, bool tagged,
 
   if (rv < 0) {
     LOG(ERROR) << __FUNCTION__ << ": failed with rv= " << rv
-               << " to remove vid=" << vid << " of link " << link;
+               << " to remove vid=" << vid << " of port_id " << port_id;
     return rv;
   }
 