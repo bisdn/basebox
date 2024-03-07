bisdn/basebox

commit a566d4caa1b0f8a05e179f7fc4d1196740bd3eb5

nl_bridge::fdb_timeout(): check ifindex of hit before deletion
nl_cache_search() uses nl_object_identical()[1] to lookup the target
object. This limits the check to unique attributes, which does not
include the ifindex [2].

Therefore nl_cache_search() may return an object for a different port,
in case it moved, which we then may wrongly remove from the cache.

So add a check for the ifindex of the hit before attempting to delete
any neighbors or cache entries.

We could have also alternatively used nl_cache_find() which uses all
attributes, but this function iterates over the whole cache intead of
using the hashtable if possible, so would be much less performant.

[1] https://github.com/thom311/libnl/blob/libnl3_7_0/lib/cache.c#L1113-L1114
[2] https://github.com/thom311/libnl/blob/libnl3_7_0/lib/route/neigh.c#L323

Fixes: 291591d6f313 ("l2 aging added")
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/netlink/nl_bridge.cc
@@ -827,20 +827,19 @@ int nl_bridge::fdb_timeout(rtnl_link *br_link, uint16_t vid,
   std::unique_ptr<nl_addr, decltype(&nl_addr_put)> h_src(
       nl_addr_build(AF_LLC, mac.somem(), mac.memlen()), nl_addr_put);
 
-  rtnl_neigh_set_ifindex(n.get(), rtnl_link_get_ifindex(br_link));
   rtnl_neigh_set_master(n.get(), rtnl_link_get_master(br_link));
   rtnl_neigh_set_family(n.get(), AF_BRIDGE);
   rtnl_neigh_set_vlan(n.get(), vid);
   rtnl_neigh_set_lladdr(n.get(), h_src.get());
   rtnl_neigh_set_flags(n.get(), NTF_MASTER | NTF_EXT_LEARNED);
-  rtnl_neigh_set_state(n.get(), NUD_REACHABLE);
 
   // find entry in local l2_cache
   std::unique_ptr<rtnl_neigh, decltype(&rtnl_neigh_put)> n_lookup(
       NEIGH_CAST(nl_cache_search(l2_cache.get(), OBJ_CAST(n.get()))),
       rtnl_neigh_put);
 
-  if (n_lookup) {
+  if (n_lookup && rtnl_neigh_get_ifindex(n_lookup.get()) ==
+                      rtnl_link_get_ifindex(br_link)) {
     // * remove l2 entry from kernel
     nl_msg *msg = nullptr;
     rtnl_neigh_build_delete_request(n.get(), NLM_F_REQUEST, &msg);

commit 09bf1a6beb829f2b103c2affc99dc4cfb3083e4f

nl_bridge: fix removing aged out neighbors from fdb
When fixing moving neighbors between ports we removed the ifindex from
the filter neigh to find a neighbor at any port with the mac address.

But since we use the same neigh to construct the fdb delete message,
this caused the message to miss the ifindex, and the kernel was
rejecting the deletion. This resulting in us not updating out local
cache of known layer 2 neighbors, and failing to relearn them.

Fix this by just using the found entry from our local cache, n_lookup.

Fixes: a566d4caa1b0 ("nl_bridge::fdb_timeout(): check ifindex of hit before deletion")
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/netlink/nl_bridge.cc
@@ -837,7 +837,7 @@ int nl_bridge::fdb_timeout(rtnl_link *br_link, uint16_t vid,
                       rtnl_link_get_ifindex(br_link)) {
     // * remove l2 entry from kernel
     nl_msg *msg = nullptr;
-    rtnl_neigh_build_delete_request(n.get(), NLM_F_REQUEST, &msg);
+    rtnl_neigh_build_delete_request(n_lookup.get(), NLM_F_REQUEST, &msg);
     assert(msg);
 
     // send the message and create new fdb entry