bisdn/basebox

commit 0a04f524bf29103b46ba8eb394d96061349a1904

nl_l3: add_l3_neigh: fix updating routes waiting on unresolved nexthops
When a route has a unresolved nexthop, we add the route's destination to
a list of nets waiting to be resolved.

When adding a l3 neighbor, we check that list, but the destination of a
neighbor will be the address of it (so the desired nexthop), not the
network waiting for it to be resolved. So we need to store the
unresolved nexthop's address as well.

Instead of expanding the net_resolved_callbacks, there actually already
is a perfectly fine way of doing that by nl_l3 implementing nh_reachable
and registering itself, then adding the route in the callback function.

This way we get all the info, and can remove the net_resolved_callback
completely, as we don't need it anymore.

Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/netlink/nl_l3.cc
@@ -668,34 +668,6 @@ int nl_l3::add_l3_neigh(struct rtnl_neigh *n) {
     }
   }
 
-  for (auto cb = std::begin(net_resolved_callbacks);
-       cb != std::end(net_resolved_callbacks);) {
-    VLOG(2) << __FUNCTION__ << " neigh ifindex=" << rtnl_neigh_get_ifindex(n)
-            << " cb ifindex=" << cb->ifindex << " neigh add=" << cb->addr
-            << " neigh dst=" << rtnl_neigh_get_dst(n);
-    if (cb->ifindex == rtnl_neigh_get_ifindex(n) &&
-        nl_addr_cmp_prefix(cb->addr, rtnl_neigh_get_dst(n)) == 0) {
-
-      // query the kernel for the correct route matching the dst addr
-      nl_route_query rq;
-      auto rt = rq.query_route(cb->addr);
-
-      auto ad = rtnl_route_get_dst(rt);
-      nl_addr_set_prefixlen(
-          ad,
-          nl_addr_get_prefixlen(cb->addr)); // guarantee the correct prefixlen
-
-      add_l3_unicast_route(rt, true);
-      nl_object_put(
-          OBJ_CAST(rt)); // return object has to be freed using nl_object_put
-
-      cb = net_resolved_callbacks.erase(cb); // erase invalidates the pointer,
-                                             // so the value must be returned
-    } else {
-      ++cb;
-    }
-  }
-
   return rv;
 }
 
@@ -1293,10 +1265,6 @@ void nl_l3::notify_on_nh_reachable(nh_reachable *f,
   nh_callbacks.emplace_back(f, p);
 }
 
-void nl_l3::notify_on_nh_resolved(struct net_params p) noexcept {
-  net_resolved_callbacks.emplace_back(p);
-}
-
 int nl_l3::get_l3_interface_id(int ifindex, const struct nl_addr *s_mac,
                                const struct nl_addr *d_mac,
                                uint32_t *l3_interface_id, uint16_t vid) {
@@ -1503,6 +1471,19 @@ int nl_l3::del_l3_ecmp_route(rtnl_route *r,
   return -EINVAL;
 }
 
+void nl_l3::nh_reachable_notification(struct nh_params p) noexcept {
+  nl_route_query rq;
+  auto rt = rq.query_route(p.np.addr);
+  auto ad = rtnl_route_get_dst(rt);
+
+  // guarantee the correct prefixlen
+  nl_addr_set_prefixlen(ad, nl_addr_get_prefixlen(p.np.addr));
+
+  add_l3_unicast_route(rt, true);
+  // return object has to be freed using nl_object_put
+  nl_object_put(OBJ_CAST(rt));
+}
+
 int nl_l3::add_l3_unicast_route(rtnl_route *r, bool update_route) {
   assert(r);
   int nnhs = rtnl_route_get_nnexthops(r);
@@ -1537,7 +1518,8 @@ int nl_l3::add_l3_unicast_route(rtnl_route *r, bool update_route) {
     if (!is_ipv6_link_local_address(rtnl_route_get_dst(r)) &&
         !is_ipv6_multicast_address(rtnl_route_get_dst(r)) &&
         rtnl_route_get_scope(r) != RT_SCOPE_LINK)
-      notify_on_nh_resolved(net_params(rtnl_route_get_dst(r), nh.ifindex));
+      notify_on_nh_reachable(
+          this, nh_params{net_params{rtnl_route_get_dst(r), nh.ifindex}, nh});
   }
 
   VLOG(2) << __FUNCTION__ << ": got " << neighs.size() << " neighbours ("
@@ -1643,15 +1625,20 @@ int nl_l3::del_l3_unicast_route(rtnl_route *r, bool keep_route) {
   VLOG(2) << __FUNCTION__ << ": number of next hops is "
           << rtnl_route_get_nnexthops(r);
 
-  // Cleanup the unresolved route on deletion
+  // Cleanup the unresolved route on deletion. Make sure to only remove the
+  // first hit, as we want to keep the second one that will have been added
+  // by add_l3_unicast_route() from update_l3_unicact_route().
+  // If this is an actual deletion, there should only be one anyway.
   for (auto i : unresolved_nh) {
-    auto it = std::find(net_resolved_callbacks.begin(),
-                        net_resolved_callbacks.end(), i.ifindex);
-    if (it == net_resolved_callbacks.end()) {
-      continue;
-    }
+    auto it = std::find_if(nh_callbacks.begin(), nh_callbacks.end(),
+                           [&](std::pair<nh_reachable *, nh_params> &cb) {
+                             return cb.first == this &&
+                                    cb.second.nh.ifindex == i.ifindex &&
+                                    !nl_addr_cmp_prefix(cb.second.np.addr, dst);
+                           });
 
-    net_resolved_callbacks.erase(it);
+    if (it != nh_callbacks.end())
+      nh_callbacks.erase(it);
   }
 
   if (neighs.size() == 0) {

src/netlink/nl_l3.h
@@ -31,7 +31,7 @@ class nl_vlan;
 class nl_bridge;
 class switch_interface;
 
-class nl_l3 {
+class nl_l3 : public nh_reachable {
 public:
   nl_l3(std::shared_ptr<nl_vlan> vlan, cnetlink *nl);
   ~nl_l3() {}
@@ -65,7 +65,8 @@ public:
 
   void notify_on_net_reachable(net_reachable *f, struct net_params p) noexcept;
   void notify_on_nh_reachable(nh_reachable *f, struct nh_params p) noexcept;
-  void notify_on_nh_resolved(struct net_params p) noexcept;
+
+  void nh_reachable_notification(struct nh_params) noexcept override;
 
 private:
   int get_l3_interface_id(int ifindex, const struct nl_addr *s_mac,
@@ -123,7 +124,6 @@ private:
   cnetlink *nl;
   std::deque<std::pair<net_reachable *, net_params>> net_callbacks;
   std::deque<std::pair<nh_reachable *, nh_params>> nh_callbacks;
-  std::deque<net_params> net_resolved_callbacks; // handle the unknown nexthops
   const uint8_t MAIN_ROUTING_TABLE = 254;
 };
 

commit 0f21cbee073ec75be8f0047591ad8ce13a3aa3f8

nl_l3: fix cleanup of nh_reachable notifications
Fix searching for nh_reachable notifications for deletion:

1. Do not use nl_addr_cmp_prefix(), this will only compare the minimum
   of prefixlengths of both, so if there is e.g. a default route, the
   comparison will always match. Use nl_addr_cmp(), which will do a full
   check, including comparing the prefixlengths.
2. Do a full nexthop check. There is no guarantee that there will always
   be only one nexthop per interface, so make sure the address is also
   the same.

Fixes: 0a04f524bf29 ("nl_l3: add_l3_neigh: fix updating routes waiting on unresolved nexthops")
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/netlink/nl_l3.cc
@@ -1998,9 +1998,8 @@ int nl_l3::del_l3_unicast_route(rtnl_route *r, bool keep_route) {
   for (auto i : unresolved_nh) {
     auto it = std::find_if(nh_callbacks.begin(), nh_callbacks.end(),
                            [&](std::pair<nh_reachable *, nh_params> &cb) {
-                             return cb.first == this &&
-                                    cb.second.nh.ifindex == i.ifindex &&
-                                    !nl_addr_cmp_prefix(cb.second.np.addr, dst);
+                             return cb.first == this && cb.second.nh == i &&
+                                    !nl_addr_cmp(cb.second.np.addr, dst);
                            });
 
     if (it != nh_callbacks.end())