bisdn/basebox

commit 590c33d200359ea4509d32d31dfa48956544fd58

nl_l3: handle neigh unreachable * Adds nh unreachable container to track nexthop unreachability changes * In case that nexthop is not anymore reachable, update route that has nexthop to point to controller and delete the reference * Thus, when NH becomes again reachable, we can update the egress reference and the route

src/netlink/nl_l3.cc
@@ -588,7 +588,7 @@ int nl_l3::del_l3_neigh_egress(struct rtnl_neigh *n) {
 
   // remove egress group reference
   int rv = del_l3_egress(ifindex, vid, s_mac, d_mac);
-  if (rv < 0) {
+  if (rv < 0 and rv != -EEXIST) {
     LOG(ERROR) << __FUNCTION__ << ": failed to add l3 egress";
   }
 
@@ -872,6 +872,22 @@ int nl_l3::del_l3_neigh(struct rtnl_neigh *n) {
   if (s_mac && d_mac)
     rv = del_l3_egress(ifindex, vid, s_mac, d_mac);
 
+  // if a route still exists thats pointing to the nexthop,
+  // update the route to point to controller and delete the nexthop egress 
+  // reference
+  if (rv == -EEXIST) {
+    for (auto cb = std::begin(nh_unreach_callbacks);
+         cb != std::end(nh_unreach_callbacks);) {
+      if (cb->second.nh.ifindex == rtnl_neigh_get_ifindex(n) &&
+          nl_addr_cmp(cb->second.nh.nh, rtnl_neigh_get_dst(n)) == 0) {
+        cb->first->nh_unreachable_notification(n, cb->second);
+        cb = nh_unreach_callbacks.erase(cb);
+      } else {
+        ++cb;
+      }
+    }
+  }
+
   return rv;
 }
 
@@ -959,8 +975,7 @@ int nl_l3::del_l3_egress(int ifindex, uint16_t vid, const struct nl_addr *s_mac,
         return rv;
       }
     }
-
-    return 0;
+    return -EEXIST;
   }
 
   LOG(ERROR) << __FUNCTION__
@@ -1275,6 +1290,11 @@ void nl_l3::notify_on_nh_reachable(nh_reachable *f,
   nh_callbacks.emplace_back(f, p);
 }
 
+void nl_l3::notify_on_nh_unreachable(nh_unreachable *f,
+                                     struct nh_params p) noexcept {
+  nh_unreach_callbacks.emplace_back(f, p);
+}
+
 int nl_l3::get_l3_interface_id(int ifindex, const struct nl_addr *s_mac,
                                const struct nl_addr *d_mac,
                                uint32_t *l3_interface_id, uint16_t vid) {
@@ -1494,6 +1514,14 @@ void nl_l3::nh_reachable_notification(struct nh_params p) noexcept {
   nl_object_put(OBJ_CAST(rt));
 }
 
+void nl_l3::nh_unreachable_notification(struct rtnl_neigh *n,
+                                        struct nh_params p) noexcept {
+  // TODO handle VRF
+  add_l3_unicast_route(p.np.addr, 0, false, true, 0);
+  del_l3_neigh_egress(n);
+  notify_on_nh_reachable(this, p);
+}
+
 int nl_l3::add_l3_unicast_route(rtnl_route *r, bool update_route) {
   assert(r);
   int nnhs = rtnl_route_get_nnexthops(r);
@@ -1566,6 +1594,13 @@ int nl_l3::add_l3_unicast_route(rtnl_route *r, bool update_route) {
 
     VLOG(2) << __FUNCTION__ << ": got l3_interface_id=" << l3_interface_id;
     l3_interfaces.emplace(l3_interface_id);
+
+    // Register route to track nexthop changes
+    struct nh_stub nh {
+      rtnl_neigh_get_dst(n), ifindex
+    };
+    notify_on_nh_unreachable(
+        this, nh_params{net_params{rtnl_route_get_dst(r), nh.ifindex}, nh});
   }
 
   switch (nnhs) {
@@ -1718,7 +1753,7 @@ int nl_l3::del_l3_unicast_route(rtnl_route *r, bool keep_route) {
     }
     rv = del_l3_egress(ifindex, vid, s_mac, d_mac);
 
-    if (rv < 0) {
+    if (rv < 0 and rv != -EEXIST) {
       // clean up
       LOG(ERROR) << __FUNCTION__ << ": del l3 egress failed for neigh "
                  << OBJ_CAST(n);

src/netlink/nl_l3.h
@@ -32,7 +32,7 @@ class nl_vlan;
 class nl_bridge;
 class switch_interface;
 
-class nl_l3 : public nh_reachable {
+class nl_l3 : public nh_reachable, public nh_unreachable {
 public:
   nl_l3(std::shared_ptr<nl_vlan> vlan, cnetlink *nl);
   ~nl_l3() {}
@@ -66,8 +66,11 @@ public:
 
   void notify_on_net_reachable(net_reachable *f, struct net_params p) noexcept;
   void notify_on_nh_reachable(nh_reachable *f, struct nh_params p) noexcept;
+  void notify_on_nh_unreachable(nh_unreachable *f, struct nh_params p) noexcept;
 
   void nh_reachable_notification(struct nh_params) noexcept override;
+  void nh_unreachable_notification(struct rtnl_neigh *,
+                                   struct nh_params) noexcept override;
 
 private:
   int get_l3_interface_id(int ifindex, const struct nl_addr *s_mac,
@@ -125,6 +128,7 @@ private:
   cnetlink *nl;
   std::list<std::pair<net_reachable *, net_params>> net_callbacks;
   std::list<std::pair<nh_reachable *, nh_params>> nh_callbacks;
+  std::list<std::pair<nh_unreachable *, nh_params>> nh_unreach_callbacks;
   const uint8_t MAIN_ROUTING_TABLE = 254;
 };
 

src/netlink/nl_l3_interfaces.h
@@ -73,4 +73,11 @@ public:
   virtual void nh_reachable_notification(struct nh_params) noexcept = 0;
 };
 
+class nh_unreachable {
+public:
+  virtual ~nh_unreachable() = default;
+  virtual void nh_unreachable_notification(struct rtnl_neigh *,
+                                           struct nh_params) noexcept = 0;
+};
+
 } // namespace basebox

commit 02904a860a8015c412e30e3a0ea05c4c1e7efb93

nl_l3::del_l3_unicast_route(): delete nh_unreach notifications
When deleting a route, we need to make sure to also remove any
next unreachable notifications we did, else we may call del_l3_neigh too
often.

Fixes: 590c33d20035 ("nl_l3: handle neigh unreachable * Adds nh unreachable container to track nexthop unreachability changes * In case that nexthop is not anymore reachable, update route that has nexthop to point to controller and delete the reference * Thus, when NH becomes again reachable, we can update the egress reference and the route")
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/netlink/nl_l3.cc
@@ -2055,6 +2055,20 @@ int nl_l3::del_l3_unicast_route(rtnl_route *r, bool keep_route) {
 
   // remove egress references
   for (auto n : neighs) {
+    int ifindex = rtnl_neigh_get_ifindex(n);
+    struct nl_addr *addr = rtnl_neigh_get_dst(n);
+
+    auto it =
+        std::find_if(nh_unreach_callbacks.begin(), nh_unreach_callbacks.end(),
+                     [&](std::pair<nh_unreachable *, nh_params> &cb) {
+                       return cb.first == this &&
+                              cb.second.nh.ifindex == ifindex &&
+                              !nl_addr_cmp(cb.second.nh.nh, addr) &&
+                              !nl_addr_cmp(cb.second.np.addr, dst);
+                     });
+
+    if (it != nh_unreach_callbacks.end())
+      nh_unreach_callbacks.erase(it);
     rv = del_l3_neigh_egress(n);
 
     if (rv < 0 and rv != -EEXIST) {