bisdn/basebox

commit ae62117d337c37be785d20712d42c4e3deaf2845

cnetlink: pass on layer 2 neigh updates
If a learned neighbor moves from one port to another, we will get an
update notification, so we need to handle it to properly update the flow
entry in that case.

Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/netlink/cnetlink.cc
@@ -1559,10 +1559,25 @@ void cnetlink::neigh_ll_created(rtnl_neigh *neigh) noexcept {
   }
 }
 
-void cnetlink::neigh_ll_updated(__attribute__((unused)) rtnl_neigh *old_neigh,
-                                __attribute__((unused))
+void cnetlink::neigh_ll_updated(rtnl_neigh *old_neigh,
                                 rtnl_neigh *new_neigh) noexcept {
-  LOG(WARNING) << __FUNCTION__ << ": neighbor update not supported";
+  if (!check_ll_neigh(old_neigh) || !check_ll_neigh(new_neigh))
+    return;
+
+  int old_ifindex = rtnl_neigh_get_ifindex(old_neigh);
+  rtnl_link *old_base_link =
+      get_link(old_ifindex, AF_UNSPEC); // either tap, vxlan, ...
+  int new_ifindex = rtnl_neigh_get_ifindex(new_neigh);
+  rtnl_link *new_base_link =
+      get_link(new_ifindex, AF_UNSPEC); // either tap, vxlan, ...
+
+  if (get_link_type(old_base_link) == LT_VXLAN ||
+      get_link_type(new_base_link) == LT_VXLAN) {
+    LOG(WARNING) << __FUNCTION__
+                 << ": neighbor update for VXLAN neighbors not supported";
+  } else {
+    bridge->add_neigh_to_fdb(new_neigh, true);
+  }
 }
 
 void cnetlink::neigh_ll_deleted(rtnl_neigh *neigh) noexcept {

commit 3b3a65e3cccaaf0804a22efb7f6296562ba6a7bc

cnetlink: properly handle ll neigh handable changes
With ae62117d337c ("cnetlink: pass on layer 2 neigh updates"), we make
sure to handle updates only on neighs we support, so we check both old
and new to be usable.

But this misses the transitions where a ll neigh becomes usable due to
updates or vice versa.

Fix this by treating those changes as a new or deleted ll neigh insted
of ignoring it alltogether.

Fixes: ae62117d337c ("cnetlink: pass on layer 2 neigh updates")
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/netlink/cnetlink.cc
@@ -1750,8 +1750,24 @@ void cnetlink::neigh_ll_created(rtnl_neigh *neigh) noexcept {
 
 void cnetlink::neigh_ll_updated(rtnl_neigh *old_neigh,
                                 rtnl_neigh *new_neigh) noexcept {
-  if (!check_ll_neigh(old_neigh) || !check_ll_neigh(new_neigh))
+  bool handle_old = check_ll_neigh(old_neigh);
+  bool handle_new = check_ll_neigh(new_neigh);
+
+  // nothing to do here
+  if (!handle_old && !handle_new)
     return;
+
+  // we ignored the previous one, so this is like a new neigh
+  if (!handle_old) {
+    neigh_ll_created(new_neigh);
+    return;
+  }
+
+  // we won't handle the neigh anymore
+  if (!handle_new) {
+    neigh_ll_deleted(old_neigh);
+    return;
+  }
 
   int old_ifindex = rtnl_neigh_get_ifindex(old_neigh);
   rtnl_link *old_base_link =