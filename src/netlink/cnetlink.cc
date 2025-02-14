bisdn/basebox

commit 824632b855fc1ae9912f8fc5a1eb1552f3b9bbb5

nbi_impl: initialize learning on connect
To ensure that ports work as expected, initialize learning on connect.

Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/netlink/nbi_impl.cc
@@ -52,6 +52,14 @@ void nbi_impl::port_notification(
     case PORT_EVENT_TABLE:
       switch (get_port_type(ntfy.port_id)) {
       case nbi::port_type_physical:
+        swi->port_set_learn(
+            ntfy.port_id,
+            switch_interface::
+                SAI_BRIDGE_PORT_FDB_LEARNING_MODE_FDB_LOG_NOTIFICATION);
+        swi->port_set_move_learn(
+            ntfy.port_id,
+            switch_interface::
+                SAI_BRIDGE_PORT_FDB_LEARNING_MODE_FDB_LOG_NOTIFICATION);
         port_man->create_portdev(ntfy.port_id, ntfy.name, ntfy.hwaddr, *this);
         break;
       default:

commit 7170e72d94ffb4c4e4716c3f84cd3084abf686f5

netlink: setup port learning from with the netlink thread
Port nbi_impl::port_notification() is handled in a different thread than
our netlink handling thread, so any gRPC calls it may do will race
against gRPC calls done by the netlink thread, and it seems our gRPC
code is not thread-safe.

So move the port learning setup to cnetlink::link_created(), so that we
avoid parallel gRPC calls.

Fixes broken bond interfaces when setup by systemd-networkd.

Fixes: 824632b855fc ("nbi_impl: initialize learning on connect")
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/netlink/cnetlink.cc
@@ -1473,6 +1473,12 @@ void cnetlink::link_created(rtnl_link *link) noexcept {
 
       port_man->set_offloaded(link, FLAGS_mark_fwd_offload);
       swi->port_set_config(port_id, port_man->get_hwaddr(port_id), false);
+      swi->port_set_learn(
+          port_id, switch_interface::
+                       SAI_BRIDGE_PORT_FDB_LEARNING_MODE_FDB_LOG_NOTIFICATION);
+      swi->port_set_move_learn(
+          port_id, switch_interface::
+                       SAI_BRIDGE_PORT_FDB_LEARNING_MODE_FDB_LOG_NOTIFICATION);
     } else {
       LOG(WARNING) << __FUNCTION__ << ": ignoring link with lt=" << lt
                    << " link:" << link;

src/netlink/nbi_impl.cc
@@ -52,14 +52,6 @@ void nbi_impl::port_notification(
     case PORT_EVENT_TABLE:
       switch (get_port_type(ntfy.port_id)) {
       case nbi::port_type_physical:
-        swi->port_set_learn(
-            ntfy.port_id,
-            switch_interface::
-                SAI_BRIDGE_PORT_FDB_LEARNING_MODE_FDB_LOG_NOTIFICATION);
-        swi->port_set_move_learn(
-            ntfy.port_id,
-            switch_interface::
-                SAI_BRIDGE_PORT_FDB_LEARNING_MODE_FDB_LOG_NOTIFICATION);
         port_man->create_portdev(ntfy.port_id, ntfy.name, ntfy.hwaddr, *this);
         break;
       default: