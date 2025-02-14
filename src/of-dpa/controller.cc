bisdn/basebox

commit 87b00ee6b64eca75e67835a5d81ddc706924338c

ofdpa_client: expose per port learning configuration
Expose the newly added functions for configuring learning on a per port
basis.

To make it more SAI-ish, use the logical SAI states [1]. Since the
default learning behavior isn't covered by any of the states, add an
extra state for that.

[1] https://github.com/opencomputeproject/SAI/blob/5ff3424512031258ed2431b06b64beea5bc36712/inc/saibridge.h#L39

Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/of-dpa/controller.cc
@@ -531,6 +531,76 @@ errout:
   std::free(pkt);
   return rv;
 }
+int controller::sai_learn_mode_to_flags(sai_bridge_port_fdb_learning_t mode,
+                                        uint32_t *flags) {
+  uint32_t hw_flags;
+
+  switch (mode) {
+  case SAI_BRIDGE_PORT_FDB_LEARNING_MODE_DROP:
+    hw_flags = ofdpa_client::SRC_MAC_LEARN_NONE;
+    break;
+  case SAI_BRIDGE_PORT_FDB_LEARNING_MODE_DISABLE:
+    hw_flags = ofdpa_client::SRC_MAC_LEARN_FWD;
+    break;
+  case SAI_BRIDGE_PORT_FDB_LEARNING_MODE_HW:
+    hw_flags =
+        ofdpa_client::SRC_MAC_LEARN_FWD | ofdpa_client::SRC_MAC_LEARN_ARL;
+    break;
+  case SAI_BRIDGE_PORT_FDB_LEARNING_MODE_CPU_TRAP:
+    hw_flags = ofdpa_client::SRC_MAC_LEARN_CPU;
+    break;
+  case SAI_BRIDGE_PORT_FDB_LEARNING_MODE_CPU_LOG:
+    hw_flags =
+        ofdpa_client::SRC_MAC_LEARN_FWD | ofdpa_client::SRC_MAC_LEARN_CPU;
+    break;
+  case SAI_BRIDGE_PORT_FDB_LEARNING_MODE_FDB_NOTIFICATION:
+    hw_flags = ofdpa_client::SRC_MAC_LEARN_CPU |
+               ofdpa_client::SRC_MAC_LEARN_ARL |
+               ofdpa_client::SRC_MAC_LEARN_PENDING;
+    break;
+  case SAI_BRIDGE_PORT_FDB_LEARNING_MODE_FDB_LOG_NOTIFICATION:
+    hw_flags =
+        ofdpa_client::SRC_MAC_LEARN_FWD | ofdpa_client::SRC_MAC_LEARN_CPU |
+        ofdpa_client::SRC_MAC_LEARN_ARL | ofdpa_client::SRC_MAC_LEARN_PENDING;
+    break;
+  default:
+    return -EINVAL;
+  }
+
+  *flags = hw_flags;
+
+  return 0;
+}
+
+int controller::port_set_learn(
+    uint32_t port_id, sai_bridge_port_fdb_learning_t l2_learn) noexcept {
+  if (!connected) {
+    VLOG(1) << __FUNCTION__ << ": not connected";
+    return -EAGAIN;
+  }
+
+  uint32_t flags;
+  int rv = sai_learn_mode_to_flags(l2_learn, &flags);
+  if (rv)
+    return rv;
+
+  return ofdpa->ofdpaPortSourceMacLearningSet(port_id, flags);
+}
+
+int controller::port_set_move_learn(
+    uint32_t port_id, sai_bridge_port_fdb_learning_t l2_learn) noexcept {
+  if (!connected) {
+    VLOG(1) << __FUNCTION__ << ": not connected";
+    return -EAGAIN;
+  }
+
+  uint32_t flags;
+  int rv = sai_learn_mode_to_flags(l2_learn, &flags);
+  if (rv)
+    return rv;
+
+  return ofdpa->ofdpaPortSourceMacMoveLearningSet(port_id, l2_learn);
+}
 
 int controller::lag_create(uint32_t *lag_id, std::string name,
                            uint8_t mode) noexcept {

src/of-dpa/controller.h
@@ -141,6 +141,11 @@ protected:
 
 public:
   // switch_interface
+  int port_set_learn(uint32_t port_id,
+                     sai_bridge_port_fdb_learning_t l2_learn) noexcept override;
+  int port_set_move_learn(
+      uint32_t port_id,
+      sai_bridge_port_fdb_learning_t l2_learn) noexcept override;
   int lag_create(uint32_t *lag_id, std::string name,
                  uint8_t mode) noexcept override;
   int lag_remove(uint32_t lag_id) noexcept override;
@@ -375,6 +380,9 @@ private:
     else
       return 1;
   }
+
+  int sai_learn_mode_to_flags(sai_bridge_port_fdb_learning_t mode,
+                              uint32_t *flags);
 
   enum timer_t {
     /* handle_timeout will be called as well from crofbase, hence we need some

src/of-dpa/ofdpa_client.cc
@@ -79,6 +79,52 @@ ofdpa_client::ofdpaTunnelTenantDelete(uint32_t tunnel_id) {
 }
 
 OfdpaStatus::OfdpaStatusCode
+ofdpa_client::ofdpaPortSourceMacLearningSet(uint32_t port_num,
+                                            uint32_t l2_learn) {
+  ::ofdpa::PortSrcMacLearning request;
+  ::ClientContext context;
+  ::OfdpaStatus response;
+
+  context.set_wait_for_ready(true);
+
+  request.set_port_num(port_num);
+  request.set_l2_learn(l2_learn);
+
+  ::Status rv =
+      stub_->ofdpaPortSourceMacLearningSet(&context, request, &response);
+
+  if (not rv.ok()) {
+    // LOG status
+    return ofdpa::OfdpaStatus::OFDPA_E_RPC;
+  }
+
+  return response.status();
+}
+
+OfdpaStatus::OfdpaStatusCode
+ofdpa_client::ofdpaPortSourceMacMoveLearningSet(uint32_t port_num,
+                                                uint32_t l2_learn) {
+  ::ofdpa::PortSrcMacLearning request;
+  ::ClientContext context;
+  ::OfdpaStatus response;
+
+  context.set_wait_for_ready(true);
+
+  request.set_port_num(port_num);
+  request.set_l2_learn(l2_learn);
+
+  ::Status rv =
+      stub_->ofdpaPortSourceMacMoveLearningSet(&context, request, &response);
+
+  if (not rv.ok()) {
+    // LOG status
+    return ofdpa::OfdpaStatus::OFDPA_E_RPC;
+  }
+
+  return response.status();
+}
+
+OfdpaStatus::OfdpaStatusCode
 ofdpa_client::ofdpaTunnelNextHopCreate(uint32_t next_hop_id, uint64_t src_mac,
                                        uint64_t dst_mac, uint32_t physical_port,
                                        uint16_t vlan_id) {

src/of-dpa/ofdpa_client.h
@@ -14,6 +14,14 @@ namespace basebox {
 
 class ofdpa_client {
 public:
+  typedef enum {
+    SRC_MAC_LEARN_NONE = 0,
+    SRC_MAC_LEARN_ARL = 1,
+    SRC_MAC_LEARN_CPU = 2,
+    SRC_MAC_LEARN_FWD = 4,
+    SRC_MAC_LEARN_PENDING = 8,
+  } ofdpa_src_mac_learn_mode_t;
+
   ofdpa_client(std::shared_ptr<grpc::Channel> channel);
 
   ofdpa::OfdpaStatus::OfdpaStatusCode ofdpaTunnelReset();
@@ -22,6 +30,11 @@ public:
 
   ofdpa::OfdpaStatus::OfdpaStatusCode
   ofdpaTunnelTenantDelete(uint32_t tunnel_id);
+
+  ofdpa::OfdpaStatus::OfdpaStatusCode
+  ofdpaPortSourceMacLearningSet(uint32_t port_num, uint32_t l2_learn);
+  ofdpa::OfdpaStatus::OfdpaStatusCode
+  ofdpaPortSourceMacMoveLearningSet(uint32_t port_num, uint32_t l2_learn);
 
   ofdpa::OfdpaStatus::OfdpaStatusCode
   ofdpaTunnelNextHopCreate(uint32_t next_hop_id, uint64_t src_mac,

src/sai.h
@@ -35,6 +35,36 @@ public:
     SAI_PORT_STAT_COLLISIONS,
   } sai_port_stat_t;
 
+  typedef enum {
+    // Drop packets with unknown source MAC. Do not learn. Do not forward.
+    SAI_BRIDGE_PORT_FDB_LEARNING_MODE_DROP,
+    // Do not learn unknown source MAC. Forward based on destination MAC.
+    SAI_BRIDGE_PORT_FDB_LEARNING_MODE_DISABLE,
+    // Hardware learning. Learn source MAC. Forward based on destination MAC.
+    SAI_BRIDGE_PORT_FDB_LEARNING_MODE_HW,
+    // Trap packets with unknown source MAC to CPU. Do not learn. Do not
+    // forward.
+    SAI_BRIDGE_PORT_FDB_LEARNING_MODE_CPU_TRAP,
+    // Trap packets with unknown source MAC to CPU. Do not learn. Forward based
+    // on destination MAC.
+    SAI_BRIDGE_PORT_FDB_LEARNING_MODE_CPU_LOG,
+    // Do not learn in hardware. Do not forward. This mode will generate only
+    // one notification per unknown source MAC to FDB callback.
+    SAI_BRIDGE_PORT_FDB_LEARNING_MODE_FDB_NOTIFICATION,
+    // Do not learn in hardware, but forward. This mode will generate only one
+    // notification per unknown source MAC to FDB callback.
+    // Not an official SAI mode, but there is no equivalent to what we currently
+    // do by default in OF-DPA.
+    SAI_BRIDGE_PORT_FDB_LEARNING_MODE_FDB_LOG_NOTIFICATION,
+  } sai_bridge_port_fdb_learning_t;
+
+  virtual int
+  port_set_learn(uint32_t port_id,
+                 sai_bridge_port_fdb_learning_t l2_learn) noexcept = 0;
+  virtual int
+  port_set_move_learn(uint32_t port_id,
+                      sai_bridge_port_fdb_learning_t l2_learn) noexcept = 0;
+
   /* @ LAG { */
   virtual int lag_create(uint32_t *lag_id, std::string name,
                          uint8_t mode) noexcept = 0;

commit 00ed2ca2f4a0960031dec4eaaf0f1f2682dca383

controller: fix setting station move configuration
In controller::port_set_move_learn(), we convert the passed enum to
flags, but then pass on the original enum instead to OF-DPA. This sets a
random configuration, which may or may not be what we actually
intentend.

Fix this by passing the correct flags variable.

Fixes: 87b00ee6b64e ("ofdpa_client: expose per port learning configuration")
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/of-dpa/controller.cc
@@ -599,7 +599,7 @@ int controller::port_set_move_learn(
   if (rv)
     return rv;
 
-  return ofdpa->ofdpaPortSourceMacMoveLearningSet(port_id, l2_learn);
+  return ofdpa->ofdpaPortSourceMacMoveLearningSet(port_id, flags);
 }
 
 int controller::lag_create(uint32_t *lag_id, std::string name,