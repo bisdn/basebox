bisdn/basebox

commit 396770433ee237a185fa2e9845ca392d9344e7ee

vrf_attach: delete ip address from ASIC

src/netlink/cnetlink.cc
@@ -1093,7 +1093,7 @@ void cnetlink::link_updated(rtnl_link *old_link, rtnl_link *new_link) noexcept {
       }
     }
     break;
-  case LT_VLAN:
+  case LT_VLAN: {
     if (lt_new != LT_VRF_SLAVE) {
       VLOG(1) << __FUNCTION__ << ": ignoring update of VLAN interface";
       break;
@@ -1102,9 +1102,16 @@ void cnetlink::link_updated(rtnl_link *old_link, rtnl_link *new_link) noexcept {
               << rtnl_link_get_name(new_link) << " to vrf "
               << OBJ_CAST(
                      get_link_by_ifindex(rtnl_link_get_master(new_link)).get());
+    // Lookup l3 addresses to delete
+    std::deque<rtnl_addr *> addresses;
+    get_l3_addrs(old_link, &addresses);
+    // delete l3 addresses
+    for (auto i : addresses)
+      l3->del_l3_addr(i);
+
     vlan->vrf_attach(old_link, new_link);
-    break;
-  case LT_VRF_SLAVE:
+    } break;
+  case LT_VRF_SLAVE: {
     if (lt_new == LT_VLAN) {
       LOG(INFO) << __FUNCTION__ << ": freed vrf slave interface "
                 << OBJ_CAST(old_link);
@@ -1122,7 +1129,7 @@ void cnetlink::link_updated(rtnl_link *old_link, rtnl_link *new_link) noexcept {
     }
     // TODO handle LT_TAP/LT_BOND, currently unimplemented
     // happens when a bond/tap interface enslaved directly to a VRF and you set nomaster
-    break;
+    } break;
   case LT_TUN:
     if (lt_new == LT_BOND_SLAVE) {
       // XXX link enslaved

commit ec6dbeaaf0a4c0f8d74eca496ead7472de1e4f51

cnetlink: also remove routes on VRF changes
Like we need to remove existing l3 addresses, we also need to remove
existing routes on VRF attachement or detachment.

Fixes: 396770433ee2 ("vrf_attach: delete ip address from ASIC")
Closes: https://github.com/bisdn/basebox/issues/290
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/netlink/cnetlink.cc
@@ -1624,6 +1624,7 @@ void cnetlink::link_updated(rtnl_link *old_link, rtnl_link *new_link) noexcept {
     // We can delete the addresses on the interface because we will later
     // receive a notification readding the addresses, that time with the correct
     // VRF
+    remove_l3_routes(old_link);
     remove_l3_addresses(old_link);
 
     vlan->vrf_attach(old_link, new_link);
@@ -1636,6 +1637,7 @@ void cnetlink::link_updated(rtnl_link *old_link, rtnl_link *new_link) noexcept {
       // We can delete the addresses on the interface because we will later
       // receive a notification readding the addresses, that time with the
       // correct VRF
+      remove_l3_routes(old_link);
       remove_l3_addresses(old_link);
       vlan->vrf_detach(old_link, new_link);
     } else if (lt_new == LT_VRF_SLAVE) {