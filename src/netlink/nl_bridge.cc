bisdn/basebox

commit 30e4b45ffe29dec9fb53b9d8a03e06d954f97ac9

nl_bridge: ensure we do not create multicast groups for 224.0.0.X
According to RFC 4541 ("Considerations for Internet Group Management
Protocol (IGMP) and Multicast Listener Discovery (MLD) Snooping
Switches", multicast traffic to addresses in the 224.0.0.X range should
always be flooded. So to ensure that we need to be sure that we never
create groups for them.

Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/netlink/nl_bridge.cc
@@ -920,6 +920,18 @@ int nl_bridge::mdb_entry_add(rtnl_mdb *mdb_entry) {
 
     auto addr = rtnl_mdb_entry_get_addr(i);
     if (rtnl_mdb_entry_get_proto(i) == ETH_P_IP) {
+      // RFC 4541, section 2.1.2 says:
+      // 2) Packets with a destination IP (DIP) address in the 224.0.0.X range
+      //    which are not IGMP must be forwarded on all ports.
+      //
+      // Therefore we must not create groups for them to ensure that.
+
+      auto p = nl_addr_alloc(4);
+      nl_addr_parse("224.0.0.0/24", AF_INET, &p);
+      std::unique_ptr<nl_addr, decltype(&nl_addr_put)> tm_addr(p, nl_addr_put);
+      if (!nl_addr_cmp_prefix(addr, tm_addr.get()))
+        continue;
+
       rofl::caddress_in4 ipv4_dst = libnl_in4addr_2_rofl(addr, &rv);
       if (rv < 0) {
         LOG(ERROR) << __FUNCTION__ << ": could not parse addr " << addr;
@@ -1000,6 +1012,12 @@ int nl_bridge::mdb_entry_remove(rtnl_mdb *mdb_entry) {
 
     auto addr = rtnl_mdb_entry_get_addr(i);
     if (rtnl_mdb_entry_get_proto(i) == ETH_P_IP) {
+      auto p = nl_addr_alloc(4);
+      nl_addr_parse("224.0.0.0/24", AF_INET, &p);
+      std::unique_ptr<nl_addr, decltype(&nl_addr_put)> tm_addr(p, nl_addr_put);
+      if (!nl_addr_cmp_prefix(addr, tm_addr.get()))
+        continue;
+
       rofl::caddress_in4 ipv4_dst = libnl_in4addr_2_rofl(addr, &rv);
       if (rv < 0) {
         LOG(ERROR) << __FUNCTION__ << ": could not parse addr " << addr;

commit 0e08577d2bcfc70cc71309faccac510387a02095

nl_bridge: statically allocate comparison addresses
Valgrind complains about memory being leaked by nl_bridge::mdb_to_set():

==2768== 36 bytes in 1 blocks are definitely lost in loss record 806 of 1,550
==2768==    at 0x484A208: calloc (vg_replace_malloc.c:1328)
==2768==    by 0x56BE091: nl_addr_alloc (addr.c:189)
==2768==    by 0x24BEB1: basebox::nl_bridge::mdb_to_set(rtnl_mdb*, std::set<std::tuple<unsigned int, unsigned short, rofl::caddress_ll>, std::less<std::tuple<unsigned int, unsigned short, rofl::caddress_ll> >, std::allocator<std::tuple<unsigned int, unsigned short, rofl::caddress_ll> > >*) (nl_bridge.cc:1082)
==2768==    by 0x24C3EB: basebox::nl_bridge::mdb_update(rtnl_mdb*, rtnl_mdb*) (nl_bridge.cc:1116)
==2768==    by 0x23C3D7: basebox::cnetlink::handle_wakeup(rofl::cthread&) (cnetlink.cc:939)
==2768==    by 0x58DD082: rofl::cthread::handle_wakeup() (in /usr/lib/librofl_common.so.0.1.1)
==2768==    by 0x58DD717: rofl::cthread::run_loop() (in /usr/lib/librofl_common.so.0.1.1)
==2768==    by 0x61ED649: start_thread (pthread_create.c:442)
==2768==    by 0x626E583: clone (clone.S:100)
==2768==
==2768== 36 bytes in 1 blocks are definitely lost in loss record 807 of 1,550
==2768==    at 0x484A208: calloc (vg_replace_malloc.c:1328)
==2768==    by 0x56BE091: nl_addr_alloc (addr.c:189)
==2768==    by 0x56BE8A7: nl_addr_parse (addr.c:448)
==2768==    by 0x24BEF2: basebox::nl_bridge::mdb_to_set(rtnl_mdb*, std::set<std::tuple<unsigned int, unsigned short, rofl::caddress_ll>, std::less<std::tuple<unsigned int, unsigned short, rofl::caddress_ll> >, std::allocator<std::tuple<unsigned int, unsigned short, rofl::caddress_ll> > >*) (nl_bridge.cc:1092)
==2768==    by 0x24C3EB: basebox::nl_bridge::mdb_update(rtnl_mdb*, rtnl_mdb*) (nl_bridge.cc:1116)
==2768==    by 0x23C3D7: basebox::cnetlink::handle_wakeup(rofl::cthread&) (cnetlink.cc:939)
==2768==    by 0x58DD082: rofl::cthread::handle_wakeup() (in /usr/lib/librofl_common.so.0.1.1)
==2768==    by 0x58DD717: rofl::cthread::run_loop() (in /usr/lib/librofl_common.so.0.1.1)
==2768==    by 0x61ED649: start_thread (pthread_create.c:442)
==2768==    by 0x626E583: clone (clone.S:100)

The core issue is that we first alloc a nl_addr, and then call
nl_addr_parse(), which will then alloc its own nl_addr, replacing the
value of p, leaking the original allocated nl_addr.

      auto p = nl_addr_alloc(4);
      nl_addr_parse("224.0.0.0/24", AF_INET, &p);

Only then we create unique_ptr<> for the second allocation, so the first
is never free'd.

In the IPv6 case, we then call nl_addr_parse() a second time, now
replacing p again with a newly allocated nl_addr(). But we never update
the unique_ptr<>, so the second allocated address is again leaked.

Since we parse static addresses, replace those dynamic allocations with
statically allocated addresses in the constructor.

This sidesteps the issue of leaking them, and avoids having to allocate
and free addresses for every mdb entry just to compare them.

Fixes: 30e4b45ffe29 ("nl_bridge: ensure we do not create multicast groups for 224.0.0.X")

src/netlink/nl_bridge.cc
@@ -44,9 +44,18 @@ nl_bridge::nl_bridge(switch_interface *sw,
                nl_cache_free) {
   memset(&empty_br_vlan, 0, sizeof(rtnl_link_bridge_vlan));
   memset(&vxlan_dom_bitmap, 0, sizeof(vxlan_dom_bitmap));
+
+  nl_addr_parse("224.0.0.0/24", AF_INET, &ipv4_igmp);
+  nl_addr_parse("ff02::1/128", AF_INET6, &ipv6_all_hosts);
+  nl_addr_parse("ff00::/15", AF_INET6, &ipv6_mld);
 }
 
-nl_bridge::~nl_bridge() { rtnl_link_put(bridge); }
+nl_bridge::~nl_bridge() {
+  rtnl_link_put(bridge);
+  nl_addr_put(ipv4_igmp);
+  nl_addr_put(ipv6_all_hosts);
+  nl_addr_put(ipv6_mld);
+}
 
 static rofl::caddress_in4 libnl_in4addr_2_rofl(struct nl_addr *addr, int *rv) {
   struct sockaddr_in sin;
@@ -1056,10 +1065,7 @@ int nl_bridge::mdb_to_set(
       //
       // Therefore we must not create groups for them to ensure that.
 
-      auto p = nl_addr_alloc(4);
-      nl_addr_parse("224.0.0.0/24", AF_INET, &p);
-      std::unique_ptr<nl_addr, decltype(&nl_addr_put)> tm_addr(p, nl_addr_put);
-      if (!nl_addr_cmp_prefix(addr, tm_addr.get()))
+      if (!nl_addr_cmp_prefix(addr, ipv4_igmp))
         continue;
 
       rofl::caddress_in4 ipv4_dst = libnl_in4addr_2_rofl(addr, &rv);
@@ -1079,18 +1085,14 @@ int nl_bridge::mdb_to_set(
       // link-scope address for which MLD messages are never sent.  Packets
       // with the all hosts link-scope address should be forwarded on all
       // ports.
-      auto p = nl_addr_alloc(16);
-      nl_addr_parse("ff02::1/128", AF_INET6, &p);
-      std::unique_ptr<nl_addr, decltype(&nl_addr_put)> tm_addr(p, nl_addr_put);
-      if (!nl_addr_cmp(addr, tm_addr.get()))
+      if (!nl_addr_cmp(addr, ipv6_all_hosts))
         continue;
 
       // MLD messages are also not sent regarding groups with addresses in the
       // range FF00::/15 (which encompasses both the reserved FF00::/16 and
       // node-local FF01::/16 IPv6 address spaces).  These addresses should
       // never appear in packets on the link.
-      nl_addr_parse("ff00::/15", AF_INET6, &p);
-      if (!nl_addr_cmp_prefix(addr, tm_addr.get()))
+      if (!nl_addr_cmp_prefix(addr, ipv6_mld))
         continue;
 
       struct in6_addr *v6_addr =

src/netlink/nl_bridge.h
@@ -230,6 +230,10 @@ private:
   rtnl_link_bridge_vlan empty_br_vlan;
   uint32_t vxlan_dom_bitmap[RTNL_LINK_BRIDGE_VLAN_BITMAP_LEN];
   std::map<uint16_t, uint32_t> vxlan_domain;
+
+  nl_addr *ipv4_igmp;
+  nl_addr *ipv6_all_hosts;
+  nl_addr *ipv6_mld;
 };
 
 } /* namespace basebox */