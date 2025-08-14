bisdn/basebox

commit 0a5c7c1854ba0eb644b74fe36c136404f1e0cc6c

bug: fix late nexthop registration
* added logic to process a route when nexthop is found;
* doc: add comments to functions;
* ensure the object is freed at the end of processing;

Signed-off-by: Rubens Figueiredo <rubens.figueiredo@bisdn.de>

src/netlink/nl_l3.cc
@@ -15,12 +15,14 @@
 #include <netlink/route/link/vrf.h>
 #include <netlink/route/neighbour.h>
 #include <netlink/route/route.h>
+#include <netlink/cache.h>
 
 #include "cnetlink.h"
 #include "nl_hashing.h"
 #include "nl_l3.h"
 #include "nl_output.h"
 #include "nl_vlan.h"
+#include "nl_route_query.h"
 #include "sai.h"
 #include "utils/rofl-utils.h"
 
@@ -268,7 +270,7 @@ int nl_l3::add_l3_addr_v6(struct rtnl_addr *a) {
   // link local addresses must redirect to controllers
   int port_id = nl->get_port_id(link);
   auto addr = rtnl_addr_get_local(a);
-  if (is_link_local_address(addr)) {
+  if (is_ipv6_link_local_address(addr)) {
 
     rofl::caddress_in6 ipv6_dst = libnl_in6addr_2_rofl(addr, &rv);
     if (rv < 0) {
@@ -629,6 +631,30 @@ int nl_l3::add_l3_neigh(struct rtnl_neigh *n) {
       cb = nh_callbacks.erase(cb);
     } else {
       ++cb;
+    }
+  }
+
+  for (auto cb = std::begin(net_resolved_callbacks);
+       cb != std::end(net_resolved_callbacks); cb++) {
+    if (cb->ifindex == rtnl_neigh_get_ifindex(n) &&
+        nl_addr_cmp_prefix(cb->addr, rtnl_neigh_get_dst(n))) {
+
+      // query the kernel for the correct route matching the dst addr
+      nl_route_query rq;
+      auto rt = rq.query_route(cb->addr);
+
+      auto ad = rtnl_route_get_dst(rt);
+      nl_addr_set_prefixlen(
+          ad,
+          nl_addr_get_prefixlen(cb->addr)); // guarantee the correct prefixlen
+
+      add_l3_unicast_route(rt, true);
+      nl_object_put(
+          OBJ_CAST(rt)); // return object has to be freed using nl_object_put
+
+#if 0
+      net_resolved_callbacks.erase(cb); // Object must as well be freed
+#endif
     }
   }
 
@@ -1243,6 +1269,10 @@ void nl_l3::notify_on_nh_reachable(nh_reachable *f,
   nh_callbacks.emplace_back(f, p);
 }
 
+void nl_l3::notify_on_nh_resolved(struct net_params p) noexcept {
+  net_resolved_callbacks.emplace_back(p);
+}
+
 int nl_l3::get_l3_interface_id(rtnl_neigh *n, uint32_t *l3_interface_id,
                                uint32_t vid) {
   assert(l3_interface_id);
@@ -1471,9 +1501,10 @@ int nl_l3::add_l3_unicast_route(rtnl_route *r, bool update_route) {
   for (auto nh : unresolved_nh) {
     VLOG(2) << __FUNCTION__ << ": got unresolved nh ifindex=" << nh.ifindex
             << ", nh=" << nh.nh;
-#if 0
-    notify_on_nh_resolved() // XXX TODO implement nh update
-#endif // 0
+
+    // If the next-hop is currently unresolved, we store the route and
+    // process it when the nh is found
+    notify_on_nh_resolved(net_params(rtnl_route_get_dst(r), nh.ifindex));
   }
 
   VLOG(2) << __FUNCTION__ << ": got " << neighs.size() << " neighbours ("
@@ -1511,24 +1542,22 @@ int nl_l3::add_l3_unicast_route(rtnl_route *r, bool update_route) {
     l3_interfaces.emplace(l3_interface_id);
   }
 
-  if (l3_interfaces.size() == 0) {
-    // got no neighs or egress interface creation failed
-    // XXX TODO handle this
-
-    // cleanup
-    for (auto n : neighs)
-      rtnl_neigh_put(n);
-
-    return -EINVAL;
-  }
-
-  if (nnhs == 1) {
-    // single next hop
+  switch (nnhs) {
+  case 0:
+    // Not yet determined next-hop, the rule must be installed to the switch
+    // pointing to controller and then updated after next-hop registration
+    rv = add_l3_unicast_route(rtnl_route_get_dst(r), 0, false, update_route,
+                              vrf_id);
+    break;
+  case 1:
+    // Single known next-hop
     rv = add_l3_unicast_route(rtnl_route_get_dst(r), *l3_interfaces.begin(),
                               false, update_route, vrf_id);
-  } else {
+    break;
+  default:
     // create ecmp group
     rv = add_l3_ecmp_route(r, l3_interfaces, update_route);
+    break;
   }
 
   if (rv < 0) {
@@ -1639,14 +1668,6 @@ int nl_l3::del_l3_unicast_route(rtnl_route *r, bool keep_route) {
   }
 
   return rv;
-}
-
-bool nl_l3::is_link_local_address(const struct nl_addr *addr) {
-  auto p = nl_addr_alloc(16);
-  nl_addr_parse("fe80::/10", AF_INET6, &p);
-  std::unique_ptr<nl_addr, decltype(&nl_addr_put)> ll_addr(p, nl_addr_put);
-
-  return !nl_addr_cmp_prefix(ll_addr.get(), addr);
 }
 
 // Modifying the previously existing entries on the VLAN table requires

src/netlink/nl_l3.h
@@ -66,7 +66,7 @@ public:
 
   void notify_on_net_reachable(net_reachable *f, struct net_params p) noexcept;
   void notify_on_nh_reachable(nh_reachable *f, struct nh_params p) noexcept;
-  // void notify_on_nh_resovled(nh_resolved *f, struct nh_params p) noexcept;
+  void notify_on_nh_resolved(struct net_params p) noexcept;
 
 private:
   int get_l3_interface_id(rtnl_neigh *n, uint32_t *l3_interface_id,
@@ -102,7 +102,22 @@ private:
   int del_l3_egress(int ifindex, uint16_t vid, const struct nl_addr *s_mac,
                     const struct nl_addr *d_mac);
 
-  bool is_link_local_address(const struct nl_addr *addr);
+  bool is_ipv6_link_local_address(const struct nl_addr *addr) {
+    auto p = nl_addr_alloc(16);
+    nl_addr_parse("fe80::/10", AF_INET6, &p);
+    std::unique_ptr<nl_addr, decltype(&nl_addr_put)> ll_addr(p, nl_addr_put);
+
+    return !nl_addr_cmp_prefix(ll_addr.get(), addr);
+  }
+
+  bool is_ipv6_multicast_address(const struct nl_addr *addr) {
+    auto p = nl_addr_alloc(16);
+    nl_addr_parse("ff80::/10", AF_INET6, &p);
+    std::unique_ptr<nl_addr, decltype(&nl_addr_put)> mc_addr(p, nl_addr_put);
+
+    return !nl_addr_cmp_prefix(mc_addr.get(), addr);
+  }
+
   uint16_t get_vrf_table_id(rtnl_link *vrf);
 
   switch_interface *sw;
@@ -110,6 +125,7 @@ private:
   cnetlink *nl;
   std::deque<std::pair<net_reachable *, net_params>> net_callbacks;
   std::deque<std::pair<nh_reachable *, nh_params>> nh_callbacks;
+  std::deque<net_params> net_resolved_callbacks; // handle the unknown nexthops
   const uint8_t MAIN_ROUTING_TABLE = 254;
 };
 

commit 77e0098481db990a7e859a3bf96c203a97e232b6

nl_l3: fix IPv6 multicast prefix
The IPv6 multicast prefix is ff00::/8, not ff80::/10 [1].

[1] https://www.rfc-editor.org/rfc/rfc2373#section-2.7

Fixes: 0a5c7c1854ba ("bug: fix late nexthop registration")
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/netlink/nl_l3.cc
@@ -108,7 +108,7 @@ nl_l3::nl_l3(std::shared_ptr<nl_vlan> vlan, cnetlink *nl)
   nl_addr_parse("127.0.0.0/8", AF_INET, &ipv4_lo);
   nl_addr_parse("::1/128", AF_INET6, &ipv6_lo);
   nl_addr_parse("fe80::/10", AF_INET6, &ipv6_ll);
-  nl_addr_parse("ff80::/10", AF_INET6, &ipv6_mc);
+  nl_addr_parse("ff00::/8", AF_INET6, &ipv6_mc);
 }
 
 nl_l3::~nl_l3() {