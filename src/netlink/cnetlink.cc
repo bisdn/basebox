bisdn/basebox

commit ec8902a05f364de71ffb8eb03feb07c86fe272f6

cnetlink::link_created(): call vxlan::create_endpoint() later
When a VxLAN interface is created, it is not yet attached to a bridge,
so vxlan::create_interface() will fail, but still do some setup.

When the interface is then later attached to a bridge,
vxlan::create_endpoint() may be called a second time, but since the
previous call already did some work, the call will then fail again,
since time because it assumes the previous call succeded.

Since the vxlan::register_bridge() code skips calling
vxlan::create_interface() for VxLAN interfaces not attached to bridges
in its scan of existing interfaces, let's mirror that in
cnetlink::link_created() and don't call vxlan::create_endpoint() there.
It is newly crated, so it cannot be attached to a bridge yet.

But since this only is done once on registration of the bridge, we still
need to call it "manually" for VxLAN interfaces attached at a later
step, so move the vxlan::create_endpoint() to the code handling bridge
attachement.

Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/netlink/cnetlink.cc
@@ -1328,6 +1328,8 @@ void cnetlink::link_created(rtnl_link *link) noexcept {
       LOG(INFO) << __FUNCTION__ << ": enslaving interface "
                 << rtnl_link_get_name(link);
 
+      if (rtnl_link_is_vxlan(base_link) && !new_bridge)
+        vxlan->create_endpoint(base_link);
       vlan->disable_vlans(link);
       bridge->add_interface(link);
 
@@ -1347,8 +1349,6 @@ void cnetlink::link_created(rtnl_link *link) noexcept {
                  << OBJ_CAST(link);
       break;
     }
-
-    vxlan->create_endpoint(link);
   } break;
   case LT_VLAN: {
     VLOG(1) << __FUNCTION__ << ": new vlan interface " << OBJ_CAST(link);

commit 73ffbf30b6298e884aa283b07ea67e17abdee63c

nl_vxlan: always pass bridge interface to ::create_endpoint()
When nl_vlxan::create_endpoint(rtnl_link *) is called, it has no
br_link, it calls nl_vlxan::create_endpoint(rtnl_link *, rtnl_link *,
nl_addr *) with a nullptr for the bridge interface.

This causes it to not add the endpoint to the l2 overlay group, breaking
connectivity.

Since we only ever call nl_vlxan::create_endpoint(rtnl_link *) from
places where we know a br_link exists, add it to the arguments.

Should fix spurious failures of the vxlan-bridge tests in our testing
pipelines.

Fixes: ec8902a05f36 ("cnetlink::link_created(): call vxlan::create_endpoint() later")
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/netlink/cnetlink.cc
@@ -1489,7 +1489,7 @@ void cnetlink::link_created(rtnl_link *link) noexcept {
                 << rtnl_link_get_name(link);
 
       if (rtnl_link_is_vxlan(base_link) && !new_bridge)
-        vxlan->create_endpoint(base_link);
+        vxlan->create_endpoint(base_link, link);
       vlan->disable_vlans(link);
       bridge->add_interface(link);
 

src/netlink/nl_vxlan.cc
@@ -260,8 +260,10 @@ void nl_vxlan::register_bridge(nl_bridge *bridge) {
     if (rtnl_link_get_master(link) == 0)
       continue;
 
+    auto br_link = nl->get_link(rtnl_link_get_ifindex(link), AF_BRIDGE);
+
     // XXX TODO maybe before cont.
-    rv = create_endpoint(link);
+    rv = create_endpoint(link, br_link);
     if (rv < 0) {
       LOG(ERROR) << __FUNCTION__ << ": failed (rv=" << rv
                  << ") to create create endpoint " << link;
@@ -529,7 +531,7 @@ int nl_vxlan::remove_vni(rtnl_link *link) {
   return 0;
 }
 
-int nl_vxlan::create_endpoint(rtnl_link *vxlan_link) {
+int nl_vxlan::create_endpoint(rtnl_link *vxlan_link, rtnl_link *br_link) {
   assert(vxlan_link);
 
   if (!rtnl_link_is_vxlan(vxlan_link)) {
@@ -555,9 +557,7 @@ int nl_vxlan::create_endpoint(rtnl_link *vxlan_link) {
     return -ENOTSUP;
   }
 
-  // XXX get br_link if master exists
-
-  create_endpoint(vxlan_link, nullptr, group_.get());
+  create_endpoint(vxlan_link, br_link, group_.get());
 
   return 0;
 }

src/netlink/nl_vxlan.h
@@ -54,7 +54,7 @@ public:
   int delete_access_port(rtnl_link *br_link, uint32_t pport_no, uint16_t vid,
                          bool wipe_l2_addresses);
 
-  int create_endpoint(rtnl_link *vxlan_link);
+  int create_endpoint(rtnl_link *vxlan_link, rtnl_link *br_link);
   int delete_endpoint(rtnl_link *vxlan_link);
 
 private: