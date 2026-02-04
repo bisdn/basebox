bisdn/basebox

commit cefce264a6575b51d7380898540842bbec5de103

knet_manager: do not try to set state for non-existing links
Since OF-DPA brings all links up at boot, there is a chance that the is
a link state change before we received the port table and created the
ports.

If this happens, then we get a change modification for speed/link, but
there is no link to modify. This is harmless since we will set the link
to down once we do create the port interface, but triggers a warning
from knet on startup:

kernel: linux-bcm-knet (1197): Warning: unknown network interface: 'port1'
kernel: linux-bcm-knet (1197): Warning: unknown network interface: 'port2'
...
kernel: linux-bcm-knet (1197): Warning: unknown network interface: 'port54'

So check if the link is created before trying to update its state.

Fixes: 40f68c81c10a ("add knet manager for using knet based interfaces")
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/netlink/knet_manager.cc
@@ -290,6 +290,15 @@ bool knet_manager::portdev_removed(rtnl_link *link) {
  * @return 0 on success
  */
 int knet_manager::change_port_status(const std::string name, bool status) {
+  {
+    std::lock_guard<std::mutex> lock{tn_mutex};
+
+    if (port_names2id.find(name) == port_names2id.end()) {
+      VLOG(1) << __FUNCTION__ << ": unknown port " << name;
+      return -EINVAL;
+    }
+  }
+
   std::ofstream file("/proc/bcm/knet/link");
 
   if (file.is_open()) {
@@ -312,6 +321,15 @@ int knet_manager::change_port_status(const std::string name, bool status) {
  */
 int knet_manager::set_port_speed(const std::string name, uint32_t speed,
                                  uint8_t duplex) {
+  {
+    std::lock_guard<std::mutex> lock{tn_mutex};
+
+    if (port_names2id.find(name) == port_names2id.end()) {
+      VLOG(1) << __FUNCTION__ << ": unknown port " << name;
+      return -EINVAL;
+    }
+  }
+
   std::ofstream file("/proc/bcm/knet/link");
 
   if (file.is_open()) {

commit 5289903690e941a964d9f8ac221b758acbbf9136

knet_manager: fix deadlock on port creation
When creating ports in knet_manager::create_portdev(), they are created
with tn_mutex held, and calls knet_manager::change_port_status() to set
the initial config to down.

Since commit cefce264a657 ("knet_manager: do not try to set state for
non-existing links"), knet_manager::change_port_status() also takes the
mutex, leading to a deadlock on port creation.

Fix this by providing a non-locking variant of
knet_manager::change_port_status() to use by
knet_manager::create_portdev().

Fixes: cefce264a657 ("knet_manager: do not try to set state for non-existing links")
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/netlink/knet_manager.cc
@@ -86,7 +86,7 @@ int knet_manager::create_portdev(uint32_t port_id, const std::string &port_name,
         if (r != 0)
           LOG(FATAL) << __FUNCTION__
                      << ": failed to create knet netif for port_id " << port_id;
-        change_port_status(port_name, false);
+        change_port_status_unlocked(port_name, false);
       }
 
     } catch (std::exception &e) {
@@ -231,6 +231,19 @@ bool knet_manager::portdev_removed(rtnl_link *link) {
   return true;
 }
 
+int knet_manager::change_port_status_unlocked(const std::string name,
+                                              bool status) {
+  std::ofstream file("/proc/bcm/knet/link");
+
+  if (file.is_open()) {
+    file << (name + "=" + (status ? "up" : "down"));
+    file.close();
+    return 0;
+  }
+
+  return 1;
+}
+
 /*
  * set netif link state according to open flow link state
  * Status is determined via the portstatus/port_desc_reply message
@@ -249,15 +262,7 @@ int knet_manager::change_port_status(const std::string name, bool status) {
     }
   }
 
-  std::ofstream file("/proc/bcm/knet/link");
-
-  if (file.is_open()) {
-    file << (name + "=" + (status ? "up" : "down"));
-    file.close();
-    return 0;
-  }
-
-  return 1;
+  return change_port_status_unlocked(name, status);
 }
 
 /*

src/netlink/knet_manager.h
@@ -47,6 +47,8 @@ private:
   knet_manager(const knet_manager &other) = delete; // non construction-copyable
   knet_manager &operator=(const knet_manager &) = delete; // non copyable
 
+  int change_port_status_unlocked(const std::string name, bool status);
+
   std::bitset<128> netif_ids_in_use;
   int get_next_netif_id();
 