bisdn/basebox

commit 96e14a879fde8a24350f35c55ff9b286c9503eac

nl_l3: ignore duplicated link routes from FRR
OSPF from FRR 9.0 will sometimes install a duplicate route to an on-link
route, then delete it shortly after:

> Oct 04 08:50:41 accton-as5835-54x baseboxd[2145]: I20231004 08:50:41.874454  2171 cnetlink.cc:1223] route_route_apply: new route (nl_obj=0x7fa654022370) inet 10.0.0.0/24 table main type unicast
> Oct 04 08:50:41 accton-as5835-54x baseboxd[2145]:     preferred-src 10.0.0.1 scope link priority 0 protocol kernel
> Oct 04 08:50:41 accton-as5835-54x baseboxd[2145]:     nexthop dev port54 <>
> ...
> Oct 04 08:50:42 accton-as5835-54x baseboxd[2145]: I20231004 08:50:42.178130  2171 cnetlink.cc:1223] route_route_apply: new route (nl_obj=0x7fa654022c30) inet 10.0.0.0/24 table main type unicast
> Oct 04 08:50:42 accton-as5835-54x baseboxd[2145]:     scope global priority 0x14 protocol 0xbc
> Oct 04 08:50:42 accton-as5835-54x baseboxd[2145]:     nexthop dev port54 <>
> ...
> Oct 04 08:50:42 accton-as5835-54x baseboxd[2145]: I20231004 08:50:42.188859  2171 cnetlink.cc:1258] route_route_apply: del route (nl_obj=0x7fa654022c30) inet 10.0.0.0/24 table main type unicast
> Oct 04 08:50:42 accton-as5835-54x baseboxd[2145]:     scope global priority 0x14 protocol 0xbc
> Oct 04 08:50:42 accton-as5835-54x baseboxd[2145]:     nexthop dev port54 <>

This causes baseboxd to first (try to) install the route a second time,
then delete it when deleting the duplicate route, landing in a state
where the route is missing from the flows.

To work around this, check routes if they have an associated protocol
that isn't kernel, and a priority higher than zero, and if so, if we
already have a route for that destination. If so, ignore the route.

This is far from proper handling of priorities/metrics, but allows us to
avoid a broken state due to duplicate routes.

Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/netlink/nl_l3.cc
@@ -1758,6 +1758,37 @@ int nl_l3::add_l3_unicast_route(rtnl_route *r, bool update_route) {
     return -ENOTSUP;
   }
 
+  // FRR may occationally install link-local routes again with a different
+  // priority. Since we cannot handle multiple routes with different
+  // priorities/metrics yet, ignore the duplicated route.
+  if (rtnl_route_get_priority(r) > 0 &&
+      rtnl_route_get_protocol(r) != RTPROT_KERNEL) {
+    nl_route_query rq;
+
+    auto route = rq.query_route(rtnl_route_get_dst(r));
+
+    if (route) {
+      bool duplicate = false;
+      VLOG(2) << __FUNCTION__ << ": got route " << OBJ_CAST(route)
+              << " for dst " << OBJ_CAST(rtnl_route_get_dst(r));
+      if (rtnl_route_get_protocol(route) == RTPROT_KERNEL &&
+          nl_addr_cmp_prefix(rtnl_route_get_dst(r),
+                             rtnl_route_get_dst(route)) == 0) {
+        // we already have a kernel route for the same dst
+        duplicate = true;
+      }
+      nl_object_put(OBJ_CAST(route));
+
+      // there already is a kernel route, so ignore this one
+      if (duplicate)
+        return 0;
+    } else {
+      // huh?
+      VLOG(2) << __FUNCTION__ << ": no route for dst "
+              << OBJ_CAST(rtnl_route_get_dst(r));
+    }
+  }
+
   std::deque<struct rtnl_neigh *> neighs;
   std::deque<nh_stub> unresolved_nh;
   int rv = get_neighbours_of_route(r, &neighs, &unresolved_nh);
@@ -1905,6 +1936,34 @@ int nl_l3::del_l3_unicast_route(rtnl_route *r, bool keep_route) {
   // table, but will enter the wrong info into the OFDPA tables
   if (vrf_id == MAIN_ROUTING_TABLE)
     vrf_id = 0;
+
+  if (rtnl_route_get_priority(r) > 0 &&
+      rtnl_route_get_protocol(r) != RTPROT_KERNEL) {
+    nl_route_query rq;
+
+    auto route = rq.query_route(rtnl_route_get_dst(r));
+
+    if (route) {
+      bool duplicate = false;
+      VLOG(2) << __FUNCTION__ << ": got route " << OBJ_CAST(route)
+              << " for dst " << OBJ_CAST(rtnl_route_get_dst(r));
+      if (rtnl_route_get_protocol(route) == RTPROT_KERNEL &&
+          nl_addr_cmp_prefix(rtnl_route_get_dst(r),
+                             rtnl_route_get_dst(route)) == 0) {
+        // we have a kernel route for the same dst
+        duplicate = true;
+      }
+      nl_object_put(OBJ_CAST(route));
+
+      // there is still a kernel route, so ignore this one
+      if (duplicate)
+        return 0;
+    } else {
+      // no route anymore, this is fine
+      VLOG(2) << __FUNCTION__ << ": no route for dst "
+              << OBJ_CAST(rtnl_route_get_dst(r));
+    }
+  }
 
   if (!keep_route) {
     rv = del_l3_unicast_route(dst, vrf_id);

commit 0e8b1a276c2c1c66aefd195b2dcfc0be5d407ca9

nl_l3: also compare prefix length in duplicate route check
When checking for duplicate routes, we also need to check the prefix
lengths, else e.g. a route 10.0.0.0/8 will match with a 10.0.10.0/24
route. This can lead to routes falsely being identified as duplicates
and not added or removed as expected.

So switch from nl_addr_cmp_prefix() to nl_addr_cmp() to ensure that
different prefix lengths will be treated as different routes.

Fixes: 96e14a879fde ("nl_l3: ignore duplicated link routes from FRR")
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

src/netlink/nl_l3.cc
@@ -1818,8 +1818,7 @@ int nl_l3::add_l3_unicast_route(rtnl_route *r, bool update_route) {
       VLOG(2) << __FUNCTION__ << ": got route " << route << " for dst "
               << rtnl_route_get_dst(r);
       if (rtnl_route_get_protocol(route) == RTPROT_KERNEL &&
-          nl_addr_cmp_prefix(rtnl_route_get_dst(r),
-                             rtnl_route_get_dst(route)) == 0) {
+          nl_addr_cmp(rtnl_route_get_dst(r), rtnl_route_get_dst(route)) == 0) {
         // we already have a kernel route for the same dst
         duplicate = true;
       }
@@ -1993,8 +1992,7 @@ int nl_l3::del_l3_unicast_route(rtnl_route *r, bool keep_route) {
       VLOG(2) << __FUNCTION__ << ": got route " << route << " for dst "
               << rtnl_route_get_dst(r);
       if (rtnl_route_get_protocol(route) == RTPROT_KERNEL &&
-          nl_addr_cmp_prefix(rtnl_route_get_dst(r),
-                             rtnl_route_get_dst(route)) == 0) {
+          nl_addr_cmp(rtnl_route_get_dst(r), rtnl_route_get_dst(route)) == 0) {
         // we have a kernel route for the same dst
         duplicate = true;
       }